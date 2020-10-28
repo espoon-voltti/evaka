815a77da3b3dc63ea6767069a2ea0729dd06a4b1
Joonas Reynders <joonas.reynders@reaktor.fi>
2020-10-28 09:58:21 +0200

    Add missing license headers

--- frontend/packages/employee-frontend/src/config.ts
+++ frontend/packages/employee-frontend/src/config.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2017-2020 City of Espoo
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
 type Env = 'staging' | 'prod'
 
 const env = (): Env | 'default' => {
--- service/src/main/kotlin/fi/espoo/evaka/attendance/ChildAttendanceQueries.kt
+++ service/src/main/kotlin/fi/espoo/evaka/attendance/ChildAttendanceQueries.kt
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2017-2020 City of Espoo
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
 package fi.espoo.evaka.attendance
 
 import org.jdbi.v3.core.Handle
