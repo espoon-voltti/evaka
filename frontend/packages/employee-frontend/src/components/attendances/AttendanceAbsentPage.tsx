793d82bb29b16ba4aa8e9de65887af4acb8809cf
Panu Klemola <panu.klemola@solinor.fi>
2020-11-03 11:47:13 +0200

    EVAKA-3968 Added list pages for coming, present, departed and absent children

---
+++ frontend/packages/employee-frontend/src/components/attendances/AttendanceAbsentPage.tsx
@@ -0,0 +1,31 @@
+// SPDX-FileCopyrightText: 2017-2020 City of Espoo
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+import React, { Fragment } from 'react'
+
+import { isFailure, isLoading, Result } from '~api'
+import { ChildInGroup } from '~api/unit'
+import Loader from '~components/shared/atoms/Loader'
+import { useTranslation } from '~state/i18n'
+import AttendanceList from './AttendanceList'
+
+interface Props {
+  groupAttendances: Result<ChildInGroup[]>
+}
+
+export default React.memo(function AttendanceAbsentPage({
+  groupAttendances
+}: Props) {
+  const { i18n } = useTranslation()
+
+  return (
+    <Fragment>
+      {isFailure(groupAttendances) && <div>{i18n.common.loadingFailed}</div>}
+      {isLoading(groupAttendances) ? (
+        <Loader />
+      ) : (
+        <AttendanceList groupAttendances={groupAttendances} type={'ABSENT'} />
+      )}
+    </Fragment>
+  )
+})
