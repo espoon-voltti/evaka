8216e3f2e32edca9b9656754aad2758d4f5c47a7
Joonas Javanainen <joonas.javanainen@reaktor.com>
2020-11-05 12:00:25 +0200

    Remove unused weekend indicator

--- frontend/packages/employee-frontend/src/components/absences/ColorInfo.tsx
+++ frontend/packages/employee-frontend/src/components/absences/ColorInfo.tsx
@@ -5,20 +5,14 @@
 import React from 'react'
 import './ColorInfo.scss'
 import { AbsenceTypes } from '~types/absence'
-import { useTranslation } from '~state/i18n'
 import ColourInfoItem from '~components/common/ColourInfoItem'
 
 const ColorInfo = () => {
-  const { i18n } = useTranslation()
   return (
     <div className={'color-info'}>
       {AbsenceTypes.concat('TEMPORARY_RELOCATION').map((absenceType, index) => (
         <ColourInfoItem type={absenceType} key={index} />
       ))}
-      <div className={'color-info-container'}>
-        <div className={`info-ball info-ball-weekend`} />
-        <div>{i18n.absences.weekend}</div>
-      </div>
     </div>
   )
 }
