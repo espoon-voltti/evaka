b4cb7dbdfed8e0a04c6aa59dc16fcfb33edb1294
Joosa Kurvinen <joosa.kurvinen@gofore.com>
2020-11-24 14:58:28 +0200

    Implement first version of pairing endpoints and queries

---
+++ service/src/main/kotlin/fi/espoo/evaka/pairing/PairingQueries.kt
@@ -0,0 +1,121 @@
+package fi.espoo.evaka.pairing
+
+import fi.espoo.evaka.shared.db.Database
+import fi.espoo.evaka.shared.domain.NotFound
+import fi.espoo.evaka.shared.utils.zoneId
+import org.jdbi.v3.core.kotlin.mapTo
+import java.time.ZonedDateTime
+import java.util.UUID
+
+const val maxAttempts = 10
+
+fun initPairing(tx: Database.Transaction, unitId: UUID): Pairing {
+    // language=sql
+    val sql =
+        """
+            INSERT INTO pairing (unit_id, expires, challenge_key) 
+            VALUES (:unitId, :expires, :challenge)
+            RETURNING *
+        """.trimIndent()
+
+    return tx.createQuery(sql)
+        .bind("unitId", unitId)
+        .bind("expires", ZonedDateTime.now(zoneId).plusMinutes(15).toInstant())
+        .bind("challenge", generatePairingKey())
+        .mapTo<Pairing>()
+        .first()
+}
+
+fun challengePairing(tx: Database.Transaction, challengeKey: String): Pairing {
+    // language=sql
+    val sql =
+        """
+            UPDATE pairing SET response_key = :response, status = 'WAITING_RESPONSE'
+            WHERE challenge_key = :challenge AND status = 'WAITING_CHALLENGE' AND expires > :now AND attempts <= :maxAttempts
+            RETURNING *
+        """.trimIndent()
+
+    return tx.createQuery(sql)
+        .bind("challenge", challengeKey)
+        .bind("response", generatePairingKey())
+        .bind("now", ZonedDateTime.now(zoneId).toInstant())
+        .bind("maxAttempts", maxAttempts)
+        .mapTo<Pairing>()
+        .list()
+        .firstOrNull() ?: throw NotFound("Valid pairing not found")
+}
+
+fun respondPairingChallenge(tx: Database.Transaction, id: UUID, challengeKey: String, responseKey: String): Pairing {
+    // language=sql
+    val sql =
+        """
+            UPDATE pairing SET status = 'READY'
+            WHERE id = :id AND challenge_key = :challenge AND status = 'WAITING_RESPONSE' AND expires > :now AND attempts <= :maxAttempts
+            RETURNING *
+        """.trimIndent()
+
+    return tx.createQuery(sql)
+        .bind("id", id)
+        .bind("challenge", challengeKey)
+        .bind("response", responseKey)
+        .bind("now", ZonedDateTime.now(zoneId).toInstant())
+        .bind("maxAttempts", maxAttempts)
+        .mapTo<Pairing>()
+        .list()
+        .firstOrNull() ?: throw NotFound("Valid pairing not found")
+}
+
+fun validatePairing(tx: Database.Read, id: UUID, challengeKey: String, responseKey: String) {
+    // language=sql
+    val sql =
+        """
+            SELECT 1 FROM pairing
+            WHERE id = :id AND challenge_key = :challenge AND response_key = :response AND status = 'READY' AND expires > :now AND attempts <= :maxAttempts
+        """.trimIndent()
+
+    tx.createQuery(sql)
+        .bind("id", id)
+        .bind("challenge", challengeKey)
+        .bind("response", responseKey)
+        .bind("now", ZonedDateTime.now(zoneId).toInstant())
+        .bind("maxAttempts", maxAttempts)
+        .mapTo<Int>()
+        .list()
+        .firstOrNull() ?: throw NotFound("Valid pairing not found")
+}
+
+fun fetchPairingStatus(tx: Database.Read, id: UUID): PairingStatus {
+    // language=sql
+    val sql =
+        """
+            SELECT status FROM pairing
+            WHERE id = :id AND expires > :now AND attempts <= :maxAttempts
+        """.trimIndent()
+
+    return tx.createQuery(sql)
+        .bind("id", id)
+        .bind("now", ZonedDateTime.now(zoneId).toInstant())
+        .bind("maxAttempts", maxAttempts)
+        .mapTo<PairingStatus>()
+        .list()
+        .firstOrNull() ?: throw NotFound("Valid pairing not found")
+}
+
+fun incrementAttempts(tx: Database.Transaction, id: UUID, challengeKey: String) {
+    // language=sql
+    val sql =
+        """
+            UPDATE pairing
+            SET attempts = attempts + 1
+            WHERE id = :id OR challenge_key = :challenge
+        """.trimIndent()
+
+    tx.createUpdate(sql)
+        .bind("id", id)
+        .bind("challenge", challengeKey)
+        .execute()
+}
+
+fun generatePairingKey(): String {
+    return "hello" // todo
+}
---
+++ service/src/main/kotlin/fi/espoo/evaka/pairing/PairingsController.kt
@@ -0,0 +1,137 @@
+package fi.espoo.evaka.pairing
+
+import fi.espoo.evaka.shared.auth.AccessControlList
+import fi.espoo.evaka.shared.auth.AuthenticatedUser
+import fi.espoo.evaka.shared.auth.UserRole
+import fi.espoo.evaka.shared.db.Database
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
+
+@RestController
+class PairingsController(
+    private val acl: AccessControlList
+) {
+    /**
+     * Unit supervisor calls this endpoint as an authorized desktop user to start a new pairing process.
+     *
+     * Endpoint returns a Pairing with an id and challengeKey.
+     *
+     * Pairing status is WAITING_CHALLENGE.
+     */
+    data class PostPairingReq(
+        val unitId: UUID
+    )
+    @PostMapping("/pairings")
+    fun postPairing(
+        db: Database.Connection,
+        user: AuthenticatedUser,
+        @RequestBody body: PostPairingReq
+    ): ResponseEntity<Pairing> {
+        acl.getRolesForUnit(user, body.unitId).requireOneOfRoles(UserRole.ADMIN, UserRole.UNIT_SUPERVISOR)
+
+        return db
+            .transaction { tx -> initPairing(tx, body.unitId) }
+            .let { ResponseEntity.ok(it) }
+    }
+
+    /**
+     * Unit supervisor calls this endpoint as an unauthenticated mobile user to start a pairing challenge phase.
+     *
+     * Endpoint takes in the challengeKey previously shown on desktop and returns a Pairing with an id,
+     * challengeKey and responseKey.
+     *
+     * Pairing status changes from WAITING_CHALLENGE to WAITING_RESPONSE.
+     */
+    data class PostPairingChallengeReq(
+        val challengeKey: String
+    )
+    @PostMapping("/pairings/challenge")
+    fun postPairingChallenge(
+        db: Database.Connection,
+        @RequestBody body: PostPairingChallengeReq
+    ): ResponseEntity<Pairing> {
+        return db
+            .transaction { tx -> challengePairing(tx, body.challengeKey) }
+            .let { ResponseEntity.ok(it) }
+    }
+
+    /**
+     * Unit supervisor calls this endpoint as an authorized desktop user to respond to challenge.
+     *
+     * Endpoint takes in the previously received id and challengeKey, as well as the responseKey shown on mobile.
+     *
+     * Pairing status changes from WAITING_RESPONSE to READY.
+     */
+    data class PostPairingResponseReq(
+        val challengeKey: String,
+        val responseKey: String
+    )
+    @PostMapping("/pairings/:id/response")
+    fun postPairingResponse(
+        db: Database.Connection,
+        user: AuthenticatedUser,
+        @PathVariable id: UUID,
+        @RequestBody body: PostPairingResponseReq
+    ): ResponseEntity<Pairing> {
+        acl.getRolesForPairing(user, id).requireOneOfRoles(UserRole.ADMIN, UserRole.UNIT_SUPERVISOR)
+        db.transaction { tx -> incrementAttempts(tx, id, body.challengeKey) }
+
+        return db
+            .transaction { tx -> respondPairingChallenge(tx, id, body.challengeKey, body.responseKey) }
+            .let { ResponseEntity.ok(it) }
+    }
+
+    /**
+     * API Gateway calls this endpoint to authorize a session creation for a mobile device.
+     *
+     * Endpoint takes in the previously received id, challengeKey and responseKey.
+     *
+     * Pairing status changes from WAITING_RESPONSE to READY.
+     */
+    data class PostPairingValidationReq(
+        val challengeKey: String,
+        val responseKey: String
+    )
+    @PostMapping("/pairings/:id/validation")
+    fun postPairingValidation(
+        db: Database.Connection,
+        user: AuthenticatedUser,
+        @PathVariable id: UUID,
+        @RequestBody body: PostPairingValidationReq
+    ): ResponseEntity<Unit> {
+        acl.getRolesForPairing(user, id).requireOneOfRoles(UserRole.ADMIN, UserRole.UNIT_SUPERVISOR)
+        db.transaction { tx -> incrementAttempts(tx, id, body.challengeKey) }
+
+        db.transaction { tx ->
+            validatePairing(tx, id, body.challengeKey, body.responseKey)
+
+            // todo: insert into mobile_device and employee ?
+        }
+
+        return ResponseEntity.noContent().build()
+    }
+
+    /**
+     * Unit supervisor calls this endpoint in a polling fashion either as an authorized desktop user or
+     * as an unauthenticated mobile user in order to monitor the progress of the async pairing process.
+     *
+     * Endpoint takes in the previously received pairing id and returns the pairing status.
+     */
+    data class PairingStatusRes(
+        val status: PairingStatus
+    )
+    @GetMapping("/pairings/:id/status")
+    fun getPairingStatus(
+        db: Database.Connection,
+        @PathVariable id: UUID
+    ): ResponseEntity<PairingStatusRes> {
+        return db
+            .transaction { tx -> fetchPairingStatus(tx, id) }
+            .let { ResponseEntity.ok(PairingStatusRes(status = it)) }
+    }
+}
