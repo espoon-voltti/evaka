68f5d47d43c711f5618cb4bbb1215473786264a2
Mikko Piuhola <me@mikkopiuhola.fi>
2020-11-25 16:38:47 +0200

    Document bulk-licensing, add missing licenses

--- frontend/packages/employee-frontend/src/components/shared/atoms/Loader.tsx
+++ frontend/packages/employee-frontend/src/components/shared/atoms/Loader.tsx
@@ -1,8 +1,10 @@
-{/*
+{
+  /*
 SPDX-FileCopyrightText: 2017-2020 City of Espoo
 
 SPDX-License-Identifier: LGPL-2.1-or-later
-*/}
+*/
+}
 
 import React from 'react'
 import styled, { keyframes } from 'styled-components'
--- frontend/packages/employee-frontend/src/components/voucher-value-decision/VoucherValueDecisionActionBar.tsx
+++ frontend/packages/employee-frontend/src/components/voucher-value-decision/VoucherValueDecisionActionBar.tsx
@@ -1,3 +1,11 @@
+{
+  /*
+SPDX-FileCopyrightText: 2017-2020 City of Espoo
+
+SPDX-License-Identifier: LGPL-2.1-or-later
+*/
+}
+
 import React from 'react'
 import { Container } from '~components/shared/layout/Container'
 import { FixedSpaceRow } from '~components/shared/layout/flex-helpers'
