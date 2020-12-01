d9e1c966c609d7ca9dad0626afb237950c80c332
Matias Heikkilä <matias@three.consulting>
2020-12-01 12:32:46 +0200

    Grant child ACL roles in the case of a backup care

--- service/src/main/resources/db/migration/R__child_acl_view.sql
+++ service/src/main/resources/db/migration/R__child_acl_view.sql
@@ -14,5 +14,15 @@ FROM person ch
     LEFT JOIN application a ON a.child_id = ch.id AND a.status = ANY ('{SENT,WAITING_PLACEMENT,WAITING_CONFIRMATION,WAITING_DECISION,WAITING_MAILING,WAITING_UNIT_CONFIRMATION, ACTIVE}'::application_status_type[])
     LEFT JOIN placement_plan pp ON pp.application_id = a.id
     JOIN daycare_acl acl ON acl.daycare_id = pl.unit_id OR acl.daycare_id = pp.unit_id
+UNION SELECT
+    acl.employee_id AS "employee_id",
+    ch.id AS "child_id",
+    CASE
+        WHEN acl.role = 'UNIT_SUPERVISOR'::public.user_role THEN 'UNIT_SUPERVISOR'::public.user_role
+        ELSE 'STAFF'::public.user_role
+    END AS "role"
+FROM person ch
+    LEFT JOIN backup_care bc ON bc.child_id = ch.id AND bc.end_date > current_date - INTERVAL '1 month'
+    JOIN daycare_acl acl ON acl.daycare_id = bc.unit_id
 ORDER BY
     role;
