b48a405e5784668832dc7a3db0571ccebbcda03b
Joonas Reynders <joonas.reynders@reaktor.fi>
2020-12-18 10:05:15 +0200

    E2E test for report showing non placed children

--- frontend/e2e-test/test/e2e/specs/5_employee/placement-sketching-report.spec.ts
+++ frontend/e2e-test/test/e2e/specs/5_employee/placement-sketching-report.spec.ts
@@ -14,7 +14,6 @@ import {
 } from '../../dev-api'
 import {
   applicationFixture,
-  applicationFixtureId,
   createDaycarePlacementFixture,
   daycareFixture,
   uuidv4
@@ -34,6 +33,8 @@ let cleanUp: () => Promise<void>
 const home = new Home()
 const reports = new ReportsPage()
 
+let applicationId: string | null = null
+
 fixture('Placement sketching report')
   .meta({ type: 'regression', subType: 'reports' })
   .page(home.homePage('admin'))
@@ -42,13 +43,52 @@ fixture('Placement sketching report')
   })
   .afterEach(logConsoleMessages)
   .afterEach(async () => {
-    await deleteApplication(applicationFixtureId)
+    if (applicationId) await deleteApplication(applicationId)
+    applicationId = null
   })
   .after(async () => {
     await cleanUp()
   })
 
-// TODO lisää 2. 3. vaihtoehto, linkki hakemukselle, e2e
+test('Not placed child shows on report', async (t) => {
+  const now = new Date()
+
+  const fixture = applicationFixture(
+    fixtures.enduserChildFixtureJari,
+    fixtures.enduserGuardianFixture,
+    undefined,
+    'preschool'
+  )
+
+  const preferredStartDate = new Date(now.getFullYear(), 7, 13)
+  applicationId = uuidv4()
+
+  const createdApplication = {
+    ...fixture,
+    form: {
+      ...fixture.form,
+      preferredStartDate: preferredStartDate.toISOString(),
+      otherGuardianAgreementStatus: 'AGREED' as OtherGuardianAgreementStatus
+    },
+    sentDate: format(sub(preferredStartDate, { months: 4 }), 'yyyy-MM-dd'),
+    status: 'SENT' as ApplicationStatus,
+    id: applicationId
+  }
+
+  await insertApplications([createdApplication])
+
+  const preferredUnit = daycareFixture
+
+  await t.useRole(seppoAdminRole)
+  await reports.selectReportsTab()
+  await reports.selectPlacementSketchingReport()
+  await reports.assertPlacementSketchingRow(
+    preferredUnit.id,
+    createdApplication.childId,
+    preferredUnit.name,
+    `${fixtures.enduserChildFixtureJari.lastName} ${fixtures.enduserChildFixtureJari.firstName}`
+  )
+})
 
 test('Placed child shows on report', async (t) => {
   const now = new Date()
@@ -61,6 +101,7 @@ test('Placed child shows on report', async (t) => {
   )
 
   const preferredStartDate = new Date(now.getFullYear(), 7, 13)
+  applicationId = uuidv4()
 
   const createdApplication = {
     ...fixture,
@@ -71,7 +112,7 @@ test('Placed child shows on report', async (t) => {
     },
     sentDate: format(sub(preferredStartDate, { months: 4 }), 'yyyy-MM-dd'),
     status: 'SENT' as ApplicationStatus,
-    id: uuidv4()
+    id: applicationId
   }
 
   await insertApplications([createdApplication])
