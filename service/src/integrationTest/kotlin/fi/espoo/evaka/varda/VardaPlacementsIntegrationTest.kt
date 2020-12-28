0630c263de8c6bb3eec411acf43f751586b681fd
Joonas Reynders <joonas.reynders@reaktor.fi>
2020-12-28 08:54:41 +0200

    Fix test

--- service/src/integrationTest/kotlin/fi/espoo/evaka/varda/VardaPlacementsIntegrationTest.kt
+++ service/src/integrationTest/kotlin/fi/espoo/evaka/varda/VardaPlacementsIntegrationTest.kt
@@ -345,7 +345,7 @@ class VardaPlacementsIntegrationTest : FullApplicationTest() {
 
     @Test
     fun `a daycare placement's end date is at most unit's closing date`() {
-        val period = ClosedPeriod(LocalDate.of(2019, 8, 1), LocalDate.of(2020, 7, 31))
+        val period = FiniteDateRange(LocalDate.of(2019, 8, 1), LocalDate.of(2020, 7, 31))
         insertVardaUnit(db)
         val placementId = insertPlacement(db, testChild_1.id, period, unitId = testDaycare.id)
         insertTestVardaDecision(db, placementId = placementId)
@@ -367,7 +367,7 @@ class VardaPlacementsIntegrationTest : FullApplicationTest() {
 
     @Test
     fun `a daycare placement is updated when placement unit's closing date is updated`() {
-        val period = ClosedPeriod(LocalDate.of(2019, 8, 1), LocalDate.of(2020, 7, 31))
+        val period = FiniteDateRange(LocalDate.of(2019, 8, 1), LocalDate.of(2020, 7, 31))
         insertVardaUnit(db)
         val placementId = insertPlacement(db, testChild_1.id, period, unitId = testDaycare.id)
         insertTestVardaDecision(db, placementId = placementId)
