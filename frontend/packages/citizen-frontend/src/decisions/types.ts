b182e2f1959dca3934e729130cf4d0511f0bc73e
Joonas Reynders <joonas.reynders@reaktor.fi>
2021-01-04 14:16:50 +0200

    Base for citizen application UI and endpoint

---
+++ frontend/packages/citizen-frontend/src/decisions/types.ts
@@ -0,0 +1,4 @@
+export type DecisionType =
+  | 'CLUB'
+  | 'DAYCARE'
+  | 'PRESCHOOL'
---
+++ service/src/main/kotlin/fi/espoo/evaka/application/ApplicationControllerCitizen.kt
@@ -0,0 +1,9 @@
+package fi.espoo.evaka.application
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/citizen")
+class ApplicationControllerCitizen() {
+}
