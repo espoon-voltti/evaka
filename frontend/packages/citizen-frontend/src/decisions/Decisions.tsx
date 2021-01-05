c8479fc3dba9b489c5f86edb13059183b54f6129
Aleksi Ahonen <aleksi.ahonen@reaktor.com>
2021-01-05 14:00:56 +0200

    Use gaps instead of margins on titles

--- frontend/packages/citizen-frontend/src/decisions/Decisions.tsx
+++ frontend/packages/citizen-frontend/src/decisions/Decisions.tsx
@@ -12,7 +12,6 @@ import Container, {
   ContentArea
 } from '@evaka/lib-components/src/layout/Container'
 import { H1, H2, H3, Label, P } from '@evaka/lib-components/src/typography'
-import { FixedSpaceColumn } from '@evaka/lib-components/src/layout/flex-helpers'
 import ListGrid from '@evaka/lib-components/src/layout/ListGrid'
 import { AlertBox } from '@evaka/lib-components/src/molecules/MessageBoxes'
 import { Gap } from '@evaka/lib-components/src/white-space'
@@ -55,34 +54,32 @@ export default React.memo(function Decisions() {
   return (
     <Container>
       <Gap size="s" />
-      <FixedSpaceColumn>
-        <ContentArea opaque>
-          <H1>{t.decisions.title}</H1>
-          <P
-            width="800px"
-            dangerouslySetInnerHTML={{ __html: t.decisions.summary }}
-          />
-          {unconfirmedDecisionsCount > 0 ? (
-            <>
-              <Gap size="s" />
-              <AlertBox
-                message={t.decisions.unconfimedDecisions(
-                  applicationDecisions.length
-                )}
-                thin
-                data-qa="alert-box-unconfirmed-decisions-count"
-              />
-            </>
-          ) : null}
-        </ContentArea>
-
-        {applicationDecisions.map((applicationDecision) => (
-          <ApplicationDecisions
-            key={applicationDecision.applicationId}
-            {...applicationDecision}
-          />
-        ))}
-      </FixedSpaceColumn>
+      <ContentArea opaque paddingVertical="L">
+        <H1 noMargin>{t.decisions.title}</H1>
+        <P
+          width="800px"
+          dangerouslySetInnerHTML={{ __html: t.decisions.summary }}
+        />
+        {unconfirmedDecisionsCount > 0 ? (
+          <>
+            <Gap size="s" />
+            <AlertBox
+              message={t.decisions.unconfimedDecisions(
+                applicationDecisions.length
+              )}
+              thin
+              data-qa="alert-box-unconfirmed-decisions-count"
+            />
+          </>
+        ) : null}
+      </ContentArea>
+      <Gap size="s" />
+      {applicationDecisions.map((applicationDecision) => (
+        <React.Fragment key={applicationDecision.applicationId}>
+          <ApplicationDecisions {...applicationDecision} />
+          <Gap size="s" />
+        </React.Fragment>
+      ))}
     </Container>
   )
 })
@@ -95,13 +92,17 @@ const ApplicationDecisions = React.memo(function ApplicationDecisions({
   const t = useTranslation()
 
   return (
-    <ContentArea opaque>
-      <H2 data-qa="title-decision-child-name">{childName}</H2>
+    <ContentArea opaque paddingVertical="L">
+      <H2 noMargin data-qa="title-decision-child-name">
+        {childName}
+      </H2>
       {decisions.map(({ decisionId, type, status, sentDate, resolved }) => (
         <React.Fragment key={decisionId}>
-          <H3 data-qa="title-decision-type">
+          <Gap size="L" />
+          <H3 noMargin data-qa="title-decision-type">
             {`${t.decisions.applicationDecisions.decision} ${t.decisions.applicationDecisions.type[type]}`}
           </H3>
+          <Gap size="m" />
           <ListGrid labelWidth="max-content" rowGap="s" columnGap="L">
             <Label>{t.decisions.applicationDecisions.sentDate}</Label>
             <span data-qa="decision-sent-date">{sentDate.format()}</span>
