e288b426cedde8335555ba96fb343dd564549f51
Joonas Reynders <joonas.reynders@reaktor.fi>
2021-01-07 09:27:43 +0200

    Use null UUID placeholder

--- frontend/e2e-test/test/e2e/dev-api/fixtures.ts
+++ frontend/e2e-test/test/e2e/dev-api/fixtures.ts
@@ -704,7 +704,7 @@ export function createBackupCareFixture(
   }
 }
 
-export const nullUUID = ''
+export const nullUUID = '00000000-0000-0000-0000-000000000000'
 
 export const uuidv4 = (): string => {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
