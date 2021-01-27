563d3b0de3c325ff4b4e576aa91fb1d4e88b4c43
Aleksi Ahonen <aleksi.ahonen@reaktor.com>
2021-01-27 12:58:02 +0200

    Fix absence colors

--- frontend/packages/lib-components/src/colors.ts
+++ frontend/packages/lib-components/src/colors.ts
@@ -48,32 +48,6 @@ const colors = {
 }
 
 export const absenceColours = {
-  UNKNOWN_ABSENCE: colors.greyscale.darkest,
-  OTHER_ABSENCE: colors.greyscale.white,
-  SICKLEAVE: colors.greyscale.white,
-  PLANNED_ABSENCE: colors.greyscale.darkest,
-  PARENTLEAVE: colors.greyscale.white,
-  FORCE_MAJEURE: colors.greyscale.white,
-  TEMPORARY_RELOCATION: colors.greyscale.white,
-  TEMPORARY_VISITOR: colors.greyscale.white,
-  PRESENCE: colors.greyscale.white
-}
-
-type AbsenceType = keyof typeof absenceColours
-
-export const absenceBackgroundColours: { [k in AbsenceType]: string } = {
-  UNKNOWN_ABSENCE: colors.accents.green,
-  OTHER_ABSENCE: colors.blues.dark,
-  SICKLEAVE: colors.accents.violet,
-  PLANNED_ABSENCE: colors.blues.light,
-  PARENTLEAVE: colors.blues.primary,
-  FORCE_MAJEURE: colors.accents.red,
-  TEMPORARY_RELOCATION: colors.accents.orange,
-  TEMPORARY_VISITOR: colors.accents.yellow,
-  PRESENCE: colors.greyscale.white
-}
-
-export const absenceBorderColours: { [k in AbsenceType]: string } = {
   UNKNOWN_ABSENCE: colors.accents.green,
   OTHER_ABSENCE: colors.blues.dark,
   SICKLEAVE: colors.accents.violet,
