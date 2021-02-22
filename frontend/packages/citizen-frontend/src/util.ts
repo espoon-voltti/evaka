558991ea8297ac06637af02046f6e1ea52be06c8
Matias Heikkilä <matias@three.consulting>
2021-02-22 10:47:05 +0200

    Debounce digitransit autocomplete requests

--- frontend/packages/citizen-frontend/src/util.ts
+++ frontend/packages/citizen-frontend/src/util.ts
@@ -3,6 +3,7 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
 import { format } from 'date-fns'
+import { useState, useEffect } from 'react'
 
 export const DATE_FORMAT_DATE_DEFAULT = 'dd.MM.yyyy'
 export const DATE_FORMAT_DATE_TIME_DEFAULT = 'dd.MM.yyyy HH:mm'
@@ -12,4 +13,20 @@ export function formatDate(
   dateFormat = DATE_FORMAT_DATE_TIME_DEFAULT
 ): string {
   return date ? format(date, dateFormat) : ''
+}
+
+export const useDebounce = <T>(value: T, delay: number): T => {
+  const [debouncedValue, setDebouncedValue] = useState(value)
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value)
+    }, delay)
+
+    return () => {
+      clearTimeout(handler)
+    }
+  }, [value])
+
+  return debouncedValue
 }
