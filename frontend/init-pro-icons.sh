b9bf052d33b4bba590bdc13465c0c7fe66912240
Joonas Javanainen <joonas.javanainen@reaktor.com>
2021-02-23 18:02:04 +0200

    Use separate init and unpack scripts
    Our CircleCI configuration persists only vendor dir after fetching the
    private dependencies

--- frontend/init-pro-icons.sh
+++ frontend/init-pro-icons.sh
@@ -7,13 +7,3 @@
 VERSION=5.14.0
 
 aws --profile voltti-local s3 sync s3://evaka-deployment-local/frontend/vendor/fortawesome/ ./vendor/fortawesome/
-
-unpack() {
-  NAME=${1}
-  mkdir -p node_modules/@fortawesome/"${NAME}"
-  tar xz --strip-components 1 -C node_modules/@fortawesome/"${NAME}" -f vendor/fortawesome/fortawesome-"${NAME}"-"${VERSION}".tgz
-}
-
-unpack pro-light-svg-icons
-unpack pro-regular-svg-icons
-unpack pro-solid-svg-icons
---
+++ frontend/unpack-pro-icons.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -eu
+
+# SPDX-FileCopyrightText: 2017-2020 City of Espoo
+#
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+VERSION=5.14.0
+
+unpack() {
+  NAME=${1}
+  mkdir -p node_modules/@fortawesome/"${NAME}"
+  tar xz --strip-components 1 -C node_modules/@fortawesome/"${NAME}" -f vendor/fortawesome/fortawesome-"${NAME}"-"${VERSION}".tgz
+}
+
+unpack pro-light-svg-icons
+unpack pro-regular-svg-icons
+unpack pro-solid-svg-icons
