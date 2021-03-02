8ce6c16ea402e402ba15bb33a6d3eb098bbbe009
Mikko Piuhola <me@mikkopiuhola.fi>
2021-03-02 17:29:53 +0200

    Fix blob download for Safari and Testcafe, hide link
    - Safari and Testcafe (probably due to the testcafe-hammerhead proxy but didn't find out for sure) consider all `target="_blank"` anchors to be pop-ups which is unnecessary when (ab)using them for blob downloads -> only set the `target` attribute if the HTML5 `download` attribute is supported
        - This feature also makes Testcafe tests unnecessarily annoying with its new multi-window support (would need to add custom logic to all file download tests to close the pop-ups)

--- frontend/packages/lib-common/src/utils/file.ts
+++ frontend/packages/lib-common/src/utils/file.ts
@@ -5,10 +5,18 @@
 export const downloadBlobAsFile = (fileName: string, blobPart: BlobPart) => {
   const url = URL.createObjectURL(new Blob([blobPart]))
   const link = document.createElement('a')
+  link.style.display = 'none'
   link.href = url
-  link.target = '_blank'
   link.setAttribute('download', fileName)
+  // Safari and Testcafe think _blank anchors are pop-ups.
+  // We only want to set _blank target if the browser does not support the HTML5
+  // download attribute. This allows you to download files in desktop safari if
+  // pop up blocking is enabled.
+  if (typeof link.download === 'undefined') {
+    link.target = '_blank'
+  }
   link.rel = 'noreferrer'
+
   document.body.appendChild(link)
   link.click()
   document.body.removeChild(link)
