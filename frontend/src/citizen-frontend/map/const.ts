f733f2facc2307c31699ffecabae96850a1e5bf4
Mikko Piuhola <me@mikkopiuhola.fi>
2021-03-03 15:36:27 +0200

    Add all missing license headers
    - Except for the eVaka logo, still figuring out whether that should be left unlicensed or not

--- frontend/src/citizen-frontend/map/const.ts
+++ frontend/src/citizen-frontend/map/const.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2017-2021 City of Espoo
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
 export type MobileMode = 'list' | 'map'
 
 export const mapViewBreakpoint = '900px'
