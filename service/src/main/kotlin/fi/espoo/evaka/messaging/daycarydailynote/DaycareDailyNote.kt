e34acd83c0d5620f0189cb750ba85aa90817b624
Panu Klemola <panu.klemola@solinor.fi>
2021-03-04 11:57:27 +0200

    EVAKA-4030 Lint fix again

--- service/src/main/kotlin/fi/espoo/evaka/messaging/daycarydailynote/DaycareDailyNote.kt
+++ service/src/main/kotlin/fi/espoo/evaka/messaging/daycarydailynote/DaycareDailyNote.kt
@@ -5,18 +5,18 @@ import java.time.LocalDate
 import java.util.UUID
 
 data class DaycareDailyNote(
-        val id: UUID,
-        val childId: UUID?,
-        val groupId: UUID?,
-        val date: LocalDate,
-        val note: String?,
-        val feedingNote: DaycareDailyNoteLevelInfo?,
-        val sleepingNote: DaycareDailyNoteLevelInfo?,
-        val sleepingHours: Double?,
-        val reminders: List<DaycareDailyNoteReminder> = emptyList(),
-        val reminderNote: String?,
-        val modifiedAt: Instant? = null,
-        val modifiedBy: String? = "evaka"
+    val id: UUID,
+    val childId: UUID?,
+    val groupId: UUID?,
+    val date: LocalDate,
+    val note: String?,
+    val feedingNote: DaycareDailyNoteLevelInfo?,
+    val sleepingNote: DaycareDailyNoteLevelInfo?,
+    val sleepingHours: Double?,
+    val reminders: List<DaycareDailyNoteReminder> = emptyList(),
+    val reminderNote: String?,
+    val modifiedAt: Instant? = null,
+    val modifiedBy: String? = "evaka"
 )
 
 enum class DaycareDailyNoteLevelInfo {
