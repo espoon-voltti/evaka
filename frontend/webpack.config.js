43165327b66c95157be97cfbe0a7c4b097d69636
Joonas Javanainen <joonas.javanainen@reaktor.com>
2021-03-22 09:41:36 +0200

    Compile using ES2020 by default, use babel-loader for browsers
    Using ES6 everywhere has significant downsides, and it's not possible to
    override the target using just the TS compiler when there's multiple
    projects.
    
    Instead, use babel-loader to transpile ES2020 code at bundling time
    using the browserslist config in package.json.

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -100,14 +100,22 @@ function baseConfig({ isDevelopment, isDevServer }, { name, publicPath }) {
         {
           test: /\.(ts|tsx|json)$/,
           exclude: /[\\/]node_modules[\\/]/,
-          use: {
-            loader: 'ts-loader',
-            options: {
-              onlyCompileBundledFiles: true,
-              projectReferences: true,
-              transpileOnly: isDevServer
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            },
+            {
+              loader: 'ts-loader',
+              options: {
+                onlyCompileBundledFiles: true,
+                projectReferences: true,
+                transpileOnly: isDevServer
+              }
             }
-          }
+          ]
         },
         // All CSS
         {
