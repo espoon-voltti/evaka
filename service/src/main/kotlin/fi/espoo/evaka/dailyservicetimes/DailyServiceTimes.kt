504371eda9ab2b554f32d257a21d446e9a08d92c
Joosa Kurvinen <joosa.kurvinen@gofore.com>
2021-04-06 17:33:48 +0300

    PUT and DELETE endpoints

--- service/src/main/kotlin/fi/espoo/evaka/dailyservicetimes/DailyServiceTimes.kt
+++ service/src/main/kotlin/fi/espoo/evaka/dailyservicetimes/DailyServiceTimes.kt
@@ -1,5 +1,6 @@
 package fi.espoo.evaka.dailyservicetimes
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo
 import fi.espoo.evaka.shared.db.Database
 import java.time.LocalTime
 import java.util.UUID
@@ -14,6 +15,7 @@ data class TimeRange(
     )
 }
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.DEDUCTION, property = "regular")
 sealed class DailyServiceTimes(
     val regular: Boolean
 ) {
@@ -71,4 +73,125 @@ fun Database.Read.getChildDailyServiceTimes(childId: UUID): DailyServiceTimes? {
             }
         }
         .firstOrNull()
+}
+
+fun Database.Transaction.upsertChildDailyServiceTimes(childId: UUID, times: DailyServiceTimes) {
+    when (times) {
+        is DailyServiceTimes.RegularTimes -> upsertRegularChildDailyServiceTimes(childId, times)
+        is DailyServiceTimes.IrregularTimes -> upsertIrregularChildDailyServiceTimes(childId, times)
+    }
+}
+
+private fun Database.Transaction.upsertRegularChildDailyServiceTimes(childId: UUID, times: DailyServiceTimes.RegularTimes) {
+    // language=sql
+    val sql = """
+        INSERT INTO daily_service_time (
+            child_id, regular, 
+            regular_start, regular_end, 
+            monday_start, monday_end, 
+            tuesday_start, tuesday_end,
+            wednesday_start, wednesday_end, 
+            thursday_start, thursday_end, 
+            friday_start, friday_end
+        )
+        VALUES (
+            :childId, TRUE, 
+            :regularStart, :regularEnd, 
+            NULL, NULL,
+            NULL, NULL,
+            NULL, NULL,
+            NULL, NULL,
+            NULL, NULL
+        )
+        
+        ON CONFLICT (child_id) DO UPDATE
+        
+        SET 
+            regular = TRUE,
+            regular_start = :regularStart,
+            regular_end = :regularEnd, 
+            monday_start = NULL,
+            monday_end = NULL, 
+            tuesday_start = NULL, 
+            tuesday_end = NULL,
+            wednesday_start = NULL,
+            wednesday_end = NULL, 
+            thursday_start = NULL, 
+            thursday_end = NULL, 
+            friday_start = NULL, 
+            friday_end = NULL
+            
+    """.trimIndent()
+
+    this.createUpdate(sql)
+        .bind("childId", childId)
+        .bind("regularStart", times.regularTimes.start)
+        .bind("regularEnd", times.regularTimes.end)
+        .execute()
+}
+
+private fun Database.Transaction.upsertIrregularChildDailyServiceTimes(childId: UUID, times: DailyServiceTimes.IrregularTimes) {
+    // language=sql
+    val sql = """
+        INSERT INTO daily_service_time (
+            child_id, regular, 
+            regular_start, regular_end, 
+            monday_start, monday_end, 
+            tuesday_start, tuesday_end,
+            wednesday_start, wednesday_end, 
+            thursday_start, thursday_end, 
+            friday_start, friday_end
+        )
+        VALUES (
+            :childId, FALSE, 
+            NULL, NULL,
+            :mondayStart, :mondayEnd, 
+            :tuesdayStart, :tuesdayEnd,
+            :wednesdayStart, :wednesdayEnd, 
+            :thursdayStart, :thursdayEnd, 
+            :fridayStart, :fridayEnd
+        )
+        
+        ON CONFLICT (child_id) DO UPDATE
+        
+        SET 
+            regular = FALSE,
+            regular_start = NULL,
+            regular_end = NULL, 
+            monday_start = :mondayStart,
+            monday_end = :mondayEnd, 
+            tuesday_start = :tuesdayStart, 
+            tuesday_end = :tuesdayEnd,
+            wednesday_start = :wednesdayStart,
+            wednesday_end = :wednesdayEnd, 
+            thursday_start = :thursdayStart, 
+            thursday_end = :thursdayEnd, 
+            friday_start = :fridayStart, 
+            friday_end = :fridayEnd
+            
+    """.trimIndent()
+
+    this.createUpdate(sql)
+        .bind("childId", childId)
+        .bind("mondayStart", times.monday?.start)
+        .bind("mondayEnd", times.monday?.end)
+        .bind("tuesdayStart", times.tuesday?.start)
+        .bind("tuesdayEnd", times.tuesday?.end)
+        .bind("wednesdayStart", times.wednesday?.start)
+        .bind("wednesdayEnd", times.wednesday?.end)
+        .bind("thursdayStart", times.thursday?.start)
+        .bind("thursdayEnd", times.thursday?.end)
+        .bind("fridayStart", times.friday?.start)
+        .bind("fridayEnd", times.friday?.end)
+        .execute()
+}
+
+fun Database.Transaction.deleteChildDailyServiceTimes(childId: UUID) {
+    // language=sql
+    val sql = """
+        DELETE FROM daily_service_time
+        WHERE child_id = :childId
+    """.trimIndent()
+
+    this.createUpdate(sql).bind("childId", childId).execute()
 }
--- service/src/main/kotlin/fi/espoo/evaka/dailyservicetimes/DailyServiceTimesController.kt
+++ service/src/main/kotlin/fi/espoo/evaka/dailyservicetimes/DailyServiceTimesController.kt
@@ -5,8 +5,11 @@ import fi.espoo.evaka.shared.auth.AuthenticatedUser
 import fi.espoo.evaka.shared.auth.UserRole
 import fi.espoo.evaka.shared.db.Database
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
 
@@ -18,6 +21,7 @@ class DailyServiceTimesController(
     data class DailyServiceTimesResponse(
         val dailyServiceTimes: DailyServiceTimes?
     )
+
     @GetMapping("/children/{childId}/daily-service-times")
     fun getDailyServiceTimes(
         db: Database.Connection,
@@ -34,5 +38,38 @@ class DailyServiceTimesController(
                 DailyServiceTimesResponse(it)
             )
         }
+    }
+
+    @PutMapping("/children/{childId}/daily-service-times")
+    fun putDailyServiceTimes(
+        db: Database.Connection,
+        user: AuthenticatedUser,
+        @PathVariable childId: UUID,
+        @RequestBody body: DailyServiceTimes
+    ): ResponseEntity<Unit> {
+        // todo audit
+        acl.getRolesForChild(user, childId).requireOneOfRoles(
+            UserRole.ADMIN, UserRole.SERVICE_WORKER, UserRole.UNIT_SUPERVISOR
+        )
+
+        db.transaction { it.upsertChildDailyServiceTimes(childId, body) }
+
+        return ResponseEntity.noContent().build()
+    }
+
+    @DeleteMapping("/children/{childId}/daily-service-times")
+    fun deleteDailyServiceTimes(
+        db: Database.Connection,
+        user: AuthenticatedUser,
+        @PathVariable childId: UUID
+    ): ResponseEntity<Unit> {
+        // todo audit
+        acl.getRolesForChild(user, childId).requireOneOfRoles(
+            UserRole.ADMIN, UserRole.SERVICE_WORKER, UserRole.UNIT_SUPERVISOR
+        )
+
+        db.transaction { it.deleteChildDailyServiceTimes(childId) }
+
+        return ResponseEntity.noContent().build()
     }
 }
