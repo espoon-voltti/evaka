6688e737774e5e1d2b6795e8fd0e953cac3a5f87
Petri Lehtinen <petri.lehtinen@reaktor.com>
2021-04-15 07:29:22 +0300

    Reset sequences to their correct start value

--- service/src/main/resources/dev-data/reset-database.sql
+++ service/src/main/resources/dev-data/reset-database.sql
@@ -3,8 +3,6 @@
 -- SPDX-License-Identifier: LGPL-2.1-or-later
 
 CREATE OR REPLACE FUNCTION reset_database() RETURNS void AS $$
-DECLARE
-  sequence text;
 BEGIN
   EXECUTE (
     SELECT 'TRUNCATE TABLE ' || string_agg(quote_ident(table_name), ', ') || ' CASCADE'
@@ -13,11 +11,9 @@ BEGIN
     AND table_type = 'BASE TABLE'
     AND table_name <> 'flyway_schema_history'
   );
-  FOR sequence IN
-    SELECT sequence_name
+  EXECUTE (
+    SELECT 'SELECT ' || string_agg(format('setval(%L, %L, false)', sequence_name, start_value), ', ')
     FROM information_schema.sequences
     WHERE sequence_schema = 'public'
-  LOOP
-    EXECUTE format('SELECT setval(%L, 1, false)', sequence);
-  END LOOP;
+  );
 END $$ LANGUAGE plpgsql;
