fd14f21b69c00f008b1d091b934141e2c0e88fd3
Kim Foudila <kimifoud@users.noreply.github.com>
2021-04-21 08:12:46 +0300

    Add licenses to new files

--- frontend/src/employee-frontend/components/employees/EmployeeList.tsx
+++ frontend/src/employee-frontend/components/employees/EmployeeList.tsx
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2017-2021 City of Espoo
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
 import { Result } from 'lib-common/api'
 import Loader from 'lib-components/atoms/Loader'
 import { Table, Tbody, Td, Th, Thead, Tr } from 'lib-components/layout/Table'
--- frontend/src/employee-frontend/components/employees/EmployeesPage.tsx
+++ frontend/src/employee-frontend/components/employees/EmployeesPage.tsx
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2017-2021 City of Espoo
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
 import { Paged, Result } from 'lib-common/api'
 import { useDebounce } from 'lib-common/utils/useDebounce'
 import { useRestApi } from 'lib-common/utils/useRestApi'
