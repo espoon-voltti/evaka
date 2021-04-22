4277b2e08bf5bf6b813219c9e8d2955794f4cce1
Kim Foudila <1176169+kimifoud@users.noreply.github.com>
2021-04-22 10:03:24 +0300

    Create table for club terms and replace hard coded values in the backend

---
+++ service/src/main/resources/dev-data/club-terms.sql
@@ -0,0 +1,11 @@
+-- SPDX-FileCopyrightText: 2017-2020 City of Espoo
+--
+-- SPDX-License-Identifier: LGPL-2.1-or-later
+
+-- 2020-2021
+INSERT INTO club_term (term, application_period)
+VALUES ('[2020-08-13,2021-06-04]', '[2020-01-08,2020-01-20]');
+
+-- 2021-2022
+INSERT INTO club_term (term, application_period)
+VALUES ('[2021-08-11,2022-06-03]', '[2021-01-08,2021-01-20]');
