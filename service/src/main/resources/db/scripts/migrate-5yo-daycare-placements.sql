43dd7ceeeb76e1afadb6ab755eb92f9f267f22d0
Aleksi Ahonen <aleksi.ahonen@reaktor.com>
2021-04-29 16:47:42 +0300

    Fix 5yo placement migration

--- service/src/main/resources/db/scripts/migrate-5yo-daycare-placements.sql
+++ service/src/main/resources/db/scripts/migrate-5yo-daycare-placements.sql
@@ -39,12 +39,16 @@ WITH child_age_period AS (
     END) AS type,
     greatest(p.start_date, lower(cap.period)) AS start_date,
     least(p.end_date, (upper(cap.period) - interval '1 day')::date) AS end_date,
-    ext.uuid_generate_v1mc() AS new_id,
     rank() OVER (PARTITION BY p.id ORDER BY cap.period ASC)
   FROM placement p
   JOIN child_age_period cap ON p.child_id = cap.id AND daterange(p.start_date, p.end_date, '[]') && cap.period
   WHERE p.type = ANY('{DAYCARE, DAYCARE_PART_TIME}'::placement_type[])
-), split_placement_with_group AS (
+), split_placement_with_new_id AS (
+   SELECT
+     p.*,
+     (CASE WHEN p.rank = 1 THEN p.id ELSE ext.uuid_generate_v1mc() END) AS new_id
+   FROM split_placement p
+ ), split_placement_with_group AS (
   SELECT
     p.*,
     gp.id AS group_placement_id,
@@ -52,27 +56,27 @@ WITH child_age_period AS (
     least(p.end_date, gp.end_date) AS group_placement_end_date,
     gp.daycare_group_id AS group_id,
     rank() OVER (PARTITION BY gp.id ORDER BY p.start_date ASC, gp.start_date ASC) gp_rank
-  FROM split_placement p
+  FROM split_placement_with_new_id p
   JOIN daycare_group_placement gp ON p.id = gp.daycare_placement_id AND daterange(p.start_date, p.end_date, '[]') && daterange(gp.start_date, gp.end_date, '[]')
   ORDER BY p.id, p.rank, group_placement_start_date
 ), updated_placement AS (
-  UPDATE placement orig SET start_date = p.start_date, end_date = p.end_date, type = p.type
-  FROM split_placement p
+  UPDATE placement orig SET id = p.new_id, start_date = p.start_date, end_date = p.end_date, type = p.type
+  FROM split_placement_with_new_id p
   WHERE orig.id = p.id AND p.rank = 1
 ), new_placement AS (
   INSERT INTO placement (id, type, child_id, unit_id, start_date, end_date)
   SELECT p.new_id, p.type, p.child_id, p.unit_id, p.start_date, p.end_date
-  FROM split_placement p
+  FROM split_placement_with_new_id p
   WHERE p.rank > 1
 ), updated_group_placement AS (
-  UPDATE daycare_group_placement orig SET daycare_placement_id = gp.new_id, start_date = gp.start_date, end_date = gp.end_date
+  UPDATE daycare_group_placement orig SET daycare_placement_id = gp.new_id, start_date = gp.group_placement_start_date, end_date = gp.group_placement_end_date
   FROM split_placement_with_group gp
   WHERE orig.id = gp.group_placement_id AND gp.gp_rank = 1
 )
 INSERT INTO daycare_group_placement (daycare_placement_id, daycare_group_id, start_date, end_date)
 SELECT gp.new_id, gp.group_id, gp.group_placement_start_date, gp.group_placement_end_date
 FROM split_placement_with_group gp
-WHERE gp.rank > 1
+WHERE gp.gp_rank > 1
 ;
 
 ROLLBACK;
