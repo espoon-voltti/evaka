61b856d7f18989a6a8306db933428085e15175d3
Kim Foudila <kim.foudila@gmail.com>
2021-05-17 12:26:43 +0300

    Create message accounts only for supervisors and special education teachers in dev data

--- service/src/main/resources/dev-data/employees.sql
+++ service/src/main/resources/dev-data/employees.sql
@@ -14,8 +14,6 @@ INSERT INTO employee (id, first_name, last_name, email, external_id) VALUES
     ('00000000-0000-0000-0005-000000000001', 'Kalle', 'Kasvattaja', 'kalle.kasvattaja@espoo.fi', 'espoo-ad:00000000-0000-0000-0005-000000000001'),
     ('00000000-0000-0000-0006-000000000000', 'Erkki', 'Erityisopettaja', 'erkki.erityisopettaja@espoo.fi', 'espoo-ad:00000000-0000-0000-0006-000000000000');
 
-INSERT INTO message_account (employee_id) SELECT id FROM employee;
-
 INSERT INTO daycare_acl (daycare_id, employee_id, role) VALUES
     ('2dd6e5f6-788e-11e9-bd72-9f1cfe2d8405', '00000000-0000-0000-0004-000000000000', 'UNIT_SUPERVISOR'),
     ('2dda790a-788e-11e9-bdab-27ae0339236a', '00000000-0000-0000-0004-000000000000', 'UNIT_SUPERVISOR'),
@@ -30,3 +28,11 @@ INSERT INTO daycare_acl (daycare_id, employee_id, role) VALUES
     ('2dd6e5f6-788e-11e9-bd72-9f1cfe2d8405', '00000000-0000-0000-0006-000000000000', 'SPECIAL_EDUCATION_TEACHER'),
     ('2dda790a-788e-11e9-bdab-27ae0339236a', '00000000-0000-0000-0006-000000000000', 'SPECIAL_EDUCATION_TEACHER');
 
+INSERT INTO message_account (employee_id)
+SELECT id
+FROM employee e
+WHERE EXISTS(
+    SELECT 1
+    FROM daycare_acl acl
+    WHERE acl.employee_id = e.id
+    AND acl.role IN ('UNIT_SUPERVISOR', 'SPECIAL_EDUCATION_TEACHER'));
