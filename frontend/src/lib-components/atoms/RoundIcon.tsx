996af1debf5677646ce8c4237c15887ddcf71164
Panu Klemola <panu.klemola@solinor.fi>
2021-05-17 12:42:13 +0300

    EVAKA-4161 Added possiblity to use a label and a info bubble with L and XL sized RoundIcons

--- frontend/src/lib-components/atoms/RoundIcon.tsx
+++ frontend/src/lib-components/atoms/RoundIcon.tsx
@@ -9,6 +9,8 @@ import { shade } from 'polished'
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { BaseProps } from '../utils'
+import { tabletMin } from 'lib-components/breakpoints'
+import { FixedSpaceColumn } from 'lib-components/layout/flex-helpers'
 
 export type IconSize = 's' | 'm' | 'L' | 'XL' | 'XXL'
 
@@ -72,6 +74,11 @@ const IconContainer = styled.div<IconContainerProps>`
   &.xl {
     font-size: 44px;
     ${diameter(64)}
+
+    @media (max-width: ${tabletMin}) {
+      font-size: 28px;
+      ${diameter(56)};
+    }
   }
   &.xxl {
     font-size: 80px;
@@ -91,6 +98,10 @@ type RoundIconProps = BaseProps & {
   role?: string
   'aria-label'?: string
   'aria-hidden'?: 'true' | 'false' | undefined
+  label?: string
+  bubble?: boolean
+  bubblecolor?: string
+  number?: number
 }
 
 function RoundIcon({
@@ -104,7 +115,11 @@ function RoundIcon({
   tabindex,
   role,
   'aria-label': ariaLabel,
-  'aria-hidden': ariaHidden
+  'aria-hidden': ariaHidden,
+  label,
+  bubble,
+  bubblecolor,
+  number
 }: RoundIconProps) {
   if (typeof content === 'string' && size !== 'm') {
     console.warn('Text symbol is designed only for size m')
@@ -113,6 +128,27 @@ function RoundIcon({
   function onKeyDown(e: React.KeyboardEvent) {
     if (onClick && (e.key === ' ' || e.key === 'Enter')) onClick(e)
   }
+
+  if (label)
+    return (
+      <WithLabel
+        content={content}
+        color={color}
+        size={size}
+        onClick={onClick}
+        active={active}
+        className={className}
+        data-qa={dataQa}
+        tabindex={tabindex}
+        role={role}
+        label={label}
+        aria-label={ariaLabel}
+        aria-hidden={ariaHidden}
+        number={number}
+        bubble={bubble}
+        bubblecolor={bubblecolor}
+      />
+    )
 
   return (
     <IconContainer
@@ -138,5 +174,79 @@ function RoundIcon({
     </IconContainer>
   )
 }
+
+type WithLabelProps = RoundIconProps & {
+  label: string
+}
+
+function WithLabel({
+  content,
+  color,
+  size,
+  onClick,
+  active = true,
+  className,
+  'data-qa': dataQa,
+  tabindex,
+  role,
+  'aria-label': ariaLabel,
+  'aria-hidden': ariaHidden,
+  label,
+  bubble,
+  bubblecolor,
+  number
+}: WithLabelProps) {
+  return (
+    <FixedSpaceColumnRelative spacing={'xxs'} alignItems={'center'}>
+      <RoundIcon
+        content={content}
+        color={color}
+        size={size}
+        onClick={onClick}
+        active={active}
+        className={className}
+        data-qa={dataQa}
+        tabindex={tabindex}
+        role={role}
+        aria-label={ariaLabel}
+        aria-hidden={ariaHidden}
+      />
+      <Text color={color}>{label}</Text>
+      {bubble && (size === 'L' || size === 'XL') && (
+        <Circle smaller={size === 'L'} color={bubblecolor}>
+          {number}
+        </Circle>
+      )}
+    </FixedSpaceColumnRelative>
+  )
+}
+
+const Text = styled.span<{ color: string }>`
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 21px;
+  color: ${(props) => props.color};
+`
+
+const Circle = styled.span<{ smaller: boolean; color?: string }>`
+  min-width: ${(p) => (p.smaller ? '16px' : '20px')};
+  min-height: ${(p) => (p.smaller ? '16px' : '20px')};
+  padding: 3px 5.5px;
+  font-size: ${(p) => (p.smaller ? '12px' : '16px')};
+  line-height: ${(p) => (p.smaller ? '10px' : '13px')};
+  border-radius: 10px;
+  background-color: ${(p) => (p.color ? p.color : colors.accents.green)};
+  color: ${colors.greyscale.white};
+  display: inline-block;
+  position: absolute;
+  right: -3px;
+  top: 0;
+  font-weight: 600;
+`
+
+const FixedSpaceColumnRelative = styled(FixedSpaceColumn)`
+  position: relative;
+`
 
 export default RoundIcon
