6d0355f1ff9f297869463bbb4868777fbda83fe8
Joonas Javanainen <joonas.javanainen@reaktor.com>
2021-05-21 10:23:52 +0300

    Take screenshots after failed Playwright tests

---
+++ frontend/src/e2e-playwright/jest-environment.js
@@ -0,0 +1,24 @@
+/* eslint-disable
+  @typescript-eslint/no-unsafe-member-access,
+  @typescript-eslint/no-unsafe-assignment,
+  @typescript-eslint/no-unsafe-call,
+  @typescript-eslint/no-var-requires,
+  no-undef
+*/
+const NodeEnvironment = require('jest-environment-node')
+
+class PlaywrightEnvironment extends NodeEnvironment {
+  async handleTestEvent(event, _state) {
+    if (event.name === 'test_fn_failure') {
+      /** @type {string} **/
+      const parentName = event.test.parent.name.replace(/\W/g, '-')
+      /** @type {string} **/
+      const specName = event.test.name.replace(/\W/g, '-')
+
+      const namePrefix = `${parentName}_${specName}`
+      await this.global.evaka?.takeScreenshots(namePrefix)
+    }
+  }
+}
+
+module.exports = PlaywrightEnvironment
--- frontend/src/e2e-playwright/jest.config.ts
+++ frontend/src/e2e-playwright/jest.config.ts
@@ -3,7 +3,7 @@ import type { Config } from '@jest/types'
 const config: Config.InitialOptions = {
   displayName: 'e2e-playwright',
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: './jest-environment',
   testRunner: 'jest-circus/runner',
   moduleNameMapper: {
     '^e2e-playwright/(.*)$': '<rootDir>/$1'
