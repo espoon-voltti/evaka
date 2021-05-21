77d64ac61e84f676f789dcfa62f453fc1eb512ab
Matias Heikkilä <matias@three.consulting>
2021-05-21 10:59:17 +0300

    Refactor messaging frontend

--- frontend/src/employee-frontend/components/messages/MessagesPage.tsx
+++ frontend/src/employee-frontend/components/messages/MessagesPage.tsx
@@ -4,13 +4,9 @@
 
 import ReceiverSelection from 'employee-frontend/components/messages/ReceiverSelection'
 import {
+  deselectAll,
   getReceiverOptions,
-  getSelected,
-  getSelectedBottomElements,
-  SelectorChange,
-  SelectorNode,
-  unitAsSelectorNode,
-  updateSelector
+  SelectorNode
 } from 'employee-frontend/components/messages/SelectorNode'
 import { Loading, Result } from 'lib-common/api'
 import { useRestApi } from 'lib-common/utils/useRestApi'
@@ -18,11 +14,11 @@ import Container from 'lib-components/layout/Container'
 import { Gap } from 'lib-components/white-space'
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
-import { getMessagingAccounts, getReceivers, postMessage } from './api'
+import { getMessagingAccounts } from './api'
 import MessageEditor from './MessageEditor'
 import MessageList from './MessageList'
 import Sidebar from './Sidebar'
-import { Message, MessageAccount, ReceiverGroup } from './types'
+import { MessageAccount } from './types'
 import { AccountView } from './types-view'
 
 const PanelContainer = styled.div`
@@ -33,81 +29,24 @@ export default React.memo(function MessagesPage() {
   const [accounts, setResult] = useState<Result<MessageAccount[]>>(Loading.of())
   const loadAccounts = useRestApi(getMessagingAccounts, setResult)
   useEffect(() => loadAccounts(), [loadAccounts])
-  const [receivers, setReceivers] = useState<Result<ReceiverGroup[]>>(
-    Loading.of()
-  )
-  const loadReceivers = useRestApi(getReceivers, setReceivers)
-  const [selectedReceivers, setSelectedReceivers] = useState<SelectorNode>()
-  const [selectorChange, setSelectorChange] = useState<SelectorChange>()
-
   const [view, setView] = useState<AccountView>()
-  const [unitStuff, setUnitStuff] = useState<{
-    unitId: string
-    unitName: string
-  }>()
+
+  const [selectedReceivers, setSelectedReceivers] = useState<SelectorNode>()
+
   const [showEditor, setShowEditor] = useState<boolean>(false)
-  const [message, setMessage] = useState<Message>({
-    title: '',
-    receivers: [],
-    content: '',
-    id: '',
-    senderId: '',
-    senderName: '',
-    sentAt: new Date(),
-    readAt: new Date()
-  })
-
-  useEffect(() => {
-    if (view?.account.daycareGroup) {
-      const { unitId, unitName } = view.account.daycareGroup
-      setUnitStuff({ unitId, unitName })
-    }
-  }, [view])
-
-  useEffect(() => {
-    if (
-      receivers.isSuccess &&
-      unitStuff &&
-      selectedReceivers?.selectorId !== unitStuff.unitId
-    ) {
-      setSelectedReceivers(
-        unitAsSelectorNode(
-          { id: unitStuff.unitId, name: unitStuff.unitName },
-          receivers.value
-        )
-      )
-    }
-  }, [unitStuff, receivers, selectedReceivers?.selectorId])
-
-  useEffect(() => {
-    if (view) {
-      view.account.daycareGroup &&
-        loadReceivers(view.account.daycareGroup.unitId)
-    }
-  }, [view, loadReceivers])
+  const hideEditor = () => {
+    selectedReceivers && setSelectedReceivers(deselectAll(selectedReceivers))
+    setShowEditor(false)
+  }
 
   useEffect(() => {
     if (accounts.isSuccess && accounts.value[0]) {
-      setView({ view: 'RECEIVED', account: accounts.value[0] })
+      setView({
+        view: 'RECEIVED',
+        account: accounts.value[0]
+      })
     }
   }, [accounts])
-
-  useEffect(() => {
-    if (selectorChange) {
-      setSelectedReceivers((selected: SelectorNode | undefined) =>
-        selected ? updateSelector(selected, selectorChange) : undefined
-      )
-    }
-  }, [selectorChange])
-
-  useEffect(() => {
-    if (selectedReceivers) {
-      setMessage((oldMessage) => ({
-        ...oldMessage,
-        receivers: getSelectedBottomElements(selectedReceivers)
-      }))
-    }
-  }, [selectedReceivers])
 
   return (
     <Container>
@@ -117,38 +56,34 @@ export default React.memo(function MessagesPage() {
           accounts={accounts}
           view={view}
           setView={setView}
+          setSelectedReceivers={setSelectedReceivers}
           showEditor={() => setShowEditor(true)}
         />
-        {view &&
-          (view.view === 'RECEIVED' ||
-            view.view === 'SENT' ||
-            view.view === 'DRAFTS') && <MessageList {...view} />}
-        {view && view.view === 'RECEIVERS' && selectedReceivers && (
+        {(view?.view === 'RECEIVED' ||
+          view?.view === 'SENT' ||
+          view?.view === 'DRAFTS') && <MessageList {...view} />}
+        {view?.view === 'RECEIVERS' && selectedReceivers && (
           <ReceiverSelection
             selectedReceivers={selectedReceivers}
-            setSelectorChange={setSelectorChange}
+            setSelectedReceivers={setSelectedReceivers}
           />
         )}
-        {showEditor && accounts.isSuccess && (
+        {showEditor && accounts.isSuccess && selectedReceivers && view && (
           <MessageEditor
-            message={message}
-            onChange={(message: Message) => setMessage(message)}
-            onClose={() => setShowEditor(false)}
-            onSend={() =>
-              postMessage(accounts.value[0].id, {
-                title: message.title,
-                content: message.content,
-                type: 'MESSAGE',
-                recipientAccountIds: message.receivers
-              })
-            }
-            selectedReceivers={
-              selectedReceivers ? getSelected(selectedReceivers) : []
-            }
+            defaultAccountSelection={{
+              value: view.account.id,
+              label: view.account.id
+            }}
+            accountOptions={accounts.value.map(({ name, id }) => ({
+              value: id,
+              label: name
+            }))}
+            selectedReceivers={selectedReceivers}
             receiverOptions={
               selectedReceivers ? getReceiverOptions(selectedReceivers) : []
             }
-            setSelectorChange={setSelectorChange}
+            setSelectedReceivers={setSelectedReceivers}
+            hideEditor={hideEditor}
           />
         )}
       </PanelContainer>
