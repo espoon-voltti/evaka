5e44aecbcb3ae9784f3c9b7a3a02e56b01b4c9ec
Panu Klemola <panu.klemola@solinor.fi>
2021-05-24 14:56:37 +0300

    EVAKA-4161 Added jest configs to mobile front

---
+++ frontend/src/employee-mobile-frontend/jest.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from '@jest/types'
+
+const config: Config.InitialOptions = {
+  displayName: 'employee-mobile-frontend',
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testRunner: 'jest-circus/runner'
+}
+export default config
