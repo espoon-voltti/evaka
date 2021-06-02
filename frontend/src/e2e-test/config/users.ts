e51f8324f125ef4ced4a450a2562e3585b780328
Petri Lehtinen <petri.lehtinen@reaktor.com>
2021-06-02 08:36:24 +0300

    Support employee login without clicking through the login page

--- frontend/src/e2e-test/config/users.ts
+++ frontend/src/e2e-test/config/users.ts
@@ -5,10 +5,50 @@
 // This will be used with Testcafe's useRole.
 
 import { Role } from 'testcafe'
+import { DevLoginUser } from '../pages/dev-login-form'
 import Home from '../pages/home'
 import config from 'e2e-test-common/config'
 
 const home = new Home()
+
+export const seppoAdmin: DevLoginUser = {
+  aad: config.adminAad,
+  roles: ['SERVICE_WORKER', 'FINANCE_ADMIN', 'ADMIN']
+}
+
+export const seppoManager: DevLoginUser = {
+  aad: config.supervisorAad,
+  roles: []
+}
+
+export async function employeeLogin(
+  t: TestController,
+  user: DevLoginUser,
+  landingUrl?: string
+) {
+  await t.navigateTo(config.employeeUrl)
+  const authUrl =
+    'http://localhost:9099/api/internal/auth/saml/login/callback?RelayState=%2Femployee'
+
+  await t.eval(
+    () => {
+      const params = new URLSearchParams()
+      params.append('preset', 'custom')
+      params.append('firstName', 'Seppo')
+      params.append('lastName', 'Sorsa')
+      params.append('email', 'seppo.sorsa@espoo.fi')
+      params.append('aad', user.aad)
+      user.roles.forEach((role) => params.append('roles', role))
+      return fetch(authUrl, { method: 'POST', body: params }).then(
+        () => undefined
+      )
+    },
+    { dependencies: { authUrl, user } }
+  )
+  if (landingUrl) {
+    await t.navigateTo(landingUrl)
+  }
+}
 
 export const seppoAdminRole = Role(
   config.employeeUrl,
