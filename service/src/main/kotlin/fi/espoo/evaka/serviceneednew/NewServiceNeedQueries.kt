7f69a43eb6203f99b070566078782754f15471b7
Antti Peltola <apeltola@users.noreply.github.com>
2021-06-03 12:59:11 +0300

    Fix per review
    Co-authored-by: Kim Foudila <kim.foudila@gmail.com>

--- service/src/main/kotlin/fi/espoo/evaka/serviceneednew/NewServiceNeedQueries.kt
+++ service/src/main/kotlin/fi/espoo/evaka/serviceneednew/NewServiceNeedQueries.kt
@@ -250,7 +250,7 @@ fun Database.Read.getServiceNeedOptionPublicInfos(placementTypes: List<Placement
             valid_placement_type
         FROM service_need_option
         WHERE default_option IS FALSE AND valid_placement_type = ANY(:placementTypes::placement_type[])
-        ORDER BY display_order;
+        ORDER BY display_order
     """.trimIndent()
     return createQuery(sql)
         .bind("placementTypes", placementTypes.toTypedArray())
