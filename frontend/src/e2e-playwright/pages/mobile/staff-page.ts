4f2582203ed9cb97cf16a18fb0a2ae72f2a2ec90
Petri Lehtinen <petri.lehtinen@reaktor.com>
2021-06-21 09:37:59 +0300

    Update e2e tests

--- frontend/src/e2e-playwright/pages/mobile/staff-page.ts
+++ frontend/src/e2e-playwright/pages/mobile/staff-page.ts
@@ -16,6 +16,11 @@ export default class StaffPage {
     this.page,
     '[data-qa="realized-occupancy"]'
   )
+  #updated = new RawElement(this.page, '[data-qa="updated"]')
+
+  private countButton(parent: RawElement, which: 'plus' | 'minus') {
+    return parent.find(`[data-qa="${which}-button"]`)
+  }
 
   get staffCount() {
     return this.#staffCount.find('[data-qa="value"]').innerText
@@ -25,20 +30,33 @@ export default class StaffPage {
     return this.#staffOtherCount.find('[data-qa="value"]').innerText
   }
 
+  async incDecButtonsVisible(): Promise<boolean[]> {
+    return Promise.all(
+      [this.#staffCount, this.#staffOtherCount]
+        .map((parent) =>
+          (['plus', 'minus'] as const).map((which) =>
+            this.countButton(parent, which)
+          )
+        )
+        .flat()
+        .map((el) => el.visible)
+    )
+  }
+
   async incStaffCount() {
-    return this.#staffCount.find('[data-qa="plus-button"]').click()
+    return this.countButton(this.#staffCount, 'plus').click()
   }
 
   async decStaffCount() {
-    return this.#staffCount.find('[data-qa="minus-button"]').click()
+    return this.countButton(this.#staffCount, 'minus').click()
   }
 
   async incStaffOtherCount() {
-    return this.#staffOtherCount.find('[data-qa="plus-button"]').click()
+    return this.countButton(this.#staffOtherCount, 'plus').click()
   }
 
   async decStaffOtherCount() {
-    return this.#staffOtherCount.find('[data-qa="minus-button"]').click()
+    return this.countButton(this.#staffOtherCount, 'minus').click()
   }
 
   async cancel() {
@@ -58,6 +76,10 @@ export default class StaffPage {
 
   get buttonsEnabled() {
     return this.buttonsDisabled.then((disabled) => !disabled)
+  }
+
+  get updated() {
+    return this.#updated.innerText
   }
 
   get occupancy() {
