4b55c51f6739a1853d909051f424543ac7b1da37
Pasi Huhtiniemi <pasi.huhtiniemi@reaktor.com>
2021-06-29 12:54:02 +0300

    Upgrade to latest PlantUML; Needed switching docker image

--- docs/convert.sh
+++ docs/convert.sh
@@ -7,7 +7,7 @@
 set -euo pipefail
 
 BASEDIR=$(dirname "$0")
-PLANTUML_VERSION=1.2020.11
+PLANTUML_VERSION=1.2021.8
 
 mkdir -p "$BASEDIR"/diagrams/svg
 rm -rf "$BASEDIR"/svg/*
@@ -15,5 +15,5 @@ rm -rf "$BASEDIR"/svg/*
 for FILE in "$BASEDIR"/diagrams/src/*.puml; do
   FILE_SVG="$BASEDIR"/diagrams/svg/"$(basename "$FILE" | sed 's/puml/svg/')"
   echo Converting "${FILE} -> ${FILE_SVG}"
-  docker run --rm -i think/plantuml:"$PLANTUML_VERSION" > "$FILE_SVG" < "$FILE"
+  docker run --rm -i dstockhammer/plantuml:"$PLANTUML_VERSION" -pipe -tsvg > "$FILE_SVG" < "$FILE"
 done
