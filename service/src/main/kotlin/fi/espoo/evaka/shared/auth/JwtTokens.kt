451bcaa0d04c2e35494c05edf413ca01573bfd63
Joonas Javanainen <joonas.javanainen@reaktor.com>
2021-07-05 17:17:05 +0300

    Drop legacy serialization, add missing weak citizen support

--- service/src/main/kotlin/fi/espoo/evaka/shared/auth/JwtTokens.kt
+++ service/src/main/kotlin/fi/espoo/evaka/shared/auth/JwtTokens.kt
@@ -19,7 +19,7 @@ private val tokenExpiration = Duration.ofHours(1)
 
 fun DecodedJWT.toAuthenticatedUser(): AuthenticatedUser? = this.subject?.let { subject ->
     val id = UUID.fromString(subject)
-    val type = this.claims["evaka_type"]?.asString()
+    val type = this.claims["evaka_type"]?.asString()?.let(AuthenticatedUserType::valueOf)
     val roles = (this.claims["scope"]?.asString() ?: "")
         .let {
             if (it.isNullOrEmpty()) emptyList()
@@ -28,36 +28,18 @@ fun DecodedJWT.toAuthenticatedUser(): AuthenticatedUser? = this.subject?.let { s
         .map { enumValueOf<UserRole>(it.removePrefix("ROLE_")) }
         .toSet()
     return when (type) {
-        "citizen" -> AuthenticatedUser.Citizen(id)
-        "citizen_weak" -> AuthenticatedUser.WeakCitizen(id)
-        "employee" -> AuthenticatedUser.Employee(id, roles)
-        "mobile" -> AuthenticatedUser.MobileDevice(id)
-        "system" -> AuthenticatedUser.SystemInternalUser
-        else -> {
-            // support legacy format temporarily
-            when {
-                id == AuthenticatedUser.SystemInternalUser.id -> AuthenticatedUser.SystemInternalUser
-                roles == setOf(UserRole.END_USER) -> AuthenticatedUser.Citizen(id)
-                roles == setOf(UserRole.MOBILE) -> AuthenticatedUser.MobileDevice(id)
-                else ->
-                    AuthenticatedUser.Employee(id, roles)
-            }
-        }
+        AuthenticatedUserType.citizen -> AuthenticatedUser.Citizen(id)
+        AuthenticatedUserType.citizen_weak -> AuthenticatedUser.WeakCitizen(id)
+        AuthenticatedUserType.employee -> AuthenticatedUser.Employee(id, roles)
+        AuthenticatedUserType.mobile -> AuthenticatedUser.MobileDevice(id)
+        AuthenticatedUserType.system -> AuthenticatedUser.SystemInternalUser
+        null -> null
     }
 }
 
 fun AuthenticatedUser.applyToJwt(jwt: JWTCreator.Builder): JWTCreator.Builder = jwt
     .withSubject(id.toString())
-    .withClaim(
-        "evaka_type",
-        when (this) {
-            is AuthenticatedUser.SystemInternalUser -> "system"
-            is AuthenticatedUser.MobileDevice -> "mobile"
-            is AuthenticatedUser.Employee -> "employee"
-            is AuthenticatedUser.Citizen -> "citizen"
-            is AuthenticatedUser.WeakCitizen -> "citizen_weak"
-        }
-    )
+    .withClaim("evaka_type", this.type.toString())
     .withClaim("scope", roles.joinToString(" "))
 
 fun encodeSignedJwtToken(
