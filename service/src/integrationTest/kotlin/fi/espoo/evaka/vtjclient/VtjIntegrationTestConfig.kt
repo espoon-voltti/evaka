8cc76735868db77a24ca32234d102e9523f7b88f
Joonas Javanainen <joonas.javanainen@reaktor.com>
2021-07-09 15:26:08 +0300

    Improve VtjCacheIntegrationTest
    - use FullApplicationTest
    - make sure the redis DB is empty, since that's a real precondition for
      the test
    - drop VtjCache mock. We have a real redis client configured for
      integration tests

--- service/src/integrationTest/kotlin/fi/espoo/evaka/vtjclient/VtjIntegrationTestConfig.kt
+++ service/src/integrationTest/kotlin/fi/espoo/evaka/vtjclient/VtjIntegrationTestConfig.kt
@@ -10,20 +10,14 @@ import fi.espoo.evaka.vtjclient.properties.XRoadProperties
 import fi.espoo.evaka.vtjclient.service.persondetails.IPersonDetailsService
 import fi.espoo.evaka.vtjclient.service.persondetails.MockPersonDetailsService
 import org.springframework.boot.test.context.TestConfiguration
-import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
 import org.springframework.context.annotation.Profile
-import redis.clients.jedis.JedisPool
 
 @TestConfiguration
 @Profile("integration-test")
 @Import(XRoadProperties::class, XroadSoapClientConfig::class, TrustManagerConfig::class)
 class VtjIntegrationTestConfig {
-
-    @MockBean
-    lateinit var redisPool: JedisPool
-
     @Bean
     fun personDetailsService(): IPersonDetailsService = MockPersonDetailsService()
 }
