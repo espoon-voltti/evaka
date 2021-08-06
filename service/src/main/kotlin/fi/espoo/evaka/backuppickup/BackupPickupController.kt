6e4ab25e2d448e2c9f0dffa5f9ec370a3fe16fc0
Joosa Kurvinen <joosa.kurvinen@gofore.com>
2021-08-06 11:16:43 +0300

    increase backwards compatibility

--- service/src/main/kotlin/fi/espoo/evaka/backuppickup/BackupPickupController.kt
+++ service/src/main/kotlin/fi/espoo/evaka/backuppickup/BackupPickupController.kt
@@ -6,7 +6,9 @@ package fi.espoo.evaka.backuppickup
 
 import fi.espoo.evaka.Audit
 import fi.espoo.evaka.shared.BackupPickupId
+import fi.espoo.evaka.shared.auth.AccessControlList
 import fi.espoo.evaka.shared.auth.AuthenticatedUser
+import fi.espoo.evaka.shared.auth.UserRole
 import fi.espoo.evaka.shared.db.Database
 import fi.espoo.evaka.shared.db.updateExactlyOne
 import fi.espoo.evaka.shared.security.AccessControl
@@ -23,7 +25,10 @@ import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
 
 @RestController
-class BackupPickupController(private val accessControl: AccessControl) {
+class BackupPickupController(
+    private val accessControl: AccessControl,
+    private val acl: AccessControlList
+) {
     @PostMapping("/children/{childId}/backup-pickups")
     fun createForChild(
         db: Database.Connection,
@@ -78,6 +83,45 @@ class BackupPickupController(private val accessControl: AccessControl) {
     ): ResponseEntity<Unit> {
         Audit.ChildBackupPickupDelete.log(targetId = id)
         accessControl.requirePermissionFor(user, Action.BackupPickup.DELETE, id)
+        db.transaction { tx ->
+            tx.deleteBackupPickup(id)
+        }
+        return ResponseEntity.noContent().build()
+    }
+
+    // TODO: Delete these after deployment
+
+    @PutMapping("/children/{childId}/backup-pickups")
+    fun updateForChildV0(
+        db: Database.Connection,
+        user: AuthenticatedUser,
+        @PathVariable("childId") childId: UUID,
+        @RequestBody body: ChildBackupPickup
+    ): ResponseEntity<Unit> {
+        Audit.ChildBackupPickupUpdate.log(targetId = childId)
+        acl.getRolesForChild(user, childId).requireOneOfRoles(UserRole.ADMIN, UserRole.UNIT_SUPERVISOR, UserRole.STAFF)
+        return db.transaction { tx ->
+            tx.updateBackupPickup(
+                body.id,
+                ChildBackupPickupContent(
+                    name = body.name,
+                    phone = body.name
+                )
+            )
+        }.let {
+            ResponseEntity.noContent().build()
+        }
+    }
+
+    @DeleteMapping("/children/{childId}/backup-pickups/{id}")
+    fun deleteV0(
+        db: Database.Connection,
+        user: AuthenticatedUser,
+        @PathVariable("id") id: BackupPickupId,
+        @PathVariable("childId") childId: UUID
+    ): ResponseEntity<Unit> {
+        Audit.ChildBackupPickupDelete.log(targetId = childId)
+        acl.getRolesForChild(user, childId).requireOneOfRoles(UserRole.ADMIN, UserRole.UNIT_SUPERVISOR, UserRole.STAFF)
         db.transaction { tx ->
             tx.deleteBackupPickup(id)
         }
