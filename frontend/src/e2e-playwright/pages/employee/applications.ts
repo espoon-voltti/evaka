08820799957a08f6c14946c3e2668a706e75d5c0
Aleksi Ahonen <aleksi.ahonen@reaktor.com>
2021-08-11 15:37:41 +0300

    Rewrite employee attachments test with Playwright

--- frontend/src/e2e-playwright/pages/employee/applications.ts
+++ frontend/src/e2e-playwright/pages/employee/applications.ts
@@ -3,7 +3,8 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
 import { Page } from 'playwright'
-import { RawElement } from 'e2e-playwright/utils/element'
+import { Checkbox, RawElement } from 'e2e-playwright/utils/element'
+import { waitUntilEqual, waitUntilFalse } from 'e2e-playwright/utils'
 
 export default class ApplicationsPage {
   constructor(private readonly page: Page) {}
@@ -48,8 +49,110 @@ export default class ApplicationsPage {
 class ApplicationDetailsPage {
   constructor(private readonly page: Page) {}
 
+  #editButton = new RawElement(this.page, '[data-qa="edit-application"]')
+  #saveButton = new RawElement(this.page, '[data-qa="save-application"]')
+  #urgentCheckbox = new Checkbox(this.page, '[data-qa="checkbox-urgent"]')
+  #urgentAttachmentFileUploadSelector = '[data-qa="file-upload-urgent"]'
+  #urgentAttachmentFileUpload = new RawElement(
+    this.page,
+    this.#urgentAttachmentFileUploadSelector
+  )
+  #shiftCareCheckbox = new Checkbox(
+    this.page,
+    '[data-qa="checkbox-service-need-shift-care"]'
+  )
+  #shiftCareAttachmentFileUploadSelector = '[data-qa="file-upload-shift-care"]'
+  #shiftCareAttachmentFileUpload = new RawElement(
+    this.page,
+    this.#shiftCareAttachmentFileUploadSelector
+  )
+
   applicantDeadIndicator = new RawElement(
     this.page,
     '[data-qa="applicant-dead"]'
   )
+
+  async startEditing() {
+    await this.#editButton.click()
+  }
+
+  async saveApplication() {
+    await this.#saveButton.click()
+  }
+
+  async setUrgent() {
+    if (await this.#urgentCheckbox.checked) {
+      return
+    }
+    await this.#urgentCheckbox.click()
+  }
+
+  async uploadUrgentAttachment(filePath: string) {
+    await this.page.setInputFiles(
+      `${this.#urgentAttachmentFileUploadSelector} [data-qa="btn-upload-file"]`,
+      filePath
+    )
+  }
+
+  async assertUrgentAttachmentUploaded(fileName: string) {
+    await waitUntilEqual(
+      () =>
+        this.#urgentAttachmentFileUpload.find(
+          '[data-qa="file-download-button"]'
+        ).innerText,
+      fileName
+    )
+  }
+
+  async assertUrgencyAttachmentReceivedAtVisible(fileName: string) {
+    const attachment = new RawElement(
+      this.page,
+      `[data-qa="urgent-attachment-${fileName}"]`
+    )
+    await attachment.waitUntilVisible()
+    await attachment
+      .find('[data-qa="attachment-received-at"]')
+      .waitUntilVisible()
+  }
+
+  async setShiftCareNeeded() {
+    if (await this.#shiftCareCheckbox.checked) {
+      return
+    }
+    await this.#shiftCareCheckbox.click()
+  }
+
+  async uploadShiftCareAttachment(filePath: string) {
+    await this.page.setInputFiles(
+      `${
+        this.#shiftCareAttachmentFileUploadSelector
+      } [data-qa="btn-upload-file"]`,
+      filePath
+    )
+  }
+
+  async deleteShiftCareAttachment(fileName: string) {
+    await this.#shiftCareAttachmentFileUpload
+      .find(`[data-qa="file-delete-button-${fileName}"]`)
+      .click()
+  }
+
+  async assertShiftCareAttachmentUploaded(fileName: string) {
+    await waitUntilEqual(
+      () =>
+        this.#shiftCareAttachmentFileUpload.find(
+          '[data-qa="file-download-button"]'
+        ).innerText,
+      fileName
+    )
+  }
+
+  async assertShiftCareAttachmentsDeleted() {
+    await waitUntilFalse(
+      () =>
+        this.#shiftCareAttachmentFileUpload.find(
+          '[data-qa="file-download-button"]'
+        ).visible
+    )
+  }
 }
