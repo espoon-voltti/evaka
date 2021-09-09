df01f5db3c934319e4b9d30598bc2de33955a66f
Kim Foudila <kim.foudila@gmail.com>
2021-09-09 14:05:09 +0300

    Do not wrap Routes in Fragments

--- frontend/src/citizen-frontend/App.tsx
+++ frontend/src/citizen-frontend/App.tsx
@@ -59,23 +59,25 @@ export default function App() {
                       component={requireAuth(ApplicationReadView)}
                     />
                     {featureFlags.experimental?.incomeStatements && (
-                      <>
-                        <Route
-                          exact
-                          path="/income"
-                          component={requireAuth(IncomeStatements)}
-                        />
-                        <Route
-                          exact
-                          path="/income/:incomeStatementId/edit"
-                          component={requireAuth(IncomeStatementEditor)}
-                        />
-                        <Route
-                          exact
-                          path="/income/:incomeStatementId"
-                          component={requireAuth(IncomeStatementView)}
-                        />
-                      </>
+                      <Route
+                        exact
+                        path="/income"
+                        component={requireAuth(IncomeStatements)}
+                      />
+                    )}
+                    {featureFlags.experimental?.incomeStatements && (
+                      <Route
+                        exact
+                        path="/income/:incomeStatementId/edit"
+                        component={requireAuth(IncomeStatementEditor)}
+                      />
+                    )}
+                    {featureFlags.experimental?.incomeStatements && (
+                      <Route
+                        exact
+                        path="/income/:incomeStatementId"
+                        component={requireAuth(IncomeStatementView)}
+                      />
                     )}
                     <Route
                       exact
