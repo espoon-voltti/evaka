974a24bb6c8c197b6323187c4ce2098c7b136430
Joonas Reynders <joonas.reynders@reaktor.fi>
2021-09-16 10:08:52 +0300

    Allow sending value decision from details page

--- frontend/src/employee-frontend/components/voucher-value-decision/VoucherValueDecisionActionBar.tsx
+++ frontend/src/employee-frontend/components/voucher-value-decision/VoucherValueDecisionActionBar.tsx
@@ -1,3 +1,5 @@
+import {useTranslation} from "../../state/i18n";
+
 {
   /*
 SPDX-FileCopyrightText: 2017-2020 City of Espoo
@@ -6,11 +8,17 @@ SPDX-License-Identifier: LGPL-2.1-or-later
 */
 }
 
-import React from 'react'
+import React, {useState} from 'react'
 import AsyncButton from 'lib-components/atoms/buttons/AsyncButton'
 import StickyActionBar from '../../components/common/StickyActionBar'
-import { markVoucherValueDecisionSent } from '../../api/invoicing'
+import {markVoucherValueDecisionSent, sendVoucherValueDecisions} from '../../api/invoicing'
 import { VoucherValueDecisionDetailed } from '../../types/invoicing'
+import styled from "styled-components";
+
+const ErrorMessage = styled.div`
+  color: ${(p) => p.theme.colors.accents.orangeDark};
+  margin: 0 20px;
+`
 
 type Props = {
   decision: VoucherValueDecisionDetailed
@@ -21,12 +29,40 @@ export default React.memo(function VoucherValueDecisionActionBar({
   decision,
   loadDecision
 }: Props) {
+  const { i18n } = useTranslation()
+  const [error, setError] = useState<string>()
+
   return decision.status === 'WAITING_FOR_MANUAL_SENDING' ? (
     <StickyActionBar align="right">
+      {error ? <ErrorMessage>{error}</ErrorMessage> : null}
       <AsyncButton
         primary
         text="Merkitse lähetetyksi"
         onClick={() => markVoucherValueDecisionSent([decision.id])}
+        onSuccess={loadDecision}
+      />
+    </StickyActionBar>
+  ) : decision.status === 'DRAFT' ? (
+    <StickyActionBar align="right">
+      <AsyncButton
+        primary
+        text="Lähetä"
+        onClick={() =>
+          sendVoucherValueDecisions([decision.id]).then((result) => {
+            if (result.isSuccess) {
+              setError(undefined)
+            }
+
+            if (result.isFailure) {
+              setError(
+                i18n.valueDecisions.buttons.errors[result.errorCode ?? ''] ??
+                i18n.common.error.unknown
+              )
+            }
+
+            return result
+          })
+        }
         onSuccess={loadDecision}
       />
     </StickyActionBar>
