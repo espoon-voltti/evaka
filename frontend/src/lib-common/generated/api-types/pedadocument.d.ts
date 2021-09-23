1a6c2d060ce58cd1a5e407008bfa509b7f467e3f
Henri Ruoho <henri.ruoho@reaktor.com>
2021-09-23 10:18:38 +0300

    Add generated types

---
+++ frontend/src/lib-common/generated/api-types/pedadocument.d.ts
@@ -0,0 +1,30 @@
+// SPDX-FileCopyrightText: 2017-2021 City of Espoo
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+// GENERATED FILE: no manual modifications
+/* eslint-disable prettier/prettier */
+
+import { UUID } from '../../types'
+
+/**
+* Generated from fi.espoo.evaka.pedadocument.PedagogicalDocument
+*/
+export interface PedagogicalDocument {
+  attachmentId: UUID | null
+  childId: UUID
+  created: Date
+  description: string
+  id: UUID
+  updated: Date
+}
+
+/**
+* Generated from fi.espoo.evaka.pedadocument.PedagogicalDocumentPostBody
+*/
+export interface PedagogicalDocumentPostBody {
+  attachmentId: UUID | null
+  childId: UUID
+  description: string
+  id: UUID
+}
