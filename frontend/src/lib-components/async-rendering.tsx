7c818dcfa8203f3f7c64e3671c45a8c03085a853
Mikko Piuhola <me@mikkopiuhola.fi>
2021-09-28 15:22:12 +0300

    Add missing license headers

--- frontend/src/lib-components/async-rendering.tsx
+++ frontend/src/lib-components/async-rendering.tsx
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2017-2021 City of Espoo
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
 import { Result } from 'lib-common/api'
 import React from 'react'
 import { SpinnerSegment } from 'lib-components/atoms/state/Spinner'
