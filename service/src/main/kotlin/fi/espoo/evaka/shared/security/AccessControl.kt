490fbccc29dc8fec2b81e8e7071a22a46baae9a4
Joonas Reynders <joonas.reynders@reaktor.fi>
2021-09-30 08:05:07 +0300

    Add missing access control checks

--- service/src/main/kotlin/fi/espoo/evaka/shared/security/AccessControl.kt
+++ service/src/main/kotlin/fi/espoo/evaka/shared/security/AccessControl.kt
@@ -301,7 +301,9 @@ WHERE employee_id = :userId
                     Database(jdbi).connect { db -> db.read { it.hasPermissionForAttachmentThroughMessageContent(user, id) } }
                 Action.Attachment.READ_MESSAGE_DRAFT_ATTACHMENT,
                 Action.Attachment.DELETE_MESSAGE_CONTENT_ATTACHMENT,
-                Action.Attachment.DELETE_MESSAGE_DRAFT_ATTACHMENT -> false
+                Action.Attachment.DELETE_MESSAGE_DRAFT_ATTACHMENT,
+                Action.Attachment.READ_PEDAGOGICAL_DOCUMENT_ATTACHMENT,
+                Action.Attachment.DELETE_PEDAGOGICAL_DOCUMENT_ATTACHMENT -> false
             }
             is AuthenticatedUser.WeakCitizen -> when (action) {
                 Action.Attachment.READ_MESSAGE_CONTENT_ATTACHMENT ->
@@ -312,7 +314,9 @@ WHERE employee_id = :userId
                 Action.Attachment.DELETE_APPLICATION_ATTACHMENT,
                 Action.Attachment.DELETE_INCOME_STATEMENT_ATTACHMENT,
                 Action.Attachment.DELETE_MESSAGE_CONTENT_ATTACHMENT,
-                Action.Attachment.DELETE_MESSAGE_DRAFT_ATTACHMENT -> false
+                Action.Attachment.DELETE_MESSAGE_DRAFT_ATTACHMENT,
+                Action.Attachment.READ_PEDAGOGICAL_DOCUMENT_ATTACHMENT,
+                Action.Attachment.DELETE_PEDAGOGICAL_DOCUMENT_ATTACHMENT -> false
             }
             is AuthenticatedUser.Employee -> when (action) {
                 Action.Attachment.READ_APPLICATION_ATTACHMENT ->
