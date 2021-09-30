43920f4599a2b57ed2bfdc48d2694460f6decca9
Aleksi Ahonen <aleksi.ahonen@reaktor.com>
2021-09-30 14:30:01 +0300

    No automatic departures for attendances in shift care units

--- service/src/integrationTest/kotlin/fi/espoo/evaka/attendance/AttendanceUpkeepIntegrationTest.kt
+++ service/src/integrationTest/kotlin/fi/espoo/evaka/attendance/AttendanceUpkeepIntegrationTest.kt
@@ -6,57 +6,30 @@ package fi.espoo.evaka.attendance
 
 import fi.espoo.evaka.FullApplicationTest
 import fi.espoo.evaka.insertGeneralTestFixtures
-import fi.espoo.evaka.placement.PlacementType
 import fi.espoo.evaka.resetDatabase
-import fi.espoo.evaka.shared.GroupId
-import fi.espoo.evaka.shared.PlacementId
-import fi.espoo.evaka.shared.dev.DevDaycareGroup
 import fi.espoo.evaka.shared.dev.insertTestChildAttendance
-import fi.espoo.evaka.shared.dev.insertTestDaycareGroup
-import fi.espoo.evaka.shared.dev.insertTestDaycareGroupPlacement
-import fi.espoo.evaka.shared.dev.insertTestPlacement
 import fi.espoo.evaka.shared.domain.HelsinkiDateTime
 import fi.espoo.evaka.shared.job.ScheduledJobs
 import fi.espoo.evaka.testChild_1
 import fi.espoo.evaka.testDaycare
+import fi.espoo.evaka.testRoundTheClockDaycare
 import org.jdbi.v3.core.kotlin.mapTo
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import java.time.LocalDate
 import java.time.LocalTime
-import java.util.UUID
 import kotlin.test.assertEquals
 
 class AttendanceUpkeepIntegrationTest : FullApplicationTest() {
     @Autowired
     private lateinit var scheduledJobs: ScheduledJobs
 
-    private val groupId = GroupId(UUID.randomUUID())
-    private val daycarePlacementId = PlacementId(UUID.randomUUID())
-    private val placementStart = LocalDate.now().minusDays(30)
-    private val placementEnd = LocalDate.now().plusDays(30)
-
     @BeforeEach
     fun beforeEach() {
         db.transaction { tx ->
             tx.resetDatabase()
             tx.insertGeneralTestFixtures()
-            tx.insertTestDaycareGroup(DevDaycareGroup(id = groupId, daycareId = testDaycare.id))
-            tx.insertTestPlacement(
-                id = daycarePlacementId,
-                childId = testChild_1.id,
-                unitId = testDaycare.id,
-                startDate = placementStart,
-                endDate = placementEnd,
-                type = PlacementType.PRESCHOOL_DAYCARE
-            )
-            tx.insertTestDaycareGroupPlacement(
-                daycarePlacementId = daycarePlacementId,
-                groupId = groupId,
-                startDate = placementStart,
-                endDate = placementEnd
-            )
         }
     }
 
@@ -89,5 +62,28 @@ class AttendanceUpkeepIntegrationTest : FullApplicationTest() {
         assertEquals(1, attendances.size)
         val expectedDeparted = HelsinkiDateTime.of(LocalDate.of(2020, 10, 25), LocalTime.of(23, 59))
         assertEquals(expectedDeparted, attendances.first().departed)
+    }
+
+    @Test
+    fun `null departures are not set in round the clock units`() {
+        val arrived = HelsinkiDateTime.of(LocalDate.of(2020, 10, 25), LocalTime.of(9, 0))
+
+        db.transaction {
+            it.insertTestChildAttendance(
+                childId = testChild_1.id,
+                unitId = testRoundTheClockDaycare.id!!,
+                arrived = arrived,
+                departed = null
+            )
+        }
+
+        scheduledJobs.endOfDayAttendanceUpkeep(db)
+
+        val attendances = db.read {
+            it.createQuery("SELECT * FROM child_attendance").mapTo<ChildAttendance>().list()
+        }
+
+        assertEquals(1, attendances.size)
+        assertEquals(null, attendances.first().departed)
     }
 }
