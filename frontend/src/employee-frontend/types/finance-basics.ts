3d070ff7a50d9b99a3a32508c0c62dafb69f3b5a
Petri Lehtinen <petri.lehtinen@reaktor.com>
2021-10-04 08:51:50 +0300

    Fix a re-export warning from webpack

--- frontend/src/employee-frontend/types/finance-basics.ts
+++ frontend/src/employee-frontend/types/finance-basics.ts
@@ -2,9 +2,9 @@
 //
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
-import { FeeThresholds } from 'lib-common/api-types/finance'
+import type { FeeThresholds } from 'lib-common/api-types/finance'
 
-export { FeeThresholds } from 'lib-common/api-types/finance'
+export type { FeeThresholds }
 
 export const familySizes = ['2', '3', '4', '5', '6'] as const
 export type FamilySize = typeof familySizes[number]
