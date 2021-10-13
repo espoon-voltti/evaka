ba383969b9840befdc228b2303007761d4f45272
Joosa Kurvinen <joosa.kurvinen@gofore.com>
2021-10-13 09:36:37 +0300

    add lisences

--- frontend/src/employee-frontend/components/unit/tab-unit-information/occupancy/OccupancyDayGraph.tsx
+++ frontend/src/employee-frontend/components/unit/tab-unit-information/occupancy/OccupancyDayGraph.tsx
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2017-2021 City of Espoo
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
 import React from 'react'
 import { RealtimeOccupancy } from 'lib-common/generated/api-types/occupancy'
 import { fi } from 'date-fns/locale'
