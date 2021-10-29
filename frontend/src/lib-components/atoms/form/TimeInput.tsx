319085cd2e0d08d290ce283d4eb293b48d5de914
Aleksi Ahonen <aleksi.ahonen@reaktor.com>
2021-10-29 09:28:34 +0300

    Add tests for TimeInput autocomplete

--- frontend/src/lib-components/atoms/form/TimeInput.tsx
+++ frontend/src/lib-components/atoms/form/TimeInput.tsx
@@ -4,6 +4,7 @@
 
 import React, { useCallback } from 'react'
 import styled from 'styled-components'
+import { autocomplete } from 'lib-common/time'
 import InputField, { TextInputProps } from './InputField'
 
 interface Props
@@ -48,55 +49,6 @@ export default React.memo(function TimeInput({
 function onFocus(event: React.FocusEvent<HTMLInputElement>) {
   event.target.select()
 }
-
-function autocomplete(previous: string, current: string) {
-  const filtered = current
-    .replace('.', ':')
-    .replace(disallowedInput, '')
-    .slice(0, 5)
-
-  if (filtered.length <= previous.length) {
-    return filtered
-  }
-
-  if (singleDigitHour.test(filtered)) {
-    return `0${filtered}:`
-  }
-
-  if (doubleDigitHour.test(filtered)) {
-    return `${filtered}:`
-  }
-
-  if (/^([0-9]{2})$/.test(filtered)) {
-    const [hour, minutes] = filtered
-    return `0${hour}:${minutes}`
-  }
-
-  const partBeforeLastChar = filtered.substring(0, filtered.length - 1)
-  const lastChar = filtered[filtered.length - 1]
-
-  if (/^([0-9]{2})$/.test(partBeforeLastChar) && /[0-9]/.test(lastChar)) {
-    return `${partBeforeLastChar}:${lastChar}`
-  }
-
-  if (lastChar === ':') {
-    if (/^([0-9])$/.test(partBeforeLastChar)) {
-      return `0${partBeforeLastChar}:`
-    }
-
-    if (/^([0-9]{2})$/.test(partBeforeLastChar)) {
-      return filtered
-    }
-
-    return partBeforeLastChar
-  }
-
-  return filtered
-}
-
-const disallowedInput = /[^0-9\\:]/g
-const singleDigitHour = /^([3-9])$/
-const doubleDigitHour = /^(([0-1][0-9])|(2[0-3]))$/
 
 const ShorterInput = styled(InputField)`
   width: 80px;
