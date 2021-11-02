3a0e5271c9d4c15f3c73e5db6c85e8a18ab86968
Kim Foudila <kim.foudila@gmail.com>
2021-11-02 13:02:29 +0200

    Remove spinner altogether from ensureAuthenticated

--- frontend/src/employee-mobile-frontend/components/ensureAuthenticated.tsx
+++ frontend/src/employee-mobile-frontend/components/ensureAuthenticated.tsx
@@ -2,24 +2,14 @@
 //
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
-import { FullScreenDimmedSpinner } from 'lib-components/molecules/FullScreenDimmedSpinner'
 import React, { ComponentType, useContext } from 'react'
 import { Redirect } from 'react-router-dom'
 import { UserContext } from '../state/user'
 
 export default function ensureAuthenticated<P>(Component: ComponentType<P>) {
   return function Authenticated(props: P) {
-    const { loggedIn, user } = useContext(UserContext)
+    const { loggedIn } = useContext(UserContext)
 
-    if (!loggedIn) {
-      return <Redirect to="" />
-    }
-
-    return (
-      <>
-        {user.isLoading && <FullScreenDimmedSpinner />}
-        <Component {...props} />
-      </>
-    )
+    return loggedIn ? <Component {...props} /> : <Redirect to="" />
   }
 }
