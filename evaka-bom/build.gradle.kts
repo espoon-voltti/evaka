61ef1a9b36a4d49014171bf8ea62b1813105d285
Joonas Javanainen <joonas.javanainen@reaktor.com>
2021-11-22 09:40:25 +0200

    Upgrade to mockito 4.1.0

--- evaka-bom/build.gradle.kts
+++ evaka-bom/build.gradle.kts
@@ -11,7 +11,7 @@ object Version {
     const val cxf = "3.4.5"
     const val flyingSaucer = "9.1.22"
     const val fuel = "2.3.1"
-    const val mockito = "4.0.0"
+    const val mockito = "4.1.0"
 }
 
 repositories {
