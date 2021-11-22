0b464c6afd0972ce8dfa7c3229849b0449f1485b
Ilja <ilja@kooditon.com>
2021-11-22 16:30:05 +0200

    Add APM monitoring to node

--- apigw/src/index.ts
+++ apigw/src/index.ts
@@ -1,9 +1,10 @@
-// SPDX-FileCopyrightText: 2017-2020 City of Espoo
+// SPDX-FileCopyrightText: 2017-2021 City of Espoo
 //
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
 import 'source-map-support/register'
 import { gatewayRole } from './shared/config'
+import './tracer'
 
 if (!gatewayRole || gatewayRole === 'enduser') {
   require('./enduser')
---
+++ apigw/src/tracer.ts
@@ -0,0 +1,28 @@
+// SPDX-FileCopyrightText: 2017-2021 City of Espoo
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+import {
+  volttiEnv,
+  serviceName,
+  appCommit,
+  tracingEnabled,
+  traceAgentHostname,
+  traceAgentPort
+} from './shared/config'
+import tracer from 'dd-trace'
+
+if (tracingEnabled) {
+  tracer.init({
+    env: volttiEnv,
+    service: serviceName,
+    version: appCommit,
+    enabled: tracingEnabled,
+    hostname: traceAgentHostname,
+    port: traceAgentPort,
+    logInjection: true,
+    runtimeMetrics: true
+  }) // initialized in a different file to avoid hoisting.
+}
+
+export default tracer
