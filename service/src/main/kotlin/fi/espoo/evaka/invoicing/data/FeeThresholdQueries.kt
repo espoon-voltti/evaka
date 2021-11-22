b1930361c174af198dca06ecc76a32710701f08d
Kim Foudila <kim.foudila@gmail.com>
2021-11-22 09:35:02 +0200

    Replace select * from fee_thresholds with explicit columns

--- service/src/main/kotlin/fi/espoo/evaka/invoicing/data/FeeThresholdQueries.kt
+++ service/src/main/kotlin/fi/espoo/evaka/invoicing/data/FeeThresholdQueries.kt
@@ -11,7 +11,34 @@ import org.jdbi.v3.core.kotlin.mapTo
 import java.time.LocalDate
 
 fun Database.Read.getFeeThresholds(from: LocalDate? = null): List<FeeThresholds> {
-    return createQuery("SELECT * FROM fee_thresholds WHERE valid_during && daterange(:from, null)")
+    return createQuery(
+        """
+SELECT
+    valid_during,
+    min_income_threshold_2,
+    min_income_threshold_3,
+    min_income_threshold_4,
+    min_income_threshold_5,
+    min_income_threshold_6,
+    income_multiplier_2,
+    income_multiplier_3,
+    income_multiplier_4,
+    income_multiplier_5,
+    income_multiplier_6,
+    max_income_threshold_2,
+    max_income_threshold_3,
+    max_income_threshold_4,
+    max_income_threshold_5,
+    max_income_threshold_6,
+    income_threshold_increase_6_plus,
+    sibling_discount_2,
+    sibling_discount_2_plus,
+    max_fee,
+    min_fee
+FROM fee_thresholds
+WHERE valid_during && daterange(:from, null)
+        """.trimIndent()
+    )
         .bindNullable("from", from)
         .mapTo<FeeThresholds>()
         .toList()
