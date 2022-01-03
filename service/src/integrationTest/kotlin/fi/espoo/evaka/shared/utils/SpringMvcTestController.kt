4ed11e38f424ed03cc2c16fee0ee7d6a3964ed13
Joonas Javanainen <joonas.javanainen@reaktor.com>
2022-01-03 09:00:10 +0200

    Remove Database.DeprecatedConnection leftovers
    No need to have an automatic closing mechanism since we no longer have
    the automatic opening mechanism.

--- service/src/integrationTest/kotlin/fi/espoo/evaka/shared/utils/SpringMvcTestController.kt
+++ service/src/integrationTest/kotlin/fi/espoo/evaka/shared/utils/SpringMvcTestController.kt
@@ -5,28 +5,13 @@
 package fi.espoo.evaka.shared.utils
 
 import fi.espoo.evaka.shared.auth.AuthenticatedUser
-import fi.espoo.evaka.shared.db.Database
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.util.concurrent.atomic.AtomicReference
 
 @RestController
 @RequestMapping("/integration-test")
 class SpringMvcTestController {
-    val lastDbConnection = AtomicReference<Database.Connection?>()
-
-    @GetMapping("/db-connection-pass")
-    fun dbConnectionPass(db: Database) {
-        db.connect { dbc -> lastDbConnection.set(dbc) }
-    }
-
-    @GetMapping("/db-connection-fail")
-    fun dbConnectionFail(db: Database) {
-        db.connect { dbc -> lastDbConnection.set(dbc) }
-        throw RuntimeException("Failed")
-    }
-
     @GetMapping("/require-auth")
     fun requireAuth(user: AuthenticatedUser) {
     }
