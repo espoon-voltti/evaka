f59145ffe8c9736b90b2385c49718e4781a21bc9
Joonas Javanainen <joonas.javanainen@reaktor.com>
2022-01-04 12:27:04 +0200

    Upgrade to JDBI 3.26

--- evaka-bom/build.gradle.kts
+++ evaka-bom/build.gradle.kts
@@ -65,7 +65,7 @@ dependencies {
 
     api(platform("com.fasterxml.jackson:jackson-bom:2.13.1"))
     api(platform("org.apache.cxf:cxf-bom:${Version.cxf}"))
-    api(platform("org.jdbi:jdbi3-bom:3.24.1"))
+    api(platform("org.jdbi:jdbi3-bom:3.26.0"))
     api(platform("org.jetbrains.kotlin:kotlin-bom:1.6.10"))
     api(platform("org.junit:junit-bom:5.8.2"))
     api(platform("org.springframework.boot:spring-boot-dependencies:2.6.2"))
