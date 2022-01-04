b3b53eb9a2f7ab1fe3c2e73a8afa1e9b2bc6d1f7
Joonas Javanainen <joonas.javanainen@reaktor.com>
2022-01-04 12:28:36 +0200

    Upgrade to CXF 3.5.0

--- evaka-bom/build.gradle.kts
+++ evaka-bom/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 object Version {
     const val bouncyCastle = "1.69"
-    const val cxf = "3.4.5"
+    const val cxf = "3.5.0"
     const val flyingSaucer = "9.1.22"
     const val fuel = "2.3.1"
     const val mockito = "4.1.0"
