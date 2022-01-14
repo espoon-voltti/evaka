b501b367640d6bd62f2f85741512709462c18c96
Henri Ruoho <henri.ruoho@reaktor.com>
2022-01-14 19:27:56 +0200

    Fix migration file for temporary fees

--- service/src/main/resources/db/migration/V197__dev_temporary_fees.sql
+++ service/src/main/resources/db/migration/V197__dev_temporary_fees.sql
@@ -1,10 +1,16 @@
 ALTER TABLE fee_thresholds
-ADD COLUMN temporary_fee int NOT NULL,
-ADD COLUMN temporary_fee_part_day int NOT NULL,
-ADD COLUMN temporary_fee_sibling int NOT NULL,
-ADD COLUMN temporary_fee_sibling_part_day int NOT NULL;
+ADD COLUMN temporary_fee int,
+ADD COLUMN temporary_fee_part_day int,
+ADD COLUMN temporary_fee_sibling int,
+ADD COLUMN temporary_fee_sibling_part_day int;
 
 UPDATE fee_thresholds SET temporary_fee = 2800 WHERE temporary_fee IS NULL;
 UPDATE fee_thresholds SET temporary_fee_part_day = 1500 WHERE temporary_fee_part_day IS NULL;
 UPDATE fee_thresholds SET temporary_fee_sibling = 1500 WHERE temporary_fee_sibling IS NULL;
 UPDATE fee_thresholds SET temporary_fee_sibling_part_day = 800 WHERE temporary_fee_sibling_part_day IS NULL;
+
+ALTER TABLE fee_thresholds
+ALTER COLUMN temporary_fee SET NOT NULL,
+ALTER COLUMN temporary_fee_part_day SET NOT NULL,
+ALTER COLUMN temporary_fee_sibling SET NOT NULL,
+ALTER COLUMN temporary_fee_sibling_part_day SET NOT NULL;
