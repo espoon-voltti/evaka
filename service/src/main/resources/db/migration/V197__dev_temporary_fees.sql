bbda4bd8a5b0731098e436ef1556e708e86bcec4
Henri Ruoho <henri.ruoho@reaktor.com>
2022-01-17 16:27:00 +0200

    Fix wrong value in temporary_fee migration

--- service/src/main/resources/db/migration/V197__dev_temporary_fees.sql
+++ service/src/main/resources/db/migration/V197__dev_temporary_fees.sql
@@ -4,7 +4,7 @@ ADD COLUMN temporary_fee_part_day int,
 ADD COLUMN temporary_fee_sibling int,
 ADD COLUMN temporary_fee_sibling_part_day int;
 
-UPDATE fee_thresholds SET temporary_fee = 2800 WHERE temporary_fee IS NULL;
+UPDATE fee_thresholds SET temporary_fee = 2900 WHERE temporary_fee IS NULL;
 UPDATE fee_thresholds SET temporary_fee_part_day = 1500 WHERE temporary_fee_part_day IS NULL;
 UPDATE fee_thresholds SET temporary_fee_sibling = 1500 WHERE temporary_fee_sibling IS NULL;
 UPDATE fee_thresholds SET temporary_fee_sibling_part_day = 800 WHERE temporary_fee_sibling_part_day IS NULL;
