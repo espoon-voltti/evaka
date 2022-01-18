6e8ef434068ca181e47b4d89cde02b3e7fd5e1cb
Pasi Huhtiniemi <pasi.huhtiniemi@reaktor.com>
2022-01-18 15:22:29 +0200

    Prettify

--- frontend/src/employee-frontend/components/unit/tab-placement-proposals/PlacementProposalRow.tsx
+++ frontend/src/employee-frontend/components/unit/tab-placement-proposals/PlacementProposalRow.tsx
@@ -129,9 +129,7 @@ export default React.memo(function PlacementProposalRow({
           </Link>
         </Td>
         <Td data-qa="child-dob">{placementPlan.child.dateOfBirth.format()}</Td>
-        <Td data-qa="placement-duration">
-          {placementPlan.period.format()}
-        </Td>
+        <Td data-qa="placement-duration">{placementPlan.period.format()}</Td>
         <Td data-qa="placement-type">
           <CareTypeChip type={placementPlan.type} />
         </Td>
