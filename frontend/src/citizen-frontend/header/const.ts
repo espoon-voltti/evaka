1b38372e45583a442c286462c6f08e0723970dc4
Jonatan Kronqvist <jonatan.kronqvist@reaktor.com>
2022-03-11 13:36:06 +0200

    EVAKA-4162 Make sure the current day is visible in the mobile calendar
    If a holiday banner is active, the current day would be obscured by the
    banner. Here we make sure that there is enough room for the banner above
    the current day.

--- frontend/src/citizen-frontend/header/const.ts
+++ frontend/src/citizen-frontend/header/const.ts
@@ -24,3 +24,4 @@ export const getLogoutUri = (user: User) =>
 export const headerHeightDesktop = 80
 export const headerHeightMobile = 60
 export const bannerHeightDesktop = 66
+export const bannerApproxHeightMobile = 95
