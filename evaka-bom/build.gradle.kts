e7e957dcca01dd8635729ca8e3abcbf369f3eb9d
Joonas Javanainen <joonas.javanainen@reaktor.com>
2022-04-06 12:28:33 +0300

    Upgrade to db-scheduler 11.0

--- evaka-bom/build.gradle.kts
+++ evaka-bom/build.gradle.kts
@@ -25,7 +25,7 @@ javaPlatform {
 dependencies {
     constraints {
         api("com.auth0:java-jwt:3.19.1")
-        api("com.github.kagkarlsson:db-scheduler:10.5")
+        api("com.github.kagkarlsson:db-scheduler:11.0")
         api("com.github.kittinunf.fuel:fuel:${Version.fuel}")
         api("com.github.kittinunf.fuel:fuel-jackson:${Version.fuel}")
         api("com.google.guava:guava:30.1.1-jre")
