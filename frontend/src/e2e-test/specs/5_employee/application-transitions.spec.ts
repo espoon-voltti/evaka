1d82f1c4bcbfd0e2d36858808d0260d8a6ff906e
Joonas Javanainen <joonas.javanainen@reaktor.com>
2022-04-25 14:49:51 +0300

    Make sure service need defaults exist in application tests
    Some application state transitions trigger async jobs that require this
    data. Even if we don't really care about the jobs, their failures can
    get propagated to the test, so it's better to make sure the jobs work.

--- frontend/src/e2e-test/specs/5_employee/application-transitions.spec.ts
+++ frontend/src/e2e-test/specs/5_employee/application-transitions.spec.ts
@@ -46,6 +46,7 @@ beforeEach(async () => {
   await cleanUpMessages()
   fixtures = await initializeAreaAndPersonData()
   serviceWorker = (await Fixture.employeeServiceWorker().save()).data
+  await insertDefaultServiceNeedOptions()
 
   page = await Page.open()
   applicationWorkbench = new ApplicationWorkbenchPage(page)
@@ -167,7 +168,6 @@ describe('Application transitions', () => {
 
   test('Placement dialog works', async () => {
     const preferredStartDate = LocalDate.of(2021, 8, 16)
-    await insertDefaultServiceNeedOptions()
 
     const group = await Fixture.daycareGroup()
       .with({ daycareId: fixtures.daycareFixture.id })
