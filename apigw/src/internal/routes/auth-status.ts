51eddd2f7f1dc371451e97d6605d8b83eecb0078
Joonas Javanainen <joonas.javanainen@reaktor.com>
2022-04-29 12:01:11 +0300

    Refactor internal gw auth status implementation
    - make flow more readable: 1. read data, 2. side effects, 3. response
    - improve type safety

--- apigw/src/internal/routes/auth-status.ts
+++ apigw/src/internal/routes/auth-status.ts
@@ -2,56 +2,83 @@
 //
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
+import express from 'express'
 import { eq } from 'lodash'
 import { toRequestHandler } from '../../shared/express'
 import {
   getEmployeeDetails,
-  getMobileDevice
+  getMobileDevice,
+  UUID
 } from '../../shared/service-client'
 import { logoutExpress, saveSession } from '../../shared/session'
 import { fromCallback } from '../../shared/promise-utils'
 import { appCommit } from '../../shared/config'
 
-export default toRequestHandler(async (req, res) => {
+interface AuthStatus {
+  loggedIn: boolean
+  user?: EmployeeUser | MobileUser
+  roles?: string[]
+  globalRoles?: string[]
+  allScopedRoles?: string[]
+  apiVersion: string
+}
+
+interface EmployeeUser {
+  userType: 'EMPLOYEE'
+  id: UUID
+  name: string
+  accessibleFeatures: object
+  permittedGlobalActions: string[]
+}
+
+interface MobileUser {
+  userType: 'MOBILE'
+  employeeId: UUID | null
+  id: UUID
+  name: string
+  personalDevice: boolean
+  unitIds: UUID[]
+  pinLoginActive: boolean
+}
+
+interface ValidatedUser {
+  user: EmployeeUser | MobileUser
+  globalRoles: string[]
+  allScopedRoles: string[]
+}
+
+async function validateUser(
+  req: express.Request
+): Promise<ValidatedUser | undefined> {
   const user = req.user
-  if (user && user.id) {
-    if (user.userType === 'MOBILE') {
+  if (!user || !user.id) return undefined
+  switch (user.userType) {
+    case 'MOBILE': {
       const device = await getMobileDevice(req, user.id)
       if (!device) {
-        // device has been removed
-        await logoutExpress(req, res, 'employee')
-        res.status(200).json({ loggedIn: false, apiVersion: appCommit })
-      } else {
-        const globalRoles = user.globalRoles ?? []
-        const allScopedRoles = user.allScopedRoles ?? ['MOBILE']
-        const employeeId = device.employeeId ?? user.mobileEmployeeId
-        const pinLoginActive = user.mobileEmployeeId
-        const { id, name, unitIds, personalDevice } = device
-        await saveSession(req)
-        res.status(200).json({
-          loggedIn: true,
-          user: {
-            id,
-            name,
-            userType: user.userType,
-            unitIds,
-            employeeId,
-            pinLoginActive,
-            personalDevice
-          },
-          globalRoles,
-          allScopedRoles,
-          roles: [...globalRoles, ...allScopedRoles],
-          apiVersion: appCommit
-        })
+        return undefined
       }
-    } else {
+      const employeeId = device.employeeId ?? user.mobileEmployeeId ?? null
+      const pinLoginActive = !!user.mobileEmployeeId
+      const { id, name, unitIds, personalDevice } = device
+      return {
+        user: {
+          id,
+          name,
+          userType: 'MOBILE',
+          unitIds,
+          employeeId,
+          pinLoginActive,
+          personalDevice
+        },
+        globalRoles: [],
+        allScopedRoles: ['MOBILE']
+      }
+    }
+    case 'EMPLOYEE': {
       const employee = await getEmployeeDetails(req, user.id)
       if (!employee) {
-        // user id is invalid (e.g. employee has been removed)
-        await logoutExpress(req, res, 'employee')
-        res.status(200).json({ loggedIn: false, apiVersion: appCommit })
-        return
+        return undefined
       }
       const {
         id,
@@ -63,34 +90,55 @@ export default toRequestHandler(async (req, res) => {
         permittedGlobalActions
       } = employee
       const name = [firstName, lastName].filter((x) => !!x).join(' ')
-
-      // Refresh roles if necessary
-      if (
-        !eq(user.globalRoles, globalRoles) ||
-        !eq(user.allScopedRoles, allScopedRoles)
-      ) {
-        await fromCallback((cb) =>
-          req.logIn({ ...user, globalRoles, allScopedRoles }, cb)
-        )
-      }
-
-      await saveSession(req)
-      res.status(200).json({
-        loggedIn: true,
+      return {
         user: {
+          userType: 'EMPLOYEE',
           id,
           name,
-          userType: user.userType,
           accessibleFeatures: accessibleFeatures ?? {},
           permittedGlobalActions: permittedGlobalActions ?? []
         },
         globalRoles,
-        allScopedRoles,
-        roles: [...globalRoles, ...allScopedRoles],
-        apiVersion: appCommit
-      })
+        allScopedRoles
+      }
+    }
+    default:
+      return undefined
+  }
+}
+
+export default toRequestHandler(async (req, res) => {
+  const sessionUser = req.user
+  const validUser = sessionUser && (await validateUser(req))
+  let status: AuthStatus
+  if (validUser) {
+    const { user, globalRoles, allScopedRoles } = validUser
+    // Refresh roles if necessary
+    if (
+      !eq(sessionUser.globalRoles, globalRoles) ||
+      !eq(sessionUser.allScopedRoles, allScopedRoles)
+    ) {
+      await fromCallback((cb) =>
+        req.logIn({ ...sessionUser, globalRoles, allScopedRoles }, cb)
+      )
+    }
+    await saveSession(req)
+    status = {
+      loggedIn: true,
+      user,
+      globalRoles,
+      allScopedRoles,
+      roles: [...globalRoles, ...allScopedRoles],
+      apiVersion: appCommit
     }
   } else {
-    res.status(200).json({ loggedIn: false, apiVersion: appCommit })
+    if (sessionUser) {
+      await logoutExpress(req, res, 'employee')
+    }
+    status = {
+      loggedIn: false,
+      apiVersion: appCommit
+    }
   }
+  res.status(200).json(status)
 })
