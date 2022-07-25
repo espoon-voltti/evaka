02ada79611820597ca18ac737a9918f76602250d
Ilja <ilja-git@users.noreply.github.com>
2022-07-25 13:08:49 +0300

    Testing pull request

--- frontend/build-docker.sh
+++ frontend/build-docker.sh
@@ -11,9 +11,21 @@ cd "$( dirname "${BASH_SOURCE[0]}")"
 rm ./espoo-customizations -rf
 cp ../espoo-customizations . -r
 
-docker build -t evaka/frontend \
-    --build-arg build=0 \
-    --build-arg commit="$(git rev-parse HEAD)" \
-    -f Dockerfile .
+if [ "${1:-}" = "test" ]; then
+    docker build -t evaka/frontend-builder \
+        --target=builder \
+        --build-arg build=0 \
+        --build-arg commit="$(git rev-parse HEAD)" \
+        -f Dockerfile .
+
+    docker run --rm evaka/frontend-builder:latest yarn lint
+    docker run --rm evaka/frontend-builder:latest yarn type-check
+    docker run --rm evaka/frontend-builder:latest yarn test --maxWorkers=2
+else
+    docker build -t evaka/frontend \
+        --build-arg build=0 \
+        --build-arg commit="$(git rev-parse HEAD)" \
+        -f Dockerfile .
+fi
 
 rm ./espoo-customizations -r
