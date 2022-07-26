ff7a3c52a6961f420a6d9841721d910672e52cba
Joonas Javanainen <joonas.javanainen@reaktor.com>
2022-07-26 10:56:12 +0300

    Upgrade to Flyway 9.0.1

--- service/buildSrc/src/main/kotlin/Version.kt
+++ service/buildSrc/src/main/kotlin/Version.kt
@@ -7,7 +7,7 @@ object Version {
     const val ktlint = "0.45.1"
 
     object GradlePlugin {
-        const val flyway = "8.5.11"
+        const val flyway = "9.0.1"
         const val kotlin = "1.7.10"
         const val kotlinter = "3.10.0"
         const val owasp = "7.1.1"
