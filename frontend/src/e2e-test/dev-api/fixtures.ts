78e966774e8614226036f499704314b09d6084aa
Joel Ahlstedt <joel.ahlstedt@reaktor.com>
2022-08-16 15:38:15 +0300

    Add prefer-arrow-callback eslint rule

--- frontend/src/e2e-test/dev-api/fixtures.ts
+++ frontend/src/e2e-test/dev-api/fixtures.ts
@@ -1032,7 +1032,7 @@ export const DecisionIncomeFixture = (total: number): DecisionIncome => ({
 export const nullUUID = '00000000-0000-0000-0000-000000000000'
 
 export const uuidv4 = (): string => {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
     const r = (Math.random() * 16) | 0,
       v = c == 'x' ? r : (r & 0x3) | 0x8
     return v.toString(16)
