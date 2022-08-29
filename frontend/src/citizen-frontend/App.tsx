2bf6ada2e77fb761abe18f175ea64a17dd67fe6c
Aleksi Ahonen <aleksi.ahonen@reaktor.com>
2022-08-29 10:55:32 +0300

    Add min-height to citizen FullPageContainer

--- frontend/src/citizen-frontend/App.tsx
+++ frontend/src/citizen-frontend/App.tsx
@@ -90,6 +90,7 @@ export default function App() {
 const FullPageContainer = styled.div`
   display: flex;
   flex-direction: column;
+  min-height: 100vh;
 `
 
 const Content = React.memo(function Content() {
