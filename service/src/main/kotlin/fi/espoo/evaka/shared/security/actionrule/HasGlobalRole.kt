ee549d7321a926d17b594930053aa295b75b73cf
Joonas Javanainen <joonas.javanainen@reaktor.com>
2022-09-06 17:20:49 +0300

    Convert HasGlobalRole rules to use QueryFragment

--- service/src/main/kotlin/fi/espoo/evaka/shared/security/actionrule/HasGlobalRole.kt
+++ service/src/main/kotlin/fi/espoo/evaka/shared/security/actionrule/HasGlobalRole.kt
@@ -7,15 +7,17 @@ package fi.espoo.evaka.shared.security.actionrule
 import fi.espoo.evaka.shared.AssistanceNeedDecisionId
 import fi.espoo.evaka.shared.AttachmentId
 import fi.espoo.evaka.shared.ChildId
+import fi.espoo.evaka.shared.DatabaseTable
+import fi.espoo.evaka.shared.Id
 import fi.espoo.evaka.shared.auth.AuthenticatedUser
 import fi.espoo.evaka.shared.auth.UserRole
-import fi.espoo.evaka.shared.db.Database
+import fi.espoo.evaka.shared.db.QueryFragment
 import fi.espoo.evaka.shared.domain.HelsinkiDateTime
 import fi.espoo.evaka.shared.security.AccessControlDecision
 import fi.espoo.evaka.shared.utils.toEnumSet
 import java.util.EnumSet
 
-private typealias Filter<T> = (tx: Database.Read, user: AuthenticatedUser.Employee, now: HelsinkiDateTime, targets: Set<T>) -> Iterable<T>
+private typealias Filter<T> = (user: AuthenticatedUser.Employee, now: HelsinkiDateTime) -> QueryFragment<T>
 
 data class HasGlobalRole(val oneOf: EnumSet<UserRole>) : StaticActionRule {
     init {
@@ -28,21 +30,47 @@ data class HasGlobalRole(val oneOf: EnumSet<UserRole>) : StaticActionRule {
             AccessControlDecision.Permitted(this)
         } else AccessControlDecision.None
 
-    private fun <T> rule(filter: Filter<T>): DatabaseActionRule.Scoped<T, HasGlobalRole> =
+    private fun <T : Id<*>> rule(filter: Filter<T>): DatabaseActionRule.Scoped<T, HasGlobalRole> =
         DatabaseActionRule.Scoped.Simple(this, Query(filter))
-    data class Query<T>(private val filter: Filter<T>) : DatabaseActionRule.Scoped.Query<T, HasGlobalRole> {
+    data class Query<T : Id<*>>(private val filter: Filter<T>) : DatabaseActionRule.Scoped.Query<T, HasGlobalRole> {
         override fun executeWithTargets(
             ctx: DatabaseActionRule.QueryContext,
             targets: Set<T>
         ): Map<T, DatabaseActionRule.Deferred<HasGlobalRole>> = when (ctx.user) {
-            is AuthenticatedUser.Employee -> filter(ctx.tx, ctx.user, ctx.now, targets).associateWith { Deferred(ctx.user.globalRoles) }
+            is AuthenticatedUser.Employee -> filter(ctx.user, ctx.now).let { subquery ->
+                ctx.tx.createQuery(
+                    QueryFragment<Any>(
+                        """
+                    SELECT id
+                    FROM (${subquery.sql}) fragment
+                    WHERE id = ANY(:ids)
+                        """.trimIndent(),
+                        subquery.bindings
+                    )
+                )
+                    .bind("ids", targets.map { it.raw })
+                    .mapTo<Id<DatabaseTable>>()
+                    .toSet()
+            }.let { matched ->
+                targets.filter { matched.contains(it) }.associateWith { Deferred(ctx.user.globalRoles) }
+            }
             else -> emptyMap()
         }
 
         override fun executeWithParams(
             ctx: DatabaseActionRule.QueryContext,
             params: HasGlobalRole
-        ): AccessControlFilter<T>? = TODO("unsupported for this rule type")
+        ): AccessControlFilter<T>? = when (ctx.user) {
+            is AuthenticatedUser.Employee -> if (ctx.user.globalRoles.any { params.oneOf.contains(it) }) {
+                filter(ctx.user, ctx.now).let { subquery ->
+                    ctx.tx.createQuery(subquery)
+                        .mapTo<Id<DatabaseTable>>()
+                        .toSet()
+                        .let { ids -> AccessControlFilter.Some(ids) }
+                }
+            } else null
+            else -> null
+        }
     }
     private class Deferred(private val globalRoles: Set<UserRole>) : DatabaseActionRule.Deferred<HasGlobalRole> {
         override fun evaluate(params: HasGlobalRole): AccessControlDecision = if (globalRoles.any { params.oneOf.contains(it) }) {
@@ -52,61 +80,47 @@ data class HasGlobalRole(val oneOf: EnumSet<UserRole>) : StaticActionRule {
         }
     }
 
-    fun andAttachmentWasUploadedByAnyEmployee() = rule<AttachmentId> { tx, _, _, ids ->
-        tx.createQuery(
+    fun andAttachmentWasUploadedByAnyEmployee() = rule { _, _ ->
+        QueryFragment<AttachmentId>(
             """
 SELECT attachment.id
 FROM attachment
 JOIN evaka_user ON uploaded_by = evaka_user.id
-WHERE attachment.id = ANY(:ids)
-AND evaka_user.type = 'EMPLOYEE'
+WHERE evaka_user.type = 'EMPLOYEE'
             """.trimIndent()
         )
-            .bind("ids", ids)
-            .mapTo()
     }
 
-    fun andIsDecisionMakerForAssistanceNeedDecision() = rule { tx, employee, _, ids ->
-        tx.createQuery(
+    fun andIsDecisionMakerForAssistanceNeedDecision() = rule { employee, _ ->
+        QueryFragment<AssistanceNeedDecisionId>(
             """
 SELECT id
 FROM assistance_need_decision
 WHERE decision_maker_employee_id = :employeeId
-  AND id = ANY(:ids)
-  AND sent_for_decision IS NOT NULL
+AND sent_for_decision IS NOT NULL
             """.trimIndent()
         )
             .bind("employeeId", employee.id)
-            .bind("ids", ids)
-            .mapTo<AssistanceNeedDecisionId>()
-            .toSet()
     }
 
-    fun andAssistanceNeedDecisionHasBeenSent() = rule { tx, _, _, ids ->
-        tx.createQuery(
+    fun andAssistanceNeedDecisionHasBeenSent() = rule { _, _ ->
+        QueryFragment<AssistanceNeedDecisionId>(
             """
 SELECT id
 FROM assistance_need_decision
-WHERE id = ANY(:ids)
-  AND sent_for_decision IS NOT NULL
+WHERE sent_for_decision IS NOT NULL
             """.trimIndent()
         )
-            .bind("ids", ids)
-            .mapTo<AssistanceNeedDecisionId>()
-            .toSet()
     }
 
-    fun andChildHasServiceVoucherPlacement() = rule<ChildId> { tx, _, _, ids ->
-        tx.createQuery(
+    fun andChildHasServiceVoucherPlacement() = rule { _, _ ->
+        QueryFragment<ChildId>(
             """
-SELECT p.child_id
+SELECT p.child_id AS id
 FROM placement p
 JOIN daycare pd ON pd.id = p.unit_id
-WHERE p.child_id = ANY(:ids)
-  AND pd.provider_type = 'PRIVATE_SERVICE_VOUCHER'
+WHERE pd.provider_type = 'PRIVATE_SERVICE_VOUCHER'
             """.trimIndent()
         )
-            .bind("ids", ids)
-            .mapTo()
     }
 }
