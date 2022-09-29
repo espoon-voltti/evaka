51e191b0b99e54e11bda7baffe20ec18c0a24274
Aleksi Ahonen <aleksi.ahonen@reaktor.com>
2022-09-29 13:58:47 +0300

    Take foster parents into account in citizen children query

--- service/src/main/kotlin/fi/espoo/evaka/children/ChildrenQueries.kt
+++ service/src/main/kotlin/fi/espoo/evaka/children/ChildrenQueries.kt
@@ -8,9 +8,14 @@ import fi.espoo.evaka.shared.PersonId
 import fi.espoo.evaka.shared.db.Database
 import java.time.LocalDate
 
-fun Database.Read.getChildrenByGuardian(id: PersonId, today: LocalDate): List<Child> =
+fun Database.Read.getChildrenByParent(id: PersonId, today: LocalDate): List<Child> =
     this.createQuery(
         """
+WITH children AS (
+    SELECT child_id FROM guardian WHERE guardian_id = :userId
+    UNION ALL
+    SELECT child_id FROM foster_parent WHERE parent_id = :userId AND valid_during @> :today
+)
 SELECT
     p.id,
     p.first_name,
@@ -22,13 +27,12 @@ SELECT
     EXISTS (SELECT 1 FROM placement pl WHERE p.id = pl.child_id AND :today <= pl.end_date) AS has_upcoming_placements,
     EXISTS (SELECT 1 FROM pedagogical_document doc WHERE p.id = doc.child_id) has_pedagogical_documents,
     EXISTS (SELECT 1 FROM curriculum_document doc WHERE p.id = doc.child_id) has_curriculums
-FROM guardian g
-INNER JOIN person p ON g.child_id = p.id
+FROM children c
+INNER JOIN person p ON c.child_id = p.id
 LEFT JOIN child_images img ON p.id = img.child_id
 LEFT JOIN placement pl ON pl.child_id = p.id AND daterange(pl.start_date, pl.end_date, '[]') @> :today::date
 LEFT JOIN daycare_group_placement dgp ON pl.id = dgp.daycare_placement_id AND daterange(dgp.start_date, dgp.end_date, '[]') @> :today::date
 LEFT JOIN daycare_group dg ON dgp.daycare_group_id = dg.id
-WHERE g.guardian_id = :userId
 ORDER BY p.date_of_birth, p.last_name, p.first_name
         """.trimIndent()
     )
