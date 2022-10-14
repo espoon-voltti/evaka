3614677fdc8b1c94e60ce748071faf30923658fb
Petri Lehtinen <petri.lehtinen@reaktor.com>
2022-10-14 07:59:37 +0300

    Upgrade ktlint and kotlinter gradle plugin

--- service/buildSrc/src/main/kotlin/Version.kt
+++ service/buildSrc/src/main/kotlin/Version.kt
@@ -4,12 +4,12 @@
 
 object Version {
     const val java = "17"
-    const val ktlint = "0.45.1"
+    const val ktlint = "0.47.1"
 
     object GradlePlugin {
         const val flyway = "9.4.0"
         const val kotlin = "1.7.20"
-        const val kotlinter = "3.10.0"
+        const val kotlinter = "3.12.0"
         const val owasp = "7.2.1"
         const val springBoot = "2.7.2"
         const val versions = "0.42.0"
