b4e5f80dd6ae637ba39460c9f2df9e88c817ef44
Jarkko Pätäri <jarkko.patari@gofore.com>
2023-01-04 09:12:00 +0200

    Add preparatory feature flag support for citizen application view

--- frontend/src/citizen-frontend/applications/editor/verification/ServiceNeedSection.tsx
+++ frontend/src/citizen-frontend/applications/editor/verification/ServiceNeedSection.tsx
@@ -10,6 +10,7 @@ import { ApplicationType } from 'lib-common/generated/api-types/application'
 import ListGrid from 'lib-components/layout/ListGrid'
 import { H2, H3, Label } from 'lib-components/typography'
 import { Gap } from 'lib-components/white-space'
+import { featureFlags } from 'lib-customizations/citizen'
 
 import { useTranslation } from '../../../localization'
 
@@ -139,7 +140,9 @@ export default React.memo(function ServiceNeedSection({
           </>
         )}
 
-        {type === 'PRESCHOOL' && <ServiceNeedPreparatory formData={formData} />}
+        {type === 'PRESCHOOL' && featureFlags.preparatory && (
+          <ServiceNeedPreparatory formData={formData} />
+        )}
       </ListGrid>
     </div>
   )
