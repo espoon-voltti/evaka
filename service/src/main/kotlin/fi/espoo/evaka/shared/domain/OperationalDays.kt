1563133238508e0cb0e2d9654e5090dca8683f6d
Joonas Javanainen <joonas.javanainen@reaktor.com>
2023-01-11 14:27:42 +0200

    Expose predicate instead of filter function
    This is much more flexible and the function works well even for single
    dates and other things than sequences

--- service/src/main/kotlin/fi/espoo/evaka/shared/domain/OperationalDays.kt
+++ service/src/main/kotlin/fi/espoo/evaka/shared/domain/OperationalDays.kt
@@ -11,15 +11,10 @@ import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.Month
 
-fun operationalDates(
-    dates: Sequence<LocalDate>,
-    operationalDays: Set<Int>,
-    holidays: Set<LocalDate>
-): Sequence<LocalDate> =
-    dates
-        .filter { operationalDays.contains(it.dayOfWeek.value) }
+fun LocalDate.isOperationalDate(operationalDays: Set<DayOfWeek>, holidays: Set<LocalDate>) =
+    operationalDays.contains(dayOfWeek) &&
         // Units that are operational every day of the week are also operational during holidays
-        .filter { operationalDays.size == 7 || !holidays.contains(it) }
+        (operationalDays.size == 7 || !holidays.contains(this))
 
 data class OperationalDays(
     val fullMonth: List<LocalDate>,
@@ -42,7 +37,7 @@ fun Database.Read.operationalDays(year: Int, month: Month): OperationalDays {
                 "SELECT id, operation_days FROM daycare WHERE NOT (operation_days @> '{1,2,3,4,5}' AND operation_days <@ '{1,2,3,4,5}')"
             )
             .map { row ->
-                row.mapColumn<DaycareId>("id") to row.mapColumn<Set<Int>>("operation_days")
+                row.mapColumn<DaycareId>("id") to row.mapColumn<Set<Int>>("operation_days").map { DayOfWeek.of(it) }.toSet()
             }
             .toList()
 
@@ -56,7 +51,7 @@ fun Database.Read.operationalDays(year: Int, month: Month): OperationalDays {
 
     val specialCases =
         specialUnitOperationalDays.associate { (unitId, operationalDays) ->
-            unitId to operationalDates(daysOfMonth, operationalDays, holidays).toList()
+            unitId to daysOfMonth.filter { it.isOperationalDate(operationalDays, holidays) }.toList()
         }
 
     return OperationalDays(daysOfMonth.toList(), generalCase, specialCases)
