7043b9373ba00ebeca6cd12882d0f7af7d81feea
Joonas Javanainen <joonas.javanainen@reaktor.com>
2023-01-31 13:40:22 +0200

    Include shibboleth parent group

--- service/settings.gradle.kts
+++ service/settings.gradle.kts
@@ -17,6 +17,7 @@ dependencyResolutionManagement {
         mavenCentral()
         maven("https://build.shibboleth.net/maven/releases") {
             content {
+                includeGroup("net.shibboleth")
                 includeGroup("net.shibboleth.utilities")
                 includeGroup("org.opensaml")
             }
