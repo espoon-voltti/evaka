ab8e51615d325ce480f15f1b2b53bc3ff1efc836
Petri Lehtinen <petri.lehtinen@reaktor.com>
2023-03-16 11:08:29 +0200

    Functional helpers for ValidationResult

--- frontend/src/lib-common/form/types.ts
+++ frontend/src/lib-common/form/types.ts
@@ -60,6 +60,15 @@ export class ValidationSuccess<Output, Error> {
     return ValidationSuccess.of(fn(this.value))
   }
 
+  apply<T, E>(
+    f: ValidationResult<(v: Output) => T, E>
+  ): ValidationResult<T, Error | E> {
+    if (f.isValid) {
+      return new ValidationSuccess(f.value(this.value))
+    }
+    return f as unknown as ValidationResult<T, Error | E>
+  }
+
   chain<T, E>(
     fn: (value: Output) => ValidationResult<T, E>
   ): ValidationResult<T, Error | E> {
@@ -82,6 +91,12 @@ export class ValidationError<Output, Error> {
     return this as unknown as ValidationError<T, Error>
   }
 
+  apply<T, E>(
+    _f: ValidationResult<(v: Output) => T, E>
+  ): ValidationResult<T, Error | E> {
+    return this as unknown as ValidationResult<T, Error | E>
+  }
+
   chain<T, E>(
     _fn: (value: Output) => ValidationResult<T, E>
   ): ValidationResult<T, Error | E> {
@@ -92,3 +107,150 @@ export class ValidationError<Output, Error> {
 export type ValidationResult<Output, Error> =
   | ValidationSuccess<Output, Error>
   | ValidationError<Output, Error>
+
+export function map<AO, BO, O, AE, BE>(
+  ar: ValidationResult<AO, AE>,
+  br: ValidationResult<BO, BE>,
+  fn: (a: AO, b: BO) => O
+): ValidationResult<O, AE | BE>
+export function map<AO, BO, CO, O, AE, BE, CE>(
+  ar: ValidationResult<AO, AE>,
+  br: ValidationResult<BO, BE>,
+  cr: ValidationResult<CO, CE>,
+  fn: (a: AO, b: BO, c: CO) => O
+): ValidationResult<O, AE | BE | CE>
+export function map<AO, BO, CO, DO, O, AE, BE, CE, DE>(
+  ar: ValidationResult<AO, AE>,
+  br: ValidationResult<BO, BE>,
+  cr: ValidationResult<CO, CE>,
+  dr: ValidationResult<DO, DE>,
+  fn: (a: AO, b: BO, c: CO, d: DO) => O
+): ValidationResult<O, AE | BE | CE | DE>
+export function map<AO, BO, CO, DO, EO, O, AE, BE, CE, DE, EE>(
+  ar: ValidationResult<AO, AE>,
+  br: ValidationResult<BO, BE>,
+  cr: ValidationResult<CO, CE>,
+  dr: ValidationResult<DO, DE>,
+  er: ValidationResult<EO, EE>,
+  fn: (a: AO, b: BO, c: CO, d: DO, e: EO) => O
+): ValidationResult<O, AE | BE | CE | DE | EE>
+export function map<AO, BO, CO, DO, EO, O, AE, BE, CE, DE, EE>(
+  ...args: unknown[]
+): ValidationResult<unknown, unknown> {
+  switch (args.length) {
+    case 3: {
+      const [ar, br, fn] = args as [
+        ValidationResult<AO, AE>,
+        ValidationResult<BO, BE>,
+        (a: AO, b: BO) => O
+      ]
+      const f = (a: AO) => (b: BO) => fn(a, b)
+      return br.apply(ar.map(f))
+    }
+    case 4: {
+      const [ar, br, cr, fn] = args as [
+        ValidationResult<AO, AE>,
+        ValidationResult<BO, BE>,
+        ValidationResult<CO, CE>,
+        (a: AO, b: BO, c: CO) => O
+      ]
+      const f = (a: AO) => (b: BO) => (c: CO) => fn(a, b, c)
+      return cr.apply(br.apply(ar.map(f)))
+    }
+    case 5: {
+      const [ar, br, cr, dr, fn] = args as [
+        ValidationResult<AO, AE>,
+        ValidationResult<BO, BE>,
+        ValidationResult<CO, CE>,
+        ValidationResult<DO, DE>,
+        (a: AO, b: BO, c: CO, d: DO) => O
+      ]
+      const f = (a: AO) => (b: BO) => (c: CO) => (d: DO) => fn(a, b, c, d)
+      return dr.apply(cr.apply(br.apply(ar.map(f))))
+    }
+    case 6: {
+      const [ar, br, cr, dr, er, fn] = args as [
+        ValidationResult<AO, AE>,
+        ValidationResult<BO, BE>,
+        ValidationResult<CO, CE>,
+        ValidationResult<DO, DE>,
+        ValidationResult<EO, EE>,
+        (a: AO, b: BO, c: CO, d: DO, e: EO) => O
+      ]
+      const f = (a: AO) => (b: BO) => (c: CO) => (d: DO) => (e: EO) =>
+        fn(a, b, c, d, e)
+      return er.apply(dr.apply(cr.apply(br.apply(ar.map(f)))))
+    }
+  }
+  throw new Error('not reached')
+}
+
+export function combine<AO, BO, AE, BE>(
+  ar: ValidationResult<AO, AE>,
+  br: ValidationResult<BO, BE>
+): ValidationResult<[AO, BO], AE | BE>
+export function combine<AO, BO, CO, AE, BE, CE>(
+  ar: ValidationResult<AO, AE>,
+  br: ValidationResult<BO, BE>,
+  cr: ValidationResult<CO, CE>
+): ValidationResult<[AO, BO, CO], AE | BE | CE>
+export function combine<AO, BO, CO, DO, AE, BE, CE, DE>(
+  ar: ValidationResult<AO, AE>,
+  br: ValidationResult<BO, BE>,
+  cr: ValidationResult<CO, CE>,
+  dr: ValidationResult<DO, DE>
+): ValidationResult<[AO, BO, CO, DO], AE | BE | CE | DE>
+export function combine<AO, BO, CO, DO, EO, AE, BE, CE, DE, EE>(
+  ar: ValidationResult<AO, AE>,
+  br: ValidationResult<BO, BE>,
+  cr: ValidationResult<CO, CE>,
+  dr: ValidationResult<DO, DE>,
+  er: ValidationResult<EO, EE>
+): ValidationResult<[AO, BO, CO, DO, EO], AE | BE | CE | DE | EE>
+export function combine<AO, BO, CO, DO, EO, AE, BE, CE, DE, EE>(
+  ...args: unknown[]
+): ValidationResult<unknown, unknown> {
+  switch (args.length) {
+    case 2: {
+      const [ar, br] = args as [
+        ValidationResult<AO, AE>,
+        ValidationResult<BO, BE>
+      ]
+      return map(ar, br, (a, b): [AO, BO] => [a, b])
+    }
+    case 3: {
+      const [ar, br, cr] = args as [
+        ValidationResult<AO, AE>,
+        ValidationResult<BO, BE>,
+        ValidationResult<CO, CE>
+      ]
+      return map(ar, br, cr, (a, b, c): [AO, BO, CO] => [a, b, c])
+    }
+    case 4: {
+      const [ar, br, cr, dr] = args as [
+        ValidationResult<AO, AE>,
+        ValidationResult<BO, BE>,
+        ValidationResult<CO, CE>,
+        ValidationResult<DO, DE>
+      ]
+      return map(ar, br, cr, dr, (a, b, c, d): [AO, BO, CO, DO] => [a, b, c, d])
+    }
+    case 5: {
+      const [ar, br, cr, dr, er] = args as [
+        ValidationResult<AO, AE>,
+        ValidationResult<BO, BE>,
+        ValidationResult<CO, CE>,
+        ValidationResult<DO, DE>,
+        ValidationResult<EO, EE>
+      ]
+      return map(ar, br, cr, dr, er, (a, b, c, d, e): [AO, BO, CO, DO, EO] => [
+        a,
+        b,
+        c,
+        d,
+        e
+      ])
+    }
+  }
+  throw new Error('not reached')
+}
