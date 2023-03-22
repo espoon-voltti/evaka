38eb43391a422b0b3a2e728d48b67e9688a93ea0
Petri Lehtinen <petri.lehtinen@reaktor.com>
2023-03-22 11:47:45 +0200

    Memoize the last value instead of all of them

--- frontend/src/lib-common/form/form.ts
+++ frontend/src/lib-common/form/form.ts
@@ -2,8 +2,7 @@
 //
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
-import memoize from 'lodash/memoize'
-
+import { memoizeLast } from './memoize'
 import {
   AnyForm,
   ErrorOf,
@@ -18,7 +17,7 @@ import {
 
 export function value<T>(): Form<T, never, T, unknown> {
   return {
-    validate: memoize((state: T) => ValidationSuccess.of(state)),
+    validate: memoizeLast((state: T) => ValidationSuccess.of(state)),
     shape: undefined
   }
 }
@@ -43,7 +42,7 @@ export function object<Fields extends AnyObjectFields>(
   Fields
 > {
   return {
-    validate: memoize((state: ObjectState<Fields>) => {
+    validate: memoizeLast((state: ObjectState<Fields>) => {
       const allFields = Object.entries(fields)
       const validValues = allFields.flatMap(([k, field]) => {
         const validationResult = field.validate(state[k])
@@ -74,7 +73,7 @@ export function array<Elem extends AnyForm>(
   Elem
 > {
   return {
-    validate: memoize((state: StateOf<Elem>[]) => {
+    validate: memoizeLast((state: StateOf<Elem>[]) => {
       const valid = state.flatMap((elemState) => {
         const validationResult = elem.validate(elemState)
         return validationResult.isValid
@@ -106,7 +105,7 @@ export function chained<
   ) => ValidationResult<VOutput, VError>
 ): Form<VOutput, Error | VError, State, Shape> {
   return {
-    validate: memoize((state: State) => mapper(form, state)),
+    validate: memoizeLast((state: State) => mapper(form, state)),
     shape: form.shape
   }
 }
@@ -123,7 +122,9 @@ export function transformed<
   transform: (output: Output) => ValidationResult<VOutput, VError>
 ): Form<VOutput, Error | VError, State, Shape> {
   return {
-    validate: memoize((state: State) => form.validate(state).chain(transform)),
+    validate: memoizeLast((state: State) =>
+      form.validate(state).chain(transform)
+    ),
     shape: form.shape
   }
 }
@@ -146,7 +147,7 @@ export function validated<
   validator: (output: Output) => VError | undefined
 ): Form<Output, Error | VError, State, Shape> {
   return {
-    validate: memoize((state: State) =>
+    validate: memoizeLast((state: State) =>
       form.validate(state).chain((value) => {
         const validationError = validator(value)
         if (validationError !== undefined) {
@@ -185,7 +186,7 @@ export type OneOf<Output, Error extends string = string> = Form<
 
 export function oneOf<Output>(): OneOf<Output, never> {
   return {
-    validate: memoize((state) =>
+    validate: memoizeLast((state) =>
       ValidationSuccess.of(
         state.options.find((o) => o.domValue === state.domValue)?.value
       )
