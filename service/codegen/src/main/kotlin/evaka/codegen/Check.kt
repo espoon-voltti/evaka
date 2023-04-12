9a7cbc43ae3d39d94eb722e7e6f6519488c24479
Joonas Javanainen <joonas.javanainen@reaktor.com>
2023-04-12 14:46:00 +0300

    Validate some basic HTTP endpoint rules

--- service/codegen/src/main/kotlin/evaka/codegen/Check.kt
+++ service/codegen/src/main/kotlin/evaka/codegen/Check.kt
@@ -7,7 +7,9 @@ package evaka.codegen
 import ch.qos.logback.classic.Level
 import ch.qos.logback.classic.LoggerContext
 import evaka.codegen.actionenum.checkGeneratedActionEnumTypes
+import evaka.codegen.apitypes.basePackage
 import evaka.codegen.apitypes.checkGeneratedApiTypes
+import evaka.codegen.apitypes.scanEndpoints
 import kotlin.io.path.div
 import org.slf4j.LoggerFactory
 
@@ -17,4 +19,5 @@ fun main() {
     checkGeneratedActionEnumTypes(generatedPath)
     checkGeneratedApiTypes(generatedPath / "api-types")
     checkLanguages(generatedPath / "language.ts")
+    scanEndpoints(basePackage).forEach { it.validate() }
 }
