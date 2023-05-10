4c2ac6f526b10530e5dda9eced8859968c619495
Joonas Javanainen <joonas.javanainen@reaktor.com>
2023-05-10 14:04:49 +0300

    Upgrade to AWS SDK 2.20.63

--- evaka-bom/build.gradle.kts
+++ evaka-bom/build.gradle.kts
@@ -66,5 +66,5 @@ dependencies {
     api(platform(libs.kotlin.bom))
     api(platform("org.junit:junit-bom:5.9.2"))
     api(platform(libs.spring.boot.dependencies))
-    api(platform("software.amazon.awssdk:bom:2.19.29"))
+    api(platform("software.amazon.awssdk:bom:2.20.63"))
 }
