56285f32d608dde08919f4e471e0b60ac1d2e9d3
Tatu Repo <tatu.repo@gofore.com>
2023-06-06 14:19:16 +0300

    Added trigger suspension during migration update

--- service/src/main/resources/db/migration/V327__service_need_shift_care_enum.sql
+++ service/src/main/resources/db/migration/V327__service_need_shift_care_enum.sql
@@ -7,9 +7,13 @@ CREATE TYPE shift_care_type AS ENUM (
 ALTER TABLE service_need
     ADD COLUMN shift_care_temp shift_care_type NOT NULL DEFAULT 'NONE'::shift_care_type;
 
+ALTER TABLE service_need DISABLE TRIGGER set_timestamp;
+
 UPDATE service_need
 SET shift_care_temp = 'FULL'::shift_care_type
 WHERE shift_care IS TRUE;
+
+ALTER TABLE service_need ENABLE TRIGGER set_timestamp;
 
 ALTER TABLE service_need
     DROP COLUMN shift_care;
