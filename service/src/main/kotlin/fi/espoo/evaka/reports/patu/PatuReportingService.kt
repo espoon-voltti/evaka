70ddedb85a390d89c17bbe99ee98dafb35e22c88
Joonas Javanainen <joonas.javanainen@reaktor.com>
2023-06-26 16:32:49 +0300

    Add support for new assistance model in reports
    - No tests exist -> no tests were updated :(
    - assistance needs and actions report requires a totally different
      approach so it's not included

--- service/src/main/kotlin/fi/espoo/evaka/reports/patu/PatuReportingService.kt
+++ service/src/main/kotlin/fi/espoo/evaka/reports/patu/PatuReportingService.kt
@@ -4,17 +4,23 @@
 
 package fi.espoo.evaka.reports.patu
 
+import fi.espoo.evaka.EvakaEnv
 import fi.espoo.evaka.reports.REPORT_STATEMENT_TIMEOUT
 import fi.espoo.evaka.reports.getRawRows
 import fi.espoo.evaka.shared.db.Database
 import fi.espoo.evaka.shared.domain.DateRange
 
-class PatuReportingService(private val patuIntegrationClient: PatuIntegrationClient) {
+class PatuReportingService(
+    env: EvakaEnv,
+    private val patuIntegrationClient: PatuIntegrationClient
+) {
+    private val assistanceModel = env.assistanceModel
+
     fun sendPatuReport(dbc: Database.Connection, dateRange: DateRange) {
         val rows =
             dbc.read {
                 it.setStatementTimeout(REPORT_STATEMENT_TIMEOUT)
-                it.getRawRows(dateRange.start, dateRange.end ?: dateRange.start)
+                it.getRawRows(assistanceModel, dateRange.start, dateRange.end ?: dateRange.start)
             }
         patuIntegrationClient.send(rows)
     }
