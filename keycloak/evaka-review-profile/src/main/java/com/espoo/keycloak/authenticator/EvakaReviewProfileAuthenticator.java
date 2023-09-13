8ee4fed72d79733a17fa2e3e339874e620f6724b
Ilja <ilja@kooditon.com>
2023-09-13 17:19:01 +0300

    KeyCloak 22.0.3

--- keycloak/evaka-review-profile/src/main/java/com/espoo/keycloak/authenticator/EvakaReviewProfileAuthenticator.java
+++ keycloak/evaka-review-profile/src/main/java/com/espoo/keycloak/authenticator/EvakaReviewProfileAuthenticator.java
@@ -42,6 +42,8 @@ import org.keycloak.userprofile.UserProfileProvider;
 import jakarta.ws.rs.core.MultivaluedMap;
 import jakarta.ws.rs.core.Response;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
@@ -177,7 +179,9 @@ public class EvakaReviewProfileAuthenticator extends AbstractIdpAuthenticator {
         };
 
         UserProfileProvider profileProvider = context.getSession().getProvider(UserProfileProvider.class);
-        UserProfile profile = profileProvider.create(UserProfileContext.IDP_REVIEW, formData, updatedProfile);
+        Map<String, List<String>> attributes = new HashMap<>(formData);
+        attributes.putIfAbsent(UserModel.USERNAME, Collections.singletonList(updatedProfile.getUsername()));
+        UserProfile profile = profileProvider.create(UserProfileContext.IDP_REVIEW, attributes, updatedProfile);
 
         List<FormMessage> errors = null;
         try {
