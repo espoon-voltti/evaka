8d7ce14a169efb0ccf370d77a3cbafff97bd24ae
Joonas Javanainen <joonas.javanainen@reaktor.com>
2023-09-21 12:39:28 +0300

    Don't try to screenshot a closed page
    The page may have closed due to a crash, so the screenshot operation
    will just throw an error and make it harder to find the actual failing
    test in the logs / console output

--- frontend/src/e2e-test/browser.ts
+++ frontend/src/e2e-test/browser.ts
@@ -118,17 +118,22 @@ async function forEachPage(
 
 async function captureScreenshots(namePrefix: string): Promise<void> {
   await forEachPage(async ({ ctxIndex, pageIndex, page }) => {
-    await page.screenshot({
-      type: 'png',
-      path: `screenshots/${namePrefix}.${ctxIndex}.${pageIndex}.png`,
-      timeout: 30_000
-    })
-    await page.screenshot({
-      type: 'png',
-      path: `screenshots/${namePrefix}.${ctxIndex}.full.${pageIndex}.png`,
-      fullPage: true,
-      timeout: 30_000
-    })
+    if (!page.isClosed()) {
+      await page.screenshot({
+        type: 'png',
+        path: `screenshots/${namePrefix}.${ctxIndex}.${pageIndex}.png`,
+        timeout: 30_000
+      })
+    }
+    // check again in case the previous screenshot operation crashed/closed something
+    if (!page.isClosed()) {
+      await page.screenshot({
+        type: 'png',
+        path: `screenshots/${namePrefix}.${ctxIndex}.full.${pageIndex}.png`,
+        fullPage: true,
+        timeout: 30_000
+      })
+    }
   })
 }
 
