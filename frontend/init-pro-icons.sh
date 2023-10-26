f230fecdd8098ba1ca96c00b424a6bcc45a08457
Ilja <ilja@kooditon.com>
2023-10-26 12:21:00 +0300

    Remove init-pro-icons.sh script

--- frontend/init-pro-icons.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -eu
-
-# SPDX-FileCopyrightText: 2017-2020 City of Espoo
-#
-# SPDX-License-Identifier: LGPL-2.1-or-later
-
-aws --profile voltti-local s3 sync s3://evaka-deployment-local/frontend/vendor/fortawesome/ ./vendor/fortawesome/
