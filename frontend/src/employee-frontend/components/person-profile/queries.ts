20a357bba44b2436984f0643664227156afdf3d0
Martin Lehtomaa <martin.lehtomaa@rebase.fi>
2023-11-10 15:23:47 +0200

    Added license headers to new files

--- frontend/src/employee-frontend/components/person-profile/queries.ts
+++ frontend/src/employee-frontend/components/person-profile/queries.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2017-2023 City of Espoo
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
 import { query } from 'lib-common/query'
 
 import { getIncomeCoefficientMultipliers } from '../../api/income'
