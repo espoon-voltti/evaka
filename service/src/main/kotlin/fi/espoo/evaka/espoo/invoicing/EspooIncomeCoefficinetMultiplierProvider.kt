8b0c439b140c87f34403745ca9ef7b1db72e5b8b
Martin Lehtomaa <martin.lehtomaa@rebase.fi>
2023-11-15 10:28:27 +0200

    Added license header

--- service/src/main/kotlin/fi/espoo/evaka/espoo/invoicing/EspooIncomeCoefficinetMultiplierProvider.kt
+++ service/src/main/kotlin/fi/espoo/evaka/espoo/invoicing/EspooIncomeCoefficinetMultiplierProvider.kt
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2017-2023 City of Espoo
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
 package fi.espoo.evaka.espoo.invoicing
 
 import fi.espoo.evaka.invoicing.domain.IncomeCoefficient
