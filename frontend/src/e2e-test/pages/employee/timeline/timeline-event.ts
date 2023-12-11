95ed05cd14604d8035da1ffd63939e5c5f63eb11
Jonni Madekivi <jonni.madekivi@reaktor.com>
2023-12-11 13:10:25 +0200

    Fix review comment

--- frontend/src/e2e-test/pages/employee/timeline/timeline-event.ts
+++ frontend/src/e2e-test/pages/employee/timeline/timeline-event.ts
@@ -3,16 +3,14 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
 import { Element } from '../../../utils/page'
-export class TimelineEvent {
-  constructor(private readonly elem: Element) {}
-
+export class TimelineEvent extends Element {
   async expandEvent() {
-    await this.elem.find('[data-qa="event-expander-button"] button').click()
+    await this.find('[data-qa="event-expander-button"] button').click()
   }
 
   assertMetadataContains(metadataContent: string) {
-    return this.elem
-      .findByDataQa('partner-metadata')
-      .assertText((text) => text.includes(metadataContent))
+    return this.findByDataQa('partner-metadata').assertText((text) =>
+      text.includes(metadataContent)
+    )
   }
 }
