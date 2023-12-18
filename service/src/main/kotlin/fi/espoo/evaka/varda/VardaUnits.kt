eb935ca1252e2f9f26b9289e7ee89ce156157968
Joonas Reynders <joonas.reynders@reaktor.fi>
2023-12-18 08:28:27 +0200

    Field rename in varda units api

--- service/src/main/kotlin/fi/espoo/evaka/varda/VardaUnits.kt
+++ service/src/main/kotlin/fi/espoo/evaka/varda/VardaUnits.kt
@@ -237,7 +237,7 @@ data class VardaUnit(
                     }
                     else -> null
                 },
-            asiointikieli_koodi = listOfNotNull(language.vardaCode),
+            toimintakieli_koodi = listOfNotNull(language.vardaCode),
             kielipainotus_kytkin = languageEmphasisId?.let { true } ?: false
         )
 }
@@ -265,6 +265,6 @@ data class VardaUnitRequest(
     val kasvatusopillinen_jarjestelma_koodi: String? = VardaUnitEducationSystem.NONE.vardaCode,
     val jarjestamismuoto_koodi: List<String>,
     val toimintamuoto_koodi: String?,
-    val asiointikieli_koodi: List<String>,
+    val toimintakieli_koodi: List<String>,
     val kielipainotus_kytkin: Boolean
 )
