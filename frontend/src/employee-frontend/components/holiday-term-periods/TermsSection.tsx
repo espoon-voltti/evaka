cf93cd7c885a112da73eda327eef9edef6fec5f4
Martin Lehtomaa <martin.lehtomaa@rebase.fi>
2024-02-05 16:49:26 +0200

    Addedd license header to new file

--- frontend/src/employee-frontend/components/holiday-term-periods/TermsSection.tsx
+++ frontend/src/employee-frontend/components/holiday-term-periods/TermsSection.tsx
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2017-2024 City of Espoo
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
 import React from 'react'
 
 import { useQueryResult } from 'lib-common/query'
