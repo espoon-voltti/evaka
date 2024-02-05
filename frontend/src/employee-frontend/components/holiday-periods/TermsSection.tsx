b6f576573e9377bd53104e8271b64253efee213e
Martin Lehtomaa <martin.lehtomaa@rebase.fi>
2024-02-05 14:20:04 +0200

    Added base buttons to delete or edit term row

--- frontend/src/employee-frontend/components/holiday-periods/TermsSection.tsx
+++ frontend/src/employee-frontend/components/holiday-periods/TermsSection.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import { faPen, faQuestion, faTrash } from 'Icons'
+import React, { useCallback, useState } from 'react'
 
+import { Success } from 'lib-common/api'
+import { ClubTerm, PreschoolTerm } from 'lib-common/generated/api-types/daycare'
 import { useQueryResult } from 'lib-common/query'
+import IconButton from 'lib-components/atoms/buttons/IconButton'
 import { Table, Tbody, Td, Th, Thead, Tr } from 'lib-components/layout/Table'
+import { FixedSpaceRow } from 'lib-components/layout/flex-helpers'
+import { AsyncFormModal } from 'lib-components/molecules/modals/FormModal'
 import { H2 } from 'lib-components/typography'
 
 import { useTranslation } from '../../state/i18n'
@@ -13,6 +19,32 @@ export default React.memo(function TermsSection() {
   const { i18n } = useTranslation()
 
   const preschoolTerms = useQueryResult(preschoolTermsQuery())
+
+  const [preschoolTermToDelete, setPreschoolTermToDelete] =
+    useState<PreschoolTerm>()
+
+  const [clubTermToDelete, setClubTermToDelete] = useState<ClubTerm>()
+
+  // TODO: Add functionality to delete preschool or club term
+  const onDeleteTerm = useCallback(() => {
+    if (preschoolTermToDelete) {
+      return Promise.resolve(Success.of())
+    } else if (clubTermToDelete) {
+      return Promise.resolve(Success.of())
+    } else {
+      return Promise.reject()
+    }
+  }, [preschoolTermToDelete])
+
+  const onDeleteTermReject = useCallback(() => {
+    setPreschoolTermToDelete(undefined)
+    setClubTermToDelete(undefined)
+  }, [setPreschoolTermToDelete, setClubTermToDelete])
+
+  const onDeleteTermSuccess = useCallback(() => {
+    setPreschoolTermToDelete(undefined)
+    setClubTermToDelete(undefined)
+  }, [setPreschoolTermToDelete, setClubTermToDelete])
 
   return (
     <>
@@ -48,12 +80,40 @@ export default React.memo(function TermsSection() {
                       ))}
                     </ul>
                   </Td>
-                  <Td />
+                  <Td>
+                    <FixedSpaceRow spacing="s">
+                      <IconButton
+                        icon={faPen}
+                        data-qa="btn-edit"
+                        //onClick={() => navigate('...')} TODO: Add functionality to navigate and edit term row
+                        aria-label={i18n.common.edit}
+                      />
+                      <IconButton
+                        icon={faTrash}
+                        data-qa="btn-delete"
+                        onClick={() => setPreschoolTermToDelete(row)}
+                        aria-label={i18n.common.remove}
+                      />
+                    </FixedSpaceRow>
+                  </Td>
                 </Tr>
               ))}
           </Tbody>
         </Table>
       ))}
+
+      {!!preschoolTermToDelete && (
+        <AsyncFormModal
+          type="warning"
+          title={i18n.terms.confirmDelete}
+          icon={faQuestion}
+          rejectAction={onDeleteTermReject}
+          rejectLabel={i18n.common.cancel}
+          resolveAction={onDeleteTerm}
+          resolveLabel={i18n.common.remove}
+          onSuccess={onDeleteTermSuccess}
+        />
+      )}
     </>
   )
 })
