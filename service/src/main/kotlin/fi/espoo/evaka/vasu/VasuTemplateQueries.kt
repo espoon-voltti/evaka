2bed0bb4d75f2b3f198db9c18e0355ed5a6cbb6a
Petri Lehtinen <petri.lehtinen@reaktor.com>
2024-03-01 07:52:26 +0200

    Migrate vasu

--- service/src/main/kotlin/fi/espoo/evaka/vasu/VasuTemplateQueries.kt
+++ service/src/main/kotlin/fi/espoo/evaka/vasu/VasuTemplateQueries.kt
@@ -16,124 +16,107 @@ fun Database.Transaction.insertVasuTemplate(
     language: VasuLanguage,
     content: VasuContent
 ): VasuTemplateId {
-    // language=sql
-    val sql =
-        """
-        INSERT INTO curriculum_template (valid, type, language, name, content)
-        VALUES (:valid, :type, :language, :name, :content)
-        RETURNING id
-    """
-            .trimIndent()
-
-    @Suppress("DEPRECATION")
-    return createQuery(sql)
-        .bind("name", name)
-        .bind("valid", valid)
-        .bind("type", type)
-        .bind("language", language)
-        .bind("content", content)
+    return createQuery {
+            sql(
+                """
+                INSERT INTO curriculum_template (valid, type, language, name, content)
+                VALUES (${bind(valid)}, ${bind(type)}, ${bind(language)}, ${bind(name)}, ${bind(content)})
+                RETURNING id
+                """
+            )
+        }
         .exactlyOne<VasuTemplateId>()
 }
 
 fun Database.Read.getVasuTemplate(id: VasuTemplateId): VasuTemplate? {
-    // language=sql
-    val sql =
-        """
-        SELECT ct.*, (SELECT count(*) FROM curriculum_document cd WHERE ct.id = cd.template_id) AS document_count
-        FROM curriculum_template ct
-        WHERE ct.id = :id
-    """
-            .trimIndent()
-
-    @Suppress("DEPRECATION") return createQuery(sql).bind("id", id).exactlyOneOrNull<VasuTemplate>()
+    return createQuery {
+            sql(
+                """
+                SELECT ct.*, (SELECT count(*) FROM curriculum_document cd WHERE ct.id = cd.template_id) AS document_count
+                FROM curriculum_template ct
+                WHERE ct.id = ${bind(id)}
+            """
+            )
+        }
+        .exactlyOneOrNull<VasuTemplate>()
 }
 
 fun Database.Read.getVasuTemplates(
     clock: EvakaClock,
     validOnly: Boolean
 ): List<VasuTemplateSummary> {
-    @Suppress("DEPRECATION")
-    return createQuery(
-            """
-        SELECT 
-            id,
-            name,
-            valid,
-            type,
-            language,
-            (SELECT count(*) FROM curriculum_document cd WHERE cd.template_id = ct.id) AS document_count
-        FROM curriculum_template ct
-        ${if (validOnly) "WHERE valid @> :today" else ""}
-    """
-        )
-        .apply { if (validOnly) bind("today", clock.today()) }
+    return createQuery {
+            sql(
+                """
+                SELECT 
+                    id,
+                    name,
+                    valid,
+                    type,
+                    language,
+                    (SELECT count(*) FROM curriculum_document cd WHERE cd.template_id = ct.id) AS document_count
+                FROM curriculum_template ct
+                ${if (validOnly) "WHERE valid @> ${bind(clock.today())}" else ""}
+                """
+            )
+        }
         .toList<VasuTemplateSummary>()
 }
 
 fun Database.Transaction.updateVasuTemplateContent(id: VasuTemplateId, content: VasuContent) {
-    // language=sql
-    val sql =
-        """
-        UPDATE curriculum_template
-        SET content = :content
-        WHERE id = :id
-    """
-            .trimIndent()
-
-    @Suppress("DEPRECATION")
-    createUpdate(sql).bind("id", id).bind("content", content).updateExactlyOne()
+    createUpdate {
+            sql(
+                """
+                UPDATE curriculum_template
+                SET content = ${bind(content)}
+                WHERE id = ${bind(id)}
+                """
+            )
+        }
+        .updateExactlyOne()
 }
 
 fun Database.Read.getVasuTemplateForUpdate(id: VasuTemplateId): VasuTemplateSummary? {
-    // language=sql
-    val sql =
-        """
-SELECT
-    id,
-    name,
-    valid,
-    type,
-    language,
-    (SELECT COUNT(*) FROM curriculum_document cd WHERE cd.template_id = ct.id) AS document_count
-FROM curriculum_template ct
-WHERE id = :id
-FOR UPDATE
-    """
-            .trimIndent()
-
-    @Suppress("DEPRECATION")
-    return createQuery(sql).bind("id", id).exactlyOneOrNull<VasuTemplateSummary>()
+    return createQuery {
+            sql(
+                """
+                SELECT
+                    id,
+                    name,
+                    valid,
+                    type,
+                    language,
+                    (SELECT COUNT(*) FROM curriculum_document cd WHERE cd.template_id = ct.id) AS document_count
+                FROM curriculum_template ct
+                WHERE id = ${bind(id)}
+                FOR UPDATE
+                """
+            )
+        }
+        .exactlyOneOrNull<VasuTemplateSummary>()
 }
 
 fun Database.Transaction.updateVasuTemplate(id: VasuTemplateId, params: VasuTemplateUpdate) {
-    // language=sql
-    val sql =
-        """
-        UPDATE curriculum_template
-        SET name = :name, valid = :valid
-        WHERE id = :id
-    """
-            .trimIndent()
-
-    @Suppress("DEPRECATION")
-    createUpdate(sql)
-        .bind("id", id)
-        .bind("name", params.name)
-        .bind("valid", params.valid)
+    createUpdate {
+            sql(
+                """
+                UPDATE curriculum_template
+                SET name = ${bind(params.name)}, valid = ${bind(params.valid)}
+                WHERE id = ${bind(id)}
+                """
+            )
+        }
         .updateExactlyOne()
 }
 
 fun Database.Transaction.deleteUnusedVasuTemplate(id: VasuTemplateId) {
-    // language=sql
-    val sql =
-        """
-        DELETE FROM curriculum_template ct
-        WHERE ct.id = :id AND NOT EXISTS(SELECT 1 FROM curriculum_document cd WHERE cd.template_id = ct.id)
-    """
-            .trimIndent()
-
-    @Suppress("DEPRECATION")
-    createUpdate(sql)
-        .bind("id", id)
+    createUpdate {
+            sql(
+                """
+                DELETE FROM curriculum_template ct
+                WHERE ct.id = ${bind(id)} AND NOT EXISTS(SELECT 1 FROM curriculum_document cd WHERE cd.template_id = ct.id)
+                """
+            )
+        }
         .updateExactlyOne(notFoundMsg = "template $id not found or is in use")
 }
