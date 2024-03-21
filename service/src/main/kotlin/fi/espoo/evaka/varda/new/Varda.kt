62a5c21621f06637b37cfba5861c24071aed7ae0
Petri Lehtinen <petri.lehtinen@reaktor.com>
2024-03-21 10:15:00 +0200

    Base diffs on `==` instead of custom interface

--- service/src/main/kotlin/fi/espoo/evaka/varda/new/Varda.kt
+++ service/src/main/kotlin/fi/espoo/evaka/varda/new/Varda.kt
@@ -13,21 +13,18 @@ import java.math.BigDecimal
 import java.net.URI
 import java.time.LocalDate
 
-interface Diffable<T> {
-    fun diffEq(other: T): Boolean
-}
-
-fun <Old, New : Diffable<Old>> diff(
+fun <Old, New> diff(
     old: List<Old>,
     new: List<New>,
+    eq: (Old, New) -> Boolean,
     onRemoved: (Old) -> Unit = { _ -> },
     onAdded: (New) -> Unit = { _ -> },
     onUnchanged: (Old, New) -> Unit = { _, _ -> }
 ) {
-    old.filter { oldItem -> new.none { newItem -> newItem.diffEq(oldItem) } }.forEach(onRemoved)
-    new.filter { newItem -> old.none { oldItem -> newItem.diffEq(oldItem) } }.forEach(onAdded)
+    old.filter { oldItem -> new.none { newItem -> eq(oldItem, newItem) } }.forEach(onRemoved)
+    new.filter { newItem -> old.none { oldItem -> eq(oldItem, newItem) } }.forEach(onAdded)
     old.forEach { oldItem ->
-        val newItem = new.find { it.diffEq(oldItem) }
+        val newItem = new.find { eq(oldItem, it) }
         if (newItem != null) {
             onUnchanged(oldItem, newItem)
         }
@@ -38,7 +35,7 @@ data class Lapsi(
     val vakatoimija_oid: String?,
     val oma_organisaatio_oid: String?,
     val paos_organisaatio_oid: String?,
-) : Diffable<VardaClient.LapsiResponse> {
+) {
     companion object {
         fun fromEvaka(data: VardaServiceNeed, omaOrganisaatioOid: String): Lapsi =
             if (data.providerType == ProviderType.PRIVATE_SERVICE_VOUCHER) {
@@ -54,6 +51,13 @@ data class Lapsi(
                     paos_organisaatio_oid = null
                 )
             }
+
+        fun fromVarda(data: VardaClient.LapsiResponse): Lapsi =
+            Lapsi(
+                vakatoimija_oid = data.vakatoimija_oid,
+                oma_organisaatio_oid = data.oma_organisaatio_oid,
+                paos_organisaatio_oid = data.paos_organisaatio_oid,
+            )
     }
 
     fun toVarda(lahdejarjestelma: String, henkilo: URI) =
@@ -64,11 +68,6 @@ data class Lapsi(
             oma_organisaatio_oid = oma_organisaatio_oid,
             paos_organisaatio_oid = paos_organisaatio_oid
         )
-
-    override fun diffEq(other: VardaClient.LapsiResponse): Boolean =
-        vakatoimija_oid == other.vakatoimija_oid &&
-            oma_organisaatio_oid == other.oma_organisaatio_oid &&
-            paos_organisaatio_oid == other.paos_organisaatio_oid
 }
 
 data class Varhaiskasvatuspaatos(
@@ -82,7 +81,7 @@ data class Varhaiskasvatuspaatos(
     val paivittainen_vaka_kytkin: Boolean,
     val kokopaivainen_vaka_kytkin: Boolean,
     val jarjestamismuoto_koodi: String,
-) : Diffable<VardaClient.VarhaiskasvatuspaatosResponse> {
+) {
     companion object {
         fun fromEvaka(data: VardaServiceNeed): Varhaiskasvatuspaatos =
             Varhaiskasvatuspaatos(
@@ -97,6 +96,20 @@ data class Varhaiskasvatuspaatos(
                 vuorohoito_kytkin = data.shiftCare,
                 jarjestamismuoto_koodi =
                     VardaUnitProviderType.fromEvakaProviderType(data.providerType).vardaCode,
+            )
+
+        fun fromVarda(data: VardaClient.VarhaiskasvatuspaatosResponse) =
+            Varhaiskasvatuspaatos(
+                hakemus_pvm = data.hakemus_pvm,
+                alkamis_pvm = data.alkamis_pvm,
+                paattymis_pvm = data.paattymis_pvm,
+                pikakasittely_kytkin = data.pikakasittely_kytkin,
+                tuntimaara_viikossa = data.tuntimaara_viikossa,
+                tilapainen_vaka_kytkin = data.tilapainen_vaka_kytkin,
+                paivittainen_vaka_kytkin = data.paivittainen_vaka_kytkin,
+                kokopaivainen_vaka_kytkin = data.kokopaivainen_vaka_kytkin,
+                vuorohoito_kytkin = data.vuorohoito_kytkin,
+                jarjestamismuoto_koodi = data.jarjestamismuoto_koodi,
             )
     }
 
@@ -115,31 +128,26 @@ data class Varhaiskasvatuspaatos(
             kokopaivainen_vaka_kytkin = kokopaivainen_vaka_kytkin,
             jarjestamismuoto_koodi = jarjestamismuoto_koodi,
         )
-
-    override fun diffEq(other: VardaClient.VarhaiskasvatuspaatosResponse): Boolean =
-        alkamis_pvm == other.alkamis_pvm &&
-            paattymis_pvm == other.paattymis_pvm &&
-            hakemus_pvm == other.hakemus_pvm &&
-            vuorohoito_kytkin == other.vuorohoito_kytkin &&
-            tilapainen_vaka_kytkin == other.tilapainen_vaka_kytkin &&
-            pikakasittely_kytkin == other.pikakasittely_kytkin &&
-            tuntimaara_viikossa == other.tuntimaara_viikossa &&
-            paivittainen_vaka_kytkin == other.paivittainen_vaka_kytkin &&
-            kokopaivainen_vaka_kytkin == other.kokopaivainen_vaka_kytkin &&
-            jarjestamismuoto_koodi == other.jarjestamismuoto_koodi
 }
 
 data class Varhaiskasvatussuhde(
     val toimipaikka_oid: String,
     val alkamis_pvm: LocalDate,
     val paattymis_pvm: LocalDate?,
-) : Diffable<VardaClient.VarhaiskasvatussuhdeResponse> {
+) {
     companion object {
         fun fromEvaka(data: VardaServiceNeed): Varhaiskasvatussuhde =
             Varhaiskasvatussuhde(
                 toimipaikka_oid = data.ophUnitOid,
                 alkamis_pvm = data.range.start,
                 paattymis_pvm = data.range.end,
+            )
+
+        fun fromVarda(data: VardaClient.VarhaiskasvatussuhdeResponse) =
+            Varhaiskasvatussuhde(
+                toimipaikka_oid = data.toimipaikka_oid,
+                alkamis_pvm = data.alkamis_pvm,
+                paattymis_pvm = data.paattymis_pvm,
             )
     }
 
@@ -151,11 +159,6 @@ data class Varhaiskasvatussuhde(
             alkamis_pvm = alkamis_pvm,
             paattymis_pvm = paattymis_pvm,
         )
-
-    override fun diffEq(other: VardaClient.VarhaiskasvatussuhdeResponse): Boolean =
-        toimipaikka_oid == other.toimipaikka_oid &&
-            alkamis_pvm == other.alkamis_pvm &&
-            paattymis_pvm == other.paattymis_pvm
 }
 
 enum class MaksunPerusteKoodi(val code: String) {
@@ -170,9 +173,8 @@ data class Maksutieto(
     val perheen_koko: Int?,
     val maksun_peruste_koodi: String,
     val asiakasmaksu: BigDecimal,
-    val palveluseteli_arvo: BigDecimal?,
-    val paos_organisaatio_oid: String?
-) : Diffable<VardaClient.MaksutietoResponse> {
+    val palveluseteli_arvo: BigDecimal?
+) {
     companion object {
         fun fromEvaka(guardians: List<PersonDTO>, data: VardaFeeData): Maksutieto? {
             val huoltajat =
@@ -211,9 +213,19 @@ data class Maksutieto(
                 asiakasmaksu = BigDecimal(data.totalFee).divide(BigDecimal(100)),
                 palveluseteli_arvo =
                     data.voucherValue?.let { BigDecimal(it).divide(BigDecimal(100)) },
-                paos_organisaatio_oid = data.voucherUnitOrganizerOid,
             )
         }
+
+        fun fromVarda(data: VardaClient.MaksutietoResponse): Maksutieto =
+            Maksutieto(
+                huoltajat = data.huoltajat,
+                alkamis_pvm = data.alkamis_pvm,
+                paattymis_pvm = data.paattymis_pvm,
+                perheen_koko = data.perheen_koko,
+                maksun_peruste_koodi = data.maksun_peruste_koodi,
+                asiakasmaksu = data.asiakasmaksu,
+                palveluseteli_arvo = data.palveluseteli_arvo,
+            )
     }
 
     fun toVarda(lahdejarjestelma: String, lapsi: URI) =
@@ -228,14 +240,4 @@ data class Maksutieto(
             asiakasmaksu = asiakasmaksu,
             perheen_koko = perheen_koko,
         )
-
-    override fun diffEq(other: VardaClient.MaksutietoResponse): Boolean =
-        huoltajat.map { it.henkilo_oid }.toSet() ==
-            other.huoltajat.map { it.henkilo_oid }.toSet() &&
-            alkamis_pvm == other.alkamis_pvm &&
-            paattymis_pvm == other.paattymis_pvm &&
-            maksun_peruste_koodi == other.maksun_peruste_koodi &&
-            palveluseteli_arvo == other.palveluseteli_arvo &&
-            asiakasmaksu == other.asiakasmaksu &&
-            perheen_koko == other.perheen_koko
 }
