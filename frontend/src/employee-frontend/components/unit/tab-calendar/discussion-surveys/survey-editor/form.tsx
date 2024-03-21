18a69d62f59eed58bc1e51fb8a0ee468b1452292
Tatu Repo <tatu.repo@gofore.com>
2024-03-21 17:44:00 +0200

    Add missing license information

--- frontend/src/employee-frontend/components/unit/tab-calendar/discussion-surveys/survey-editor/form.tsx
+++ frontend/src/employee-frontend/components/unit/tab-calendar/discussion-surveys/survey-editor/form.tsx
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2017-2024 City of Espoo
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
 import orderBy from 'lodash/orderBy'
 
 import DateRange from 'lib-common/date-range'
