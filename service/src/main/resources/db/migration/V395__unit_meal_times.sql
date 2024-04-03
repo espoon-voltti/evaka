2371778c94912793e1c721a44d22856802758c88
Jonni Madekivi <jonni.madekivi@reaktor.com>
2024-04-03 14:39:29 +0300

    Rename migrations sql

---
+++ service/src/main/resources/db/migration/V395__unit_meal_times.sql
@@ -0,0 +1,6 @@
+alter table daycare
+    add mealtime_breakfast time without time zone,
+    add mealtime_lunch time without time zone,
+    add mealtime_snack time without time zone,
+    add mealtime_supper time without time zone,
+    add mealtime_evening_snack time without time zone;
