2939fbd111b1eb69995804df06027c996727621d
Petri Lehtinen <petri.lehtinen@reaktor.com>
2024-04-16 08:01:10 +0300

    Add license comments

--- frontend/src/employee-frontend/components/reports/MealReport.tsx
+++ frontend/src/employee-frontend/components/reports/MealReport.tsx
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2017-2024 City of Espoo
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
 import React, { useState } from 'react'
 
 import { Daycare } from 'lib-common/generated/api-types/daycare'
--- service/src/main/kotlin/fi/espoo/evaka/reports/MealReportController.kt
+++ service/src/main/kotlin/fi/espoo/evaka/reports/MealReportController.kt
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2017-2024 City of Espoo
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
 package fi.espoo.evaka.reports
 
 import fi.espoo.evaka.Audit
