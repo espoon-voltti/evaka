f4d05306ed18ee19d91578430d0d3a0cfae2011a
Joonas Javanainen <joonas.javanainen@reaktor.com>
2024-04-19 12:57:22 +0300

    Drop dependant view before the column

--- service/src/main/resources/db/migration/V398__application_other_guardian.sql
+++ service/src/main/resources/db/migration/V398__application_other_guardian.sql
@@ -4,4 +4,5 @@ FROM application
 WHERE other_guardian_id IS NOT NULL
 ON CONFLICT (application_id, guardian_id) DO NOTHING;
 
+DROP VIEW IF EXISTS application_view;
 ALTER TABLE application DROP COLUMN other_guardian_id;
