ae4b6a160811809255c88999dc464dfb457dc139
Petri Lehtinen <petri.lehtinen@reaktor.com>
2024-05-02 09:20:33 +0300

    Use mock client instead of mock endpoint in Varda unit upload tests
    Also, stop using global test data and create the fixtures on the fly
    instead.

--- service/src/integrationTest/kotlin/fi/espoo/evaka/varda/VardaUnitIntegrationTest.kt
+++ service/src/integrationTest/kotlin/fi/espoo/evaka/varda/VardaUnitIntegrationTest.kt
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2017-2020 City of Espoo
+// SPDX-FileCopyrightText: 2017-2024 City of Espoo
 //
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
@@ -7,55 +7,49 @@ package fi.espoo.evaka.varda
 import fi.espoo.evaka.daycare.domain.ProviderType
 import fi.espoo.evaka.shared.DaycareId
 import fi.espoo.evaka.shared.db.Database
+import fi.espoo.evaka.shared.dev.DevCareArea
 import fi.espoo.evaka.shared.dev.DevDaycare
 import fi.espoo.evaka.shared.dev.insert
 import fi.espoo.evaka.shared.domain.HelsinkiDateTime
 import fi.espoo.evaka.shared.domain.MockEvakaClock
-import fi.espoo.evaka.testArea
-import fi.espoo.evaka.testDaycare
-import fi.espoo.evaka.testDaycare2
-import fi.espoo.evaka.testPurchasedDaycare
-import fi.espoo.evaka.varda.integration.MockVardaIntegrationEndpoint
 import java.time.Instant
 import java.time.LocalDate
 import java.time.LocalTime
+import java.util.UUID
 import kotlin.test.assertEquals
 import kotlin.test.assertNotEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
 
 class VardaUnitIntegrationTest : VardaIntegrationTest(resetDbBeforeEach = true) {
-    @Autowired lateinit var mockEndpoint: MockVardaIntegrationEndpoint
     lateinit var clock: MockEvakaClock
+
+    val area = DevCareArea()
+    val unitId1 = DaycareId(UUID.randomUUID())
+    val unitId2 = DaycareId(UUID.randomUUID())
 
     @BeforeEach
     fun beforeEach() {
         clock = MockEvakaClock(HelsinkiDateTime.of(LocalDate.of(2020, 1, 1), LocalTime.of(8, 0, 0)))
 
         db.transaction { tx ->
-            tx.insert(testArea)
-            tx.insert(
-                DevDaycare(areaId = testArea.id, id = testDaycare.id, name = testDaycare.name)
-            )
-            tx.insert(
-                DevDaycare(areaId = testArea.id, id = testDaycare2.id, name = testDaycare2.name)
-            )
+            tx.insert(area)
+            tx.insert(DevDaycare(areaId = area.id, id = unitId1, name = "Päiväkoti 1"))
+            tx.insert(DevDaycare(areaId = area.id, id = unitId2, name = "Päiväkoti 2"))
         }
-
-        mockEndpoint.cleanUp()
     }
 
     @Test
     fun `uploading municipal units works`() {
-        updateUnits()
+        val client = TestClient()
+
+        updateUnits(client)
         assertEquals(2, getVardaUnits(db).size)
-        assertEquals(2, mockEndpoint.units.size)
-        assertEquals(
-            vardaClient.sourceSystem,
-            mockEndpoint.units.values.elementAt(0).lahdejarjestelma
-        )
-        assertEquals("[FI]", mockEndpoint.units.values.elementAt(0).toimintakieli_koodi.toString())
+        assertEquals(2, client.units.size)
+
+        val uploadedUnit = client.findUnitByName("Päiväkoti 1")
+        assertEquals(vardaClient.sourceSystem, uploadedUnit.lahdejarjestelma)
+        assertEquals(listOf("FI"), uploadedUnit.toimintakieli_koodi)
     }
 
     @Test
@@ -68,7 +62,7 @@ class VardaUnitIntegrationTest : VardaIntegrationTest(resetDbBeforeEach = true) 
                         """
                         ALTER TABLE daycare DISABLE TRIGGER set_timestamp;
                         UPDATE daycare SET closing_date = ${bind(closingDate)}, updated = ${bind(clock.now())}
-                        WHERE id = ${bind(testDaycare.id)};
+                        WHERE id = ${bind(unitId1)};
                         ALTER TABLE daycare ENABLE TRIGGER set_timestamp;
                         """
                     )
@@ -77,11 +71,10 @@ class VardaUnitIntegrationTest : VardaIntegrationTest(resetDbBeforeEach = true) 
         }
         clock.tick()
 
-        updateUnits()
-        assertEquals(
-            closingDate.toString(),
-            mockEndpoint.units.values.first { it.nimi == testDaycare.name }.paattymis_pvm
-        )
+        val client = TestClient()
+
+        updateUnits(client)
+        assertEquals(closingDate.toString(), client.findUnitByName("Päiväkoti 1").paattymis_pvm)
 
         clock.tick()
         db.transaction {
@@ -90,14 +83,14 @@ class VardaUnitIntegrationTest : VardaIntegrationTest(resetDbBeforeEach = true) 
                         """
                         ALTER TABLE daycare DISABLE TRIGGER set_timestamp;
                         UPDATE daycare SET closing_date = NULL, updated = ${bind(clock.now())}
-                        WHERE id = ${bind(testDaycare.id)};
+                        WHERE id = ${bind(unitId1)};
                         ALTER TABLE daycare ENABLE TRIGGER set_timestamp;
                         """
                     )
                 }
                 .execute()
         }
-        val unit = db.read { getNewOrStaleUnits(it) }.find { it.name == testDaycare.name }
+        val unit = db.read { getNewOrStaleUnits(it) }.find { it.evakaDaycareId == unitId1 }
 
         // Because of too tight serialization annotation the unit closing date removal (setting as
         // null) was dropped out
@@ -113,36 +106,37 @@ class VardaUnitIntegrationTest : VardaIntegrationTest(resetDbBeforeEach = true) 
                 .contains(""""paattymis_pvm":null""")
         )
 
-        updateUnits()
-        assertEquals(
-            null,
-            mockEndpoint.units.values.first { it.nimi == testDaycare.name }.paattymis_pvm
-        )
+        updateUnits(client)
+        assertEquals(null, client.findUnitByName("Päiväkoti 1").paattymis_pvm)
     }
 
     @Test
     fun `not uploading purchased units works`() {
-        updateUnits()
+        val client = TestClient()
+
+        updateUnits(client)
         assertEquals(2, getVardaUnits(db).size)
 
         db.transaction {
             it.insert(
                 DevDaycare(
-                    areaId = testArea.id,
-                    id = testPurchasedDaycare.id,
-                    name = testPurchasedDaycare.name,
+                    areaId = area.id,
+                    name = "Ostettu päiväkoti",
                     providerType = ProviderType.PURCHASED
                 )
             )
         }
-        updateUnits()
+
+        updateUnits(client)
 
         assertEquals(2, getVardaUnits(db).size)
     }
 
     @Test
     fun `updating stale unit works`() {
-        updateUnits()
+        val client = TestClient()
+
+        updateUnits(client)
         val unitToStale = getVardaUnits(db)[0]
         val daycareId = unitToStale.evakaDaycareId
         val vardaId = unitToStale.vardaUnitId
@@ -155,7 +149,8 @@ class VardaUnitIntegrationTest : VardaIntegrationTest(resetDbBeforeEach = true) 
                     sql(
                         """
                         ALTER TABLE daycare DISABLE TRIGGER set_timestamp;
-                        UPDATE daycare SET street_address = 'new address', updated = ${bind(clock.now())}
+                        UPDATE daycare SET street_address = 'new address', updated =
+    ${bind(clock.now())}
                         WHERE id = ${bind(daycareId)};
                         ALTER TABLE daycare ENABLE TRIGGER set_timestamp;
                         """
@@ -164,7 +159,7 @@ class VardaUnitIntegrationTest : VardaIntegrationTest(resetDbBeforeEach = true) 
                 .execute()
         }
 
-        updateUnits()
+        updateUnits(client)
 
         val updatedUnit = getVardaUnits(db).toList().find { it.evakaDaycareId == daycareId }!!
         assertEquals(vardaId, updatedUnit.vardaUnitId)
@@ -172,17 +167,36 @@ class VardaUnitIntegrationTest : VardaIntegrationTest(resetDbBeforeEach = true) 
         assertNotEquals(lastSuccessAt, updatedUnit.lastSuccessAt)
     }
 
-    private fun updateUnits() {
+    private fun updateUnits(client: VardaUnitClient) {
         val ophMunicipalOrganizerIdUrl = "${vardaEnv.url}/v1/vakajarjestajat/${ophEnv.organizerId}/"
         updateUnits(
             db,
             clock,
-            vardaClient,
+            client,
             vardaClient.sourceSystem,
             ophEnv.municipalityCode,
             ophMunicipalOrganizerIdUrl
         )
     }
+}
+
+class TestClient : VardaUnitClient {
+    val units = mutableMapOf<Long, VardaUnitRequest>()
+    private var nextId = 1L
+
+    override fun createUnit(unit: VardaUnitRequest): VardaUnitResponse {
+        val id = nextId++
+        units[id] = unit
+        return VardaUnitResponse(id, "foo.$id")
+    }
+
+    override fun updateUnit(id: Long, unit: VardaUnitRequest): VardaUnitResponse {
+        if (!units.containsKey(id)) throw IllegalStateException("Unit not found")
+        units[id] = unit
+        return VardaUnitResponse(id, "foo.$id")
+    }
+
+    fun findUnitByName(name: String) = units.values.find { it.nimi == name }!!
 }
 
 fun getVardaUnits(db: Database.Connection): List<VardaUnitRow> =
