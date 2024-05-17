fc6610e1e771b3d4aa43e8404a47565dd04081dc
Petri Lehtinen <petri.lehtinen@reaktor.com>
2024-05-17 08:26:56 +0300

    Migrate child image

--- service/src/main/kotlin/fi/espoo/evaka/childimages/ChildImageQueries.kt
+++ service/src/main/kotlin/fi/espoo/evaka/childimages/ChildImageQueries.kt
@@ -8,20 +8,16 @@ import fi.espoo.evaka.shared.ChildId
 import fi.espoo.evaka.shared.ChildImageId
 import fi.espoo.evaka.shared.db.Database
 
-fun Database.Transaction.insertChildImage(childId: ChildId): ChildImageId {
-    // language=sql
-    val sql =
-        """
-        INSERT INTO child_images (child_id) VALUES (:childId) RETURNING id;
+fun Database.Transaction.insertChildImage(childId: ChildId): ChildImageId =
+    createQuery {
+            sql(
+                """
+        INSERT INTO child_images (child_id) VALUES (${bind(childId)}) RETURNING id
     """
-            .trimIndent()
-    @Suppress("DEPRECATION")
-    return createQuery(sql).bind("childId", childId).exactlyOne<ChildImageId>()
-}
+            )
+        }
+        .exactlyOne()
 
-fun Database.Transaction.deleteChildImage(childId: ChildId): ChildImageId? {
-    @Suppress("DEPRECATION")
-    return createQuery("DELETE FROM child_images WHERE child_id = :childId RETURNING id")
-        .bind("childId", childId)
+fun Database.Transaction.deleteChildImage(childId: ChildId): ChildImageId? =
+    createQuery { sql("DELETE FROM child_images WHERE child_id = ${bind(childId)} RETURNING id") }
         .exactlyOneOrNull<ChildImageId>()
-}
