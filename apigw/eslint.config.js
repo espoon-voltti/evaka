a04000d410b60d9293841e70d6c08dbebf1c363a
Joonas Javanainen <joonas.javanainen@reaktor.com>
2024-06-24 13:04:07 +0300

    Add eslint-plugin-import to apigw

--- apigw/eslint.config.js
+++ apigw/eslint.config.js
@@ -2,9 +2,11 @@
 //
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
+import { fixupPluginRules } from '@eslint/compat'
 import eslint from '@eslint/js'
-import typescriptEslint from 'typescript-eslint'
+import importPlugin from 'eslint-plugin-import'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import typescriptEslint from 'typescript-eslint'
 
 export default [
   { ignores: ['dist/*', '.yarn/*'] },
@@ -14,12 +16,28 @@ export default [
     ...typescriptEslint.configs.stylistic
   ),
   {
+    // Compatibility tricks for plugins that don't support ESLint v9 flat configs yet
+    plugins: {
+      import: fixupPluginRules(importPlugin)
+    }
+  },
+  {
     rules: {
       '@typescript-eslint/no-unused-vars': [
         'warn',
         {
           argsIgnorePattern: '^_',
           varsIgnorePattern: '^_'
+        }
+      ],
+      'import/order': [
+        'warn',
+        {
+          alphabetize: {
+            order: 'asc'
+          },
+          groups: ['builtin', 'external', 'parent', 'sibling', 'index'],
+          'newlines-between': 'always'
         }
       ]
     }
