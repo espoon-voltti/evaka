d43a3132e28fa5b1a66828cace7c58304d9d2ce1
Joosa Kurvinen <joosa.kurvinen@reaktor.com>
2024-08-08 13:50:58 +0300

    make environment label bigger and fixed

--- frontend/src/lib-components/atoms/EnvironmentLabel.tsx
+++ frontend/src/lib-components/atoms/EnvironmentLabel.tsx
@@ -7,13 +7,17 @@ import styled from 'styled-components'
 import { tabletMin } from '../breakpoints'
 
 export const EnvironmentLabel = styled.div`
-  position: absolute;
+  position: fixed;
   left: 0;
   top: 0;
   z-index: 100;
   background-color: ${(p) => p.theme.colors.status.success};
-  color: ${(p) => p.theme.colors.grayscale.g0};
-  padding: 4px 12px;
+  color: ${(p) => p.theme.colors.grayscale.g100};
+  font-size: 24px;
+  font-weight: 600;
+  padding: 8px 16px;
+  min-width: 150px;
+  text-align: center;
   border-radius: 0 0 12px 0;
 
   @media (max-width: ${tabletMin}) {
