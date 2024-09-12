644c1de5ce99e962c927e1bbab70a0d0dc2d98fb
Joonas Javanainen <joonas.javanainen@reaktor.com>
2024-09-12 16:59:02 +0300

    Add documentation to all environment variables

--- apigw/src/shared/config.ts
+++ apigw/src/shared/config.ts
@@ -9,83 +9,383 @@ import { TrustedCertificates } from './certificates.js'
 
 type EnvVariables = typeof envVariables
 const envVariables = {
+  // ----- Miscellaneous configuration -----
+  /**
+   * Environment name.
+   *
+   * This gets passed to log events in the `env` field, and to DataDog config `env` field.
+   */
   VOLTTI_ENV: unset<string>(),
+  /**
+   * The port the HTTP server listens on
+   */
   HTTP_PORT: 3000,
+  /**
+   * If true, adds a proxy route to the evaka-service dev API.
+   *
+   * **Do not enable in production**
+   */
   ENABLE_DEV_API: false,
 
+  // ----- Session configuration -----
+  /**
+   * @deprecated use CITIZEN_COOKIE_SECRET and EMPLOYEE_COOKIE_SECRET instead
+   */
   COOKIE_SECRET: unset<string>(),
+  /**
+   * Default timeout (in minutes) for idle sessions.
+   *
+   * This timeout can be overridden for specific session types by more specific environment variables.
+   */
   SESSION_TIMEOUT_MINUTES: 32,
+  /**
+   * If true, all cookies are set with the `Secure` flag.
+   *
+   * **Do not disable in production**
+   */
   USE_SECURE_COOKIES: true,
+  /**
+   * Secret used for signing citizen session cookies.
+   *
+   * Recommended to be a string of at least 32 random characters.
+   */
   CITIZEN_COOKIE_SECRET: unset<string>(),
+  /**
+   * Timeout (in minutes) for idle citizen sessions.
+   *
+   * If set, overrides the value of SESSION_TIMEOUT_MINUTES for these sessions
+   */
   CITIZEN_SESSION_TIMEOUT_MINUTES: unset<number>(),
+  /**
+   * Secret used for signing employee / employee mobile session cookies.
+   *
+   * Recommended to be a string of at least 32 random characters.
+   */
   EMPLOYEE_COOKIE_SECRET: unset<string>(),
+  /**
+   * Timeout (in minutes) for idle employee / employee mobile sessions.
+   *
+   * If set, overrides the value of SESSION_TIMEOUT_MINUTES for these sessions
+   */
   EMPLOYEE_SESSION_TIMEOUT_MINUTES: unset<number>(),
+  /**
+   * Timeout (in seconds) for employee mobile pin login sessions.
+   */
   PIN_SESSION_TIMEOUT_SECONDS: 10 * 60,
 
+  // ----- Redis configuration -----
+  /**
+   * Redis server hostname
+   */
   REDIS_HOST: unset<string>(),
+  /**
+   * Redis server port
+   */
   REDIS_PORT: unset<number>(),
+  /**
+   * Redis password
+   */
   REDIS_PASSWORD: unset<string>(),
+  /**
+   * If true, connections to Redis are made without using TLS encryption
+   */
   REDIS_DISABLE_SECURITY: false,
+  /**
+   * Redis server TLS hostname.
+   *
+   * Required if REDIS_DISABLE_SECURITY is false.
+   */
   REDIS_TLS_SERVER_NAME: unset<string>(),
 
+  // ----- Logging / debugging -----
+  /**
+   * Value for the `appBuild` field in log events.
+   *
+   * Normally set to a fixed value during Docker container build.
+   */
   APP_BUILD: 'UNDEFINED',
+  /**
+   * Value for the `appCommit` field in log events.
+   *
+   * Normally set to a fixed value during Docker container build.
+   */
   APP_COMMIT: 'UNDEFINED',
+  /**
+   * Value for the `hostIp` field in log events.
+   *
+   * Normally set by Docker container entrypoint script at container startup.
+   */
   HOST_IP: 'UNDEFINED',
+  /**
+   * If true, includes the error message in all error responses.
+   *
+   * **Do not enable in production**
+   */
   INCLUDE_ALL_ERROR_MESSAGES: false,
+  /**
+   * If true, outputs pretty-printed simple logs instead of JSON.
+   */
   PRETTY_LOGS: false,
 
+  // ----- DataDog tracing/profiling support -----
+  /**
+   * If true, enables DataDog tracing.
+   */
   DD_TRACE_ENABLED: unset<boolean>(),
+  /**
+   * If true, enables DataDog profiling.
+   */
   DD_PROFILING_ENABLED: unset<boolean>(),
+  /**
+   * Hostname for the DataDog agent
+   */
   DD_TRACE_AGENT_HOSTNAME: 'localhost',
+  /**
+   * Port for the DataDog agent
+   */
   DD_TRACE_AGENT_PORT: 8126,
 
+  // ----- JWT token configuration -----
+  /**
+   * Path to a private key PEM file for JWT signing
+   */
   JWT_PRIVATE_KEY: unset<string>(),
+  /**
+   * Value for the `keyid` field in generated JWT tokens
+   */
   JWT_KID: 'evaka-api-gw',
+  /**
+   * If true, automatically refreshes JWT tokens when near expiration.
+   *
+   * **Do not disable in production**
+   */
   JWT_REFRESH_ENABLED: true,
 
+  // ----- eVaka frontend/backend URLs -----
+  /**
+   * Base URL for eVaka frontend (e.g. https://evaka.example.com)
+   */
   EVAKA_BASE_URL: unset<string>(),
+  /**
+   * Base URL for evaka-service backend (e.g. http://evaka-service.example.local)
+   */
   EVAKA_SERVICE_URL: unset<string>(),
 
+  // ----- Active Directory SAML authentication -----
+  /**
+   * @deprecated Use `AD_MOCK` instead
+   */
   DEV_LOGIN: unset<boolean>(),
+  /**
+   * If true, uses a mock authentication provider for AD, meant for local development only.
+   *
+   * **Do not enable in production**
+   */
   AD_MOCK: false,
+  /**
+   * Hard-coded prefix for external IDs that originate from AD SAML authentication.
+   *
+   * When an AD user authenticates, a full external ID that looks like "prefix:id" is sent to evaka-service.
+   */
   AD_SAML_EXTERNAL_ID_PREFIX: 'espoo-ad',
+  /**
+   * SAML authentication response attribute containing a persistent identifier for AD users.
+   *
+   * This attribute must be present in the SAML response or authentication will fail. When an AD user authenticates,
+   * a full external ID that looks like "prefix:id" is sent to evaka-service.
+   */
   AD_USER_ID_KEY:
     'http://schemas.microsoft.com/identity/claims/objectidentifier',
+  /**
+   * If true, assumes SAML responses from AD are encrypted using our SP public certificate.
+   *
+   * This depends on how AD is configured and may not be necessary.
+   */
   AD_DECRYPT_ASSERTIONS: false,
+  /**
+   * URN for the NameIDFormat used in AD SAML requests
+   */
   AD_NAME_ID_FORMAT: 'urn:oasis:names:tc:SAML:2.0:nameid-format:transient',
+  /**
+   * SAML service provider callback URL for AD authentication.
+   *
+   * This is the URL for our SAML authentication response endpoint
+   * (e.g. https://evaka.example.com/api/internal/auth/saml/login/callback)
+   */
   AD_SAML_CALLBACK_URL: unset<string>(),
+  /**
+   * SAML identity provider entrypoint URL for AD authentication.
+   *
+   * This is the URL for the AD-side SAML authentication request endpoint.
+   * (e.g. https://login.microsoftonline.com/tenant-id/saml2)
+   */
   AD_SAML_ENTRYPOINT_URL: unset<string>(),
+  /**
+   * SAML identity provider logout URL for AD authentication.
+   *
+   * This is the URL for the AD-side SAML logout request endpoint.
+   * (e.g. https://login.microsoftonline.com/tenant-id/saml2)
+   */
   AD_SAML_LOGOUT_URL: unset<string>(),
+  /**
+   * SAML issuer for AD authentication.
+   *
+   * This value should match the entityID specified in the SAML metadata sent to AD.
+   */
   AD_SAML_ISSUER: unset<string>(),
+  /**
+   * Comma-separated paths to SAML IDP public certificates in PEM format.
+   *
+   * If multiple certificates are provided, SAML messages signed by any of them are accepted.
+   */
   AD_SAML_PUBLIC_CERT: unset<string[]>(),
+  /**
+   * Path to SAML SP private certificate in PEM format
+   */
   AD_SAML_PRIVATE_CERT: unset<string>(),
 
+  // ----- Suomi.fi SAML authentication -----
+  /**
+   * @deprecated use SFI_MODE=mock instead
+   */
   SFI_MOCK: unset<boolean>(),
+  /**
+   * Chooses the SFI authentication mode and environment.
+   *
+   * prod = use SAML and Suomi.fi production environment
+   * test = use SAML and Suomi.fi test environment
+   * mock = use a mock implementation intended for local development only
+   *
+   * **Do not use "mock" in production**
+   */
   SFI_MODE: unset<'test' | 'prod' | 'mock'>(),
+  /**
+   * SAML service provider callback URL for SFI authentication.
+   *
+   * This is the URL for our SAML authentication response endpoint
+   * (e.g. https://evaka.example.com/api/application/auth/saml/login/callback)
+   */
   SFI_SAML_CALLBACK_URL: unset<string>(),
+  /**
+   * SAML identity provider entrypoint URL for SFI authentication.
+   *
+   * If SFI_MODE is set, this value is automatically set and is not required.
+   * This is the URL for the SFI-side SAML authentication request endpoint.
+   * (e.g. https://tunnistautuminen.suomi.fi/idp/profile/SAML2/Redirect/SSO)
+   */
   SFI_SAML_ENTRYPOINT: unset<string>(),
+  /**
+   * SAML identity provider entrypoint URL for SFI authentication.
+   *
+   * If SFI_MODE is set, this value is automatically set and is not required.
+   * This is the URL for the SFI-side SAML logout request endpoint.
+   * (e.g. https://tunnistautuminen.suomi.fi/idp/profile/SAML2/Redirect/SLO)
+   */
   SFI_SAML_LOGOUT_URL: unset<string>(),
+  /**
+   * SAML issuer for SFI authentication.
+   *
+   * This value should match the entityID specified in the SAML metadata sent to SFI.
+   */
   SFI_SAML_ISSUER: unset<string>(),
+  /**
+   * Comma-separated paths to SAML IDP public certificates in PEM format or known builtin certificate names.
+   *
+   * If SFI_MODE is set, the currently valid builtin certificates are automatically used and setting this variable is
+   * not required. If multiple certificates are provided, SAML messages signed by any of them are accepted.
+   */
   SFI_SAML_PUBLIC_CERT: unset<string[]>(),
+  /**
+   * Path to SAML SP private certificate in PEM format
+   */
   SFI_SAML_PRIVATE_CERT: unset<string>(),
 
+  // ----- eVaka employee Keycloak SAML authentication -----
+  /**
+   * SAML service provider callback URL for citizen Keycloak authentication.
+   *
+   * This is the URL for our SAML authentication response endpoint
+   * (e.g. https://evaka.example.com/api/internal/auth/evaka/login/callback)
+   */
   EVAKA_SAML_CALLBACK_URL: unset<string>(),
+  /**
+   * SAML identity provider entrypoint URL for citizen Keycloak authentication.
+   *
+   * This is the URL for the Keycloak-side SAML authentication request endpoint.
+   * (e.g. https://evaka.example.com/auth/realms/evaka/protocol/saml)
+   */
   EVAKA_SAML_ENTRYPOINT: unset<string>(),
+  /**
+   * SAML issuer for employee Keycloak authentication.
+   *
+   * This value should match the "Service provider entity ID" field in Keycloak configuration.
+   */
   EVAKA_SAML_ISSUER: unset<string>(),
+  /**
+   * Comma-separated paths to SAML IDP public certificates in PEM format.
+   *
+   * If multiple certificates are provided, SAML messages signed by any of them are accepted.
+   */
   EVAKA_SAML_PUBLIC_CERT: unset<string[]>(),
+  /**
+   * Path to SAML SP private certificate in PEM format
+   */
   EVAKA_SAML_PRIVATE_CERT: unset<string>(),
 
+  // ----- eVaka citizen Keycloak SAML authentication -----
+  /**
+   * SAML service provider callback URL for citizen Keycloak authentication.
+   *
+   * This is the URL for our SAML authentication response endpoint
+   * (e.g. https://evaka.example.com/api/application/auth/evaka-customer/login/callback)
+   */
   EVAKA_CUSTOMER_SAML_CALLBACK_URL: unset<string>(),
+  /**
+   * SAML identity provider entrypoint URL for citizen Keycloak authentication.
+   *
+   * This is the URL for the Keycloak-side SAML authentication request endpoint.
+   * (e.g. https://evaka.example.com/auth/realms/evaka-customer/protocol/saml)
+   */
   EVAKA_CUSTOMER_SAML_ENTRYPOINT: unset<string>(),
+  /**
+   * SAML issuer for citizen Keycloak authentication.
+   *
+   * This value should match the "Service provider entity ID" field in Keycloak configuration.
+   */
   EVAKA_CUSTOMER_SAML_ISSUER: unset<string>(),
+  /**
+   * Comma-separated paths to SAML IDP public certificates in PEM format.
+   *
+   * If multiple certificates are provided, SAML messages signed by any of them are accepted.
+   */
   EVAKA_CUSTOMER_SAML_PUBLIC_CERT: unset<string[]>(),
+  /**
+   * Path to SAML SP private certificate in PEM format
+   */
   EVAKA_CUSTOMER_SAML_PRIVATE_CERT: unset<string>(),
 
+  // ----- Titania integration -----
+  /**
+   * The username Titania uses to authenticate with eVaka using Basic Auth
+   */
   EVAKA_TITANIA_USERNAME: unset<string>(),
+  /**
+   * The password Titania uses to authenticate with eVaka using Basic Auth
+   */
   EVAKA_TITANIA_PASSWORD: unset<string>(),
 
+  // ----- Digitransit API integration -----
+  /**
+   * If true, enables proxying to Digitransit API
+   */
   DIGITRANSIT_API_ENABLED: true,
+  /**
+   * Base URL for the Digitransit API
+   */
   DIGITRANSIT_API_URL: 'https://api.digitransit.fi',
+  /**
+   * API key for the Digitransit API
+   */
   DIGITRANSIT_API_KEY: unset<string>()
 }
 
