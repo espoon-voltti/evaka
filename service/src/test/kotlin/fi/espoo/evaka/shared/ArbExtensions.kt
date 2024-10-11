1b1e9d16a3223065d41c99aa9855e6b6664878ed
Joonas Javanainen <joonas.javanainen@reaktor.com>
2024-10-11 12:05:05 +0300

    Add some property-based tests

--- service/src/test/kotlin/fi/espoo/evaka/shared/ArbExtensions.kt
+++ service/src/test/kotlin/fi/espoo/evaka/shared/ArbExtensions.kt
@@ -4,7 +4,9 @@
 
 package fi.espoo.evaka.shared
 
+import fi.espoo.evaka.shared.data.DateMap
 import fi.espoo.evaka.shared.data.DateSet
+import fi.espoo.evaka.shared.data.DateTimeMap
 import fi.espoo.evaka.shared.data.DateTimeSet
 import fi.espoo.evaka.shared.domain.FiniteDateRange
 import fi.espoo.evaka.shared.domain.HelsinkiDateTime
@@ -15,6 +17,7 @@ import io.kotest.property.arbitrary.list
 import io.kotest.property.arbitrary.localDate
 import io.kotest.property.arbitrary.long
 import io.kotest.property.arbitrary.map
+import io.kotest.property.arbitrary.pair
 import io.kotest.property.arbitrary.positiveLong
 import java.time.Duration
 import java.time.LocalDate
@@ -32,21 +35,29 @@ fun Arb.Companion.finiteDateRange(
 fun Arb.Companion.dateSet(): Arb<DateSet> =
     Arb.list(Arb.finiteDateRange()).map { ranges -> DateSet.of(ranges) }
 
+fun <T> Arb.Companion.dateMap(value: Arb<T>): Arb<DateMap<T>> =
+    Arb.list(Arb.pair(Arb.finiteDateRange(), value)).map { entries -> DateMap.of(entries) }
+
+fun <T> Arb.Companion.dateTimeMap(value: Arb<T>): Arb<DateTimeMap<T>> =
+    Arb.list(Arb.pair(Arb.helsinkiDateTimeRange(), value)).map { entries ->
+        DateTimeMap.of(entries)
+    }
+
 fun Arb.Companion.helsinkiDateTime(
     min: HelsinkiDateTime = HelsinkiDateTime.of(LocalDateTime.of(2019, 1, 1, 12, 0)),
     max: HelsinkiDateTime = HelsinkiDateTime.of(LocalDateTime.of(2030, 12, 31, 12, 0)),
 ): Arb<HelsinkiDateTime> =
-    Arb.long(min = 0, max = Duration.between(min.toInstant(), max.toInstant()).seconds).map {
-        secondsSinceMin ->
-        min.plusSeconds(secondsSinceMin)
+    Arb.long(min = 0, max = Duration.between(min.toInstant(), max.toInstant()).toNanos()).map {
+        nanosSinceMin ->
+        min.plus(Duration.ofNanos(nanosSinceMin))
     }
 
 fun Arb.Companion.helsinkiDateTimeRange(
     start: Arb<HelsinkiDateTime> = Arb.helsinkiDateTime(),
-    durationSeconds: Arb<Long> = Arb.positiveLong(max = 48L * 60L * 60L),
+    duration: Arb<Duration> = Arb.positiveLong(max = 48L * 60L * 60L).map { Duration.ofSeconds(it) },
 ): Arb<HelsinkiDateTimeRange> =
-    Arb.bind(start, durationSeconds) { startTimestamp, seconds ->
-        HelsinkiDateTimeRange(startTimestamp, startTimestamp.plusSeconds(seconds))
+    Arb.bind(start, duration) { startTimestamp, duration ->
+        HelsinkiDateTimeRange(startTimestamp, startTimestamp.plus(duration))
     }
 
 fun Arb.Companion.dateTimeSet(): Arb<DateTimeSet> =
