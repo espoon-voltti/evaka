3901aa854be14529194d71b1516eefdd7aae9974
Joosa Kurvinen <joosa.kurvinen@reaktor.com>
2024-10-21 11:57:06 +0300

    more updates

--- service/evaka-bom/build.gradle.kts
+++ service/evaka-bom/build.gradle.kts
@@ -67,6 +67,6 @@ dependencies {
     api(platform(libs.kotlin.bom))
     api(platform("org.junit:junit-bom:5.11.0"))
     api(platform(libs.spring.boot.dependencies))
-    api(platform("org.springframework:spring-framework-bom:6.1.13")) // only needed for CVE fix
+    api(platform("org.springframework:spring-framework-bom:6.1.14")) // only needed for CVE fix
     api(platform("software.amazon.awssdk:bom:2.28.1"))
 }
