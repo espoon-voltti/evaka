993fdbecec26bed56a2c9af1ab15177610be61b1
Joonas Javanainen <joonas@merulogic.com>
2024-11-06 15:27:50 +0200

    Add test for Keycloak argon2 password migration path

--- service/src/test/kotlin/fi/espoo/evaka/shared/auth/PasswordTest.kt
+++ service/src/test/kotlin/fi/espoo/evaka/shared/auth/PasswordTest.kt
@@ -6,6 +6,7 @@ package fi.espoo.evaka.shared.auth
 
 import fi.espoo.evaka.Sensitive
 import kotlin.test.*
+import org.bouncycastle.crypto.params.Argon2Parameters
 
 class PasswordTest {
     private val algorithm = PasswordHashAlgorithm.DEFAULT
@@ -37,5 +38,29 @@ class PasswordTest {
         val password = algorithm.encode(Sensitive("password"))
         val parsed = EncodedPassword.parse(password.toString())
         assertEquals(password, parsed)
+    }
+
+    @Test
+    fun `matching an Argon2 password originating from Keycloak is possible`() {
+        // Raw example keycloak credential data from the `credential` database table:
+        // secret_data:
+        // {"value":"O7vS90g14jWr4ESbUpJ3KX5y1NMZcMgjuqPHrZ4Eq8U=","salt":"LgSKeCa5qZH6Dh0PE17AwQ==","additionalParameters":{}}
+        // credential_data:
+        // {"hashIterations":5,"algorithm":"argon2","additionalParameters":{"hashLength":["32"],"memory":["7168"],"type":["id"],"version":["1.3"],"parallelism":["1"]}}
+        val password =
+            EncodedPassword(
+                PasswordHashAlgorithm.Argon2id(
+                    hashLength = 32,
+                    version = Argon2Parameters.ARGON2_VERSION_13,
+                    m = 7168,
+                    t = 5,
+                    p = 1,
+                ),
+                EncodedPassword.Salt.parse("LgSKeCa5qZH6Dh0PE17AwQ=="),
+                EncodedPassword.Hash.parse("O7vS90g14jWr4ESbUpJ3KX5y1NMZcMgjuqPHrZ4Eq8U="),
+            )
+        assertTrue(password.isMatch(Sensitive("testpassword")))
+        // make sure it also survives a format/parse round-trip
+        assertEquals(password, EncodedPassword.parse(password.toString()))
     }
 }
