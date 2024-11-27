20cb2ffcef247c7ef48af599a2a666386f0ab1ab
Joosa Kurvinen <joosa.kurvinen@reaktor.com>
2024-11-27 09:12:21 +0200

    remove deprecated departure endpoints

--- service/codegen/src/main/kotlin/evaka/codegen/api/Config.kt
+++ service/codegen/src/main/kotlin/evaka/codegen/api/Config.kt
@@ -39,11 +39,7 @@ val forceIncludes: Set<KType> =
     )
 
 // these endpoint paths are deprecated, and API clients should not call them
-private val deprecatedEndpoints: Set<String> =
-    setOf(
-        "/employee-mobile/attendances/units/{unitId}/children/{childId}/departure/expected-absences",
-        "/employee-mobile/attendances/units/{unitId}/children/{childId}/departure",
-    )
+private val deprecatedEndpoints: Set<String> = setOf()
 
 // these endpoint paths are planned, and API clients should not call them *yet*
 private val plannedEndpoints: Set<String> = setOf()
