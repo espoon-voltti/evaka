dad71fc2ca2cf6dc6c08fb004bb904b456f1094f
Petri Lehtinen <petri.lehtinen@reaktor.com>
2024-12-04 09:42:08 +0200

    Make `AssistanceActionOptionId` strict

--- service/codegen/src/main/kotlin/evaka/codegen/api/Config.kt
+++ service/codegen/src/main/kotlin/evaka/codegen/api/Config.kt
@@ -75,7 +75,6 @@ object Imports {
 // For others, a strict type is generated: `type FooId = Id<'Foo'>`.
 val lenientIdTypes =
     setOf(
-        DatabaseTable.AssistanceActionOption::class,
         DatabaseTable.AssistanceFactor::class,
         DatabaseTable.AssistanceNeedDecision::class,
         DatabaseTable.AssistanceNeedDecisionGuardian::class,
