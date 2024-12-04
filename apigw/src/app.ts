fc12d4548d217d87752057f279c57eb97b014a90
Joonas Javanainen <joonas@merulogic.com>
2024-12-04 17:37:46 +0200

    Move citizen routes to top-level

--- apigw/src/app.ts
+++ apigw/src/app.ts
@@ -2,18 +2,28 @@
 //
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
+import { SAML } from '@node-saml/node-saml'
 import express from 'express'
 import expressBasicAuth from 'express-basic-auth'
 
-import { enduserGwRouter } from './enduser/app.js'
+import { createDevSfiRouter } from './enduser/dev-sfi-auth.js'
+import { authenticateKeycloakCitizen } from './enduser/keycloak-citizen-saml.js'
+import mapRoutes from './enduser/mapRoutes.js'
+import { authStatus } from './enduser/routes/auth-status.js'
+import { authWeakLogin } from './enduser/routes/auth-weak-login.js'
+import { authenticateSuomiFi } from './enduser/suomi-fi-saml.js'
 import { internalGwRouter } from './internal/app.js'
 import { integrationUserHeader } from './shared/auth/index.js'
 import { appCommit, Config, titaniaConfig } from './shared/config.js'
 import { cacheControl } from './shared/middleware/cache-control.js'
+import { csrf } from './shared/middleware/csrf.js'
 import { errorHandler } from './shared/middleware/error-handler.js'
 import { createProxy } from './shared/proxy-utils.js'
 import { RedisClient } from './shared/redis-client.js'
 import { handleCspReport } from './shared/routes/csp.js'
+import createSamlRouter from './shared/routes/saml.js'
+import { createSamlConfig } from './shared/saml/index.js'
+import redisCacheProvider from './shared/saml/node-saml-cache-redis.js'
 import { sessionSupport } from './shared/session.js'
 
 export function apiRouter(config: Config, redisClient: RedisClient) {
@@ -23,6 +33,22 @@ export function apiRouter(config: Config, redisClient: RedisClient) {
       req.url = '/version'
     } else if (req.url.startsWith('/internal/integration/')) {
       req.url = req.url.replace('/internal/integration/', '/integration/')
+    } else if (req.url.startsWith('/application/citizen/')) {
+      req.url = req.url.replace('/application/citizen/', '/citizen/')
+    } else if (req.url.startsWith('/application/map-api/')) {
+      req.url = req.url.replace(
+        '/application/map-api/',
+        '/citizen/public/map-api/'
+      )
+    } else if (req.url.startsWith('/application/auth/saml/')) {
+      req.url = req.url.replace('/application/auth/saml/', '/citizen/auth/sfi/')
+    } else if (req.url.startsWith('/application/auth/evaka-customer/')) {
+      req.url = req.url.replace(
+        '/application/auth/evaka-customer/',
+        '/citizen/auth/keycloak/'
+      )
+    } else if (req.url.startsWith('/application/auth/')) {
+      req.url = req.url.replace('/application/auth/', '/citizen/auth/')
     }
     next()
   })
@@ -45,15 +71,90 @@ export function apiRouter(config: Config, redisClient: RedisClient) {
   })
 
   const citizenSessions = sessionSupport('enduser', redisClient, config.citizen)
+  const citizenProxy = createProxy({
+    getUserHeader: (req) => citizenSessions.getUserHeader(req)
+  })
   const internalSessions = sessionSupport(
     'employee',
     redisClient,
     config.employee
   )
 
+  if (config.sfi.type === 'mock') {
+    router.use(
+      '/citizen/auth/sfi/',
+      citizenSessions.middleware,
+      createDevSfiRouter(citizenSessions)
+    )
+  } else if (config.sfi.type === 'saml') {
+    router.use(
+      '/citizen/auth/sfi/',
+      citizenSessions.middleware,
+      createSamlRouter({
+        sessions: citizenSessions,
+        strategyName: 'suomifi',
+        saml: new SAML(
+          createSamlConfig(
+            config.sfi.saml,
+            redisCacheProvider(redisClient, { keyPrefix: 'suomifi-saml-resp:' })
+          )
+        ),
+        authenticate: authenticateSuomiFi,
+        defaultPageUrl: '/'
+      })
+    )
+  }
+
+  if (!config.keycloakCitizen)
+    throw new Error('Missing Keycloak SAML configuration (citizen)')
   router.use(
-    '/application',
-    enduserGwRouter(config, redisClient, { citizenSessions })
+    '/citizen/auth/keycloak/',
+    citizenSessions.middleware,
+    createSamlRouter({
+      sessions: citizenSessions,
+      strategyName: 'evaka-customer',
+      saml: new SAML(
+        createSamlConfig(
+          config.keycloakCitizen,
+          redisCacheProvider(redisClient, { keyPrefix: 'customer-saml-resp:' })
+        )
+      ),
+      authenticate: authenticateKeycloakCitizen,
+      defaultPageUrl: '/'
+    })
+  )
+
+  router.use(
+    '/citizen/public/map-api',
+    csrf,
+    citizenSessions.middleware,
+    mapRoutes
+  )
+  router.all(
+    '/citizen/public/*',
+    csrf,
+    citizenSessions.middleware,
+    citizenProxy
+  )
+  router.get(
+    '/citizen/auth/status',
+    csrf,
+    citizenSessions.middleware,
+    authStatus(citizenSessions)
+  )
+  router.post(
+    '/citizen/auth/weak-login',
+    csrf,
+    citizenSessions.middleware,
+    express.json(),
+    authWeakLogin(citizenSessions, redisClient)
+  )
+  router.all(
+    '/citizen/*',
+    csrf,
+    citizenSessions.middleware,
+    citizenSessions.requireAuthentication,
+    citizenProxy
   )
   router.use(
     '/internal',
