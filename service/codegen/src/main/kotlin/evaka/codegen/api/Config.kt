e9580ea42d97f180bcb5533515acbfd8e3757b95
Petri Lehtinen <petri.lehtinen@reaktor.com>
2024-12-10 08:21:44 +0200

    Make `DailyServiceTimeNotificationId` strict

--- service/codegen/src/main/kotlin/evaka/codegen/api/Config.kt
+++ service/codegen/src/main/kotlin/evaka/codegen/api/Config.kt
@@ -75,7 +75,6 @@ object Imports {
 // For others, a strict type is generated: `type FooId = Id<'Foo'>`.
 val lenientIdTypes =
     setOf(
-        DatabaseTable.DailyServiceTimeNotification::class,
         DatabaseTable.Daycare::class,
         DatabaseTable.DaycareAssistance::class,
         DatabaseTable.DaycareCaretaker::class,
