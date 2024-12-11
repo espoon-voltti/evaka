c29ba0a6806e6c48b858a5f32c54dd018e89e56a
Mikko Savolainen <mikko.savolainen@gofore.com>
2024-12-11 12:18:58 +0200

    remove unused feature flag from frontend

--- frontend/src/lib-customizations/types.d.ts
+++ frontend/src/lib-customizations/types.d.ts
@@ -278,12 +278,6 @@ interface BaseFeatureFlags {
    * Enable support for citizen weak login
    */
   weakLogin?: boolean
-
-  /**
-   * Select the type of holiday questionnaire
-   * Note the corresponding backend environment variable feature flag.
-   */
-  holidayQuestionnaireType?: 'FIXED_PERIOD' | 'OPEN_RANGES'
 }
 
 export type FeatureFlags = DeepReadonly<BaseFeatureFlags>
