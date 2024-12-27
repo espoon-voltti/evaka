cae30fdc8e7fd774d46fe8bc7b81ebc581f1c83e
Joonas Javanainen <joonas@merulogic.com>
2024-12-27 08:52:00 +0200

    Upgrade Logback + Logback Access

--- service/evaka-bom/build.gradle.kts
+++ service/evaka-bom/build.gradle.kts
@@ -8,7 +8,11 @@ javaPlatform { allowDependencies() }
 
 dependencies {
     constraints {
-        api("ch.qos.logback.access:tomcat:2.0.2")
+        api("ch.qos.logback.access:logback-access-tomcat:2.0.5")
+
+        // These constraints are needed for CVE fixes
+        api("ch.qos.logback:logback-classic:1.5.15")
+        api("ch.qos.logback:logback-core:1.5.15")
 
         // These constraints are needed for CVE fixes
         api("org.apache.tomcat.embed:tomcat-embed-core:10.1.34")
