661dbfd92d005ed8cb5849f3fe0fec00a3929a3a
Joonas Javanainen <joonas@merulogic.com>
2024-12-30 14:35:52 +0200

    Add tests for e-mail verification

---
+++ frontend/src/e2e-test/specs/0_citizen/citizen-email-verification.spec.ts
@@ -0,0 +1,92 @@
+// SPDX-FileCopyrightText: 2017-2024 City of Espoo
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+import HelsinkiDateTime from 'lib-common/helsinki-date-time'
+
+import { Fixture } from '../../dev-api/fixtures'
+import {
+  getSentEmails,
+  resetServiceState,
+  runJobs
+} from '../../generated/api-clients'
+import { DevPerson } from '../../generated/api-types'
+import CitizenHeader from '../../pages/citizen/citizen-header'
+import CitizenPersonalDetailsPage from '../../pages/citizen/citizen-personal-details'
+import { Page } from '../../utils/page'
+import { enduserLogin } from '../../utils/user'
+
+const mockedTime = HelsinkiDateTime.of(2024, 1, 1, 12, 0)
+let page: Page
+
+beforeEach(async () => {
+  await resetServiceState()
+})
+
+describe('Citizen e-mail verification', () => {
+  it('works for persons who already have an unverified e-mail set', async () => {
+    const citizen = await Fixture.person({
+      email: 'test@example.com'
+    }).saveAdult({
+      updateMockVtjWithDependants: []
+    })
+    page = await Page.open({ mockedTime })
+
+    const personalDetailsPage = await openPersonalDetailsPage(citizen)
+    const section = personalDetailsPage.personalDetailsSection
+    await section.unverifiedEmailStatus.waitUntilVisible()
+
+    await section.sendVerificationCode.click()
+    await section.verificationCodeField.waitUntilVisible()
+    await runJobs({ mockedTime })
+
+    const verificationCode = await getVerificationCodeFromEmail()
+    expect(verificationCode).toBeTruthy()
+    await section.verificationCodeField.fill(verificationCode ?? '')
+    await section.verifyEmail.click()
+
+    await section.verifiedEmailStatus.waitUntilVisible()
+  })
+  test('if a person has a verified e-mail and they change it, it requires re-verification', async () => {
+    const citizen = await Fixture.person({
+      email: 'test@example.com',
+      verifiedEmail: 'test@example.com'
+    }).saveAdult({
+      updateMockVtjWithDependants: []
+    })
+    page = await Page.open({ mockedTime })
+
+    const personalDetailsPage = await openPersonalDetailsPage(citizen)
+    const section = personalDetailsPage.personalDetailsSection
+    await section.verifiedEmailStatus.waitUntilVisible()
+
+    await section.editPersonalData(
+      {
+        preferredName: citizen.firstName.split(' ')[1],
+        email: 'unverified@example.com',
+        phone: citizen.phone,
+        backupPhone: citizen.backupPhone
+      },
+      true
+    )
+    await section.unverifiedEmailStatus.waitUntilVisible()
+  })
+})
+
+async function openPersonalDetailsPage(citizen: DevPerson) {
+  await enduserLogin(page, citizen)
+  const header = new CitizenHeader(page)
+  await header.selectTab('personal-details')
+  return new CitizenPersonalDetailsPage(page)
+}
+
+async function getVerificationCodeFromEmail(): Promise<string | null> {
+  const emails = await getSentEmails()
+  if (emails.length === 0) return null
+  expect(emails.length).toBe(1)
+  const email = emails[0]
+
+  const verificationCodeRegex = /[0-9]{8}/
+  const matches = verificationCodeRegex.exec(email.content.text)
+  return matches ? matches[0] : null
+}
