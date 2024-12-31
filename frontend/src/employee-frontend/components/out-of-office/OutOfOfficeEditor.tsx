89047bbecb4981cd646b3838e45f4a4957fd2d95
Tero Laakso <tero.laakso@reaktor.com>
2024-12-31 14:11:48 +0200

    Enable editing of out of office ranges

--- frontend/src/employee-frontend/components/out-of-office/OutOfOfficeEditor.tsx
+++ frontend/src/employee-frontend/components/out-of-office/OutOfOfficeEditor.tsx
@@ -9,6 +9,7 @@ import { localDateRange } from 'lib-common/form/fields'
 import { object, required } from 'lib-common/form/form'
 import { useForm, useFormFields } from 'lib-common/form/hooks'
 import { StateOf } from 'lib-common/form/types'
+import { OutOfOfficePeriod } from 'lib-common/generated/api-types/outofoffice'
 import LocalDate from 'lib-common/local-date'
 import { useMutationResult } from 'lib-common/query'
 import { AsyncButton } from 'lib-components/atoms/buttons/AsyncButton'
@@ -21,22 +22,29 @@ import { upsertOutOfOfficePeriodMutation } from './queries'
 
 const outOfOfficeForm = object({ period: required(localDateRange()) })
 
-function initialFormState(): StateOf<typeof outOfOfficeForm> {
+function initialFormState(
+  editedPeriod: OutOfOfficePeriod | null
+): StateOf<typeof outOfOfficeForm> {
+  const rangeConfig = { minDate: LocalDate.todayInSystemTz() }
   return {
-    period: localDateRange.empty({ minDate: LocalDate.todayInSystemTz() })
+    period: editedPeriod
+      ? localDateRange.fromRange(editedPeriod.period, rangeConfig)
+      : localDateRange.empty(rangeConfig)
   }
 }
 
 export interface OutOfOfficeEditorProps {
   onClose: () => void
+  editedPeriod: OutOfOfficePeriod | null
 }
 
 export default React.memo(function OutOfOfficeEditor({
-  onClose
+  onClose,
+  editedPeriod
 }: OutOfOfficeEditorProps) {
   const { i18n, lang } = useTranslation()
 
-  const form = useForm(outOfOfficeForm, initialFormState, {
+  const form = useForm(outOfOfficeForm, () => initialFormState(editedPeriod), {
     ...i18n.validationErrors
   })
 
@@ -49,7 +57,7 @@ export default React.memo(function OutOfOfficeEditor({
   const onSubmit = () =>
     upsertOutOfOffice({
       body: {
-        id: null,
+        id: editedPeriod?.id ?? null,
         period: period.value()
       }
     })
