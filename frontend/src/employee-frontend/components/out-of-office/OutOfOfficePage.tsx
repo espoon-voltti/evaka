59d1e0b04278ea2dc8b39eab75b00293ae12a696
Tero Laakso <tero.laakso@reaktor.com>
2024-12-31 13:04:25 +0200

    Display existing out of office periods

--- frontend/src/employee-frontend/components/out-of-office/OutOfOfficePage.tsx
+++ frontend/src/employee-frontend/components/out-of-office/OutOfOfficePage.tsx
@@ -3,18 +3,27 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
 import React, { Fragment, useState } from 'react'
+import styled from 'styled-components'
 
 import { useTranslation } from 'employee-frontend/state/i18n'
+import { Result } from 'lib-common/api'
+import { useQueryResult } from 'lib-common/query'
+import { AsyncButton } from 'lib-components/atoms/buttons/AsyncButton'
 import { Button } from 'lib-components/atoms/buttons/Button'
 import Container, { ContentArea } from 'lib-components/layout/Container'
 import { H1, Label, P } from 'lib-components/typography'
-import { Gap } from 'lib-components/white-space'
+import { defaultMargins, Gap } from 'lib-components/white-space'
+import { faPen, faTrash } from 'lib-icons'
 
 import OutOfOfficeEditor from './OutOfOfficeEditor'
+import { outOfOfficePeriodsQuery } from './queries'
 
 export default React.memo(function OutOfOfficePage() {
   const { i18n } = useTranslation()
   const [isEditing, setIsEditing] = useState(false)
+
+  const getPeriodsResult = useQueryResult(outOfOfficePeriodsQuery())
+  const periods = getPeriodsResult.getOrElse([])
 
   return (
     <Container>
@@ -24,7 +33,33 @@ export default React.memo(function OutOfOfficePage() {
         <Gap size="m" />
         <Label>{i18n.outOfOffice.header}</Label>
         <Gap size="s" />
-        {isEditing ? (
+        {periods.length > 0 ? (
+          <PeriodListContainer>
+            {periods.map((period) => (
+              <li key={period.id}>
+                <PeriodItemContainer>
+                  <div>{period.period.format()}</div>
+                  <Button
+                    text={i18n.common.edit}
+                    appearance="inline"
+                    icon={faPen}
+                  />
+                  <AsyncButton
+                    text={i18n.common.remove}
+                    onClick={function (): void | Promise<Result<unknown>> {
+                      throw new Error('Function not implemented.')
+                    }}
+                    onSuccess={function (value: unknown): void {
+                      throw new Error('Function not implemented.')
+                    }}
+                    appearance="inline"
+                    icon={faTrash}
+                  />
+                </PeriodItemContainer>
+              </li>
+            ))}
+          </PeriodListContainer>
+        ) : isEditing ? (
           <OutOfOfficeEditor onClose={() => setIsEditing(false)} />
         ) : (
           <Fragment>
@@ -41,3 +76,20 @@ export default React.memo(function OutOfOfficePage() {
     </Container>
   )
 })
+
+const PeriodListContainer = styled.ul`
+  margin-block-start: 0;
+  padding-inline-start: ${defaultMargins.m};
+  display: flex;
+  flex-direction: column;
+  gap: ${defaultMargins.m};
+`
+const PeriodItemContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: ${defaultMargins.m};
+
+  & > :first-child {
+    margin-right: ${defaultMargins.s};
+  }
+`
