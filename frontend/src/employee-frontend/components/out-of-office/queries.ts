a734edd7de4e32537b16eff571dfca90a67595db
Tero Laakso <tero.laakso@reaktor.com>
2025-01-10 13:59:23 +0200

    Fix out of office queries after rebase

--- frontend/src/employee-frontend/components/out-of-office/queries.ts
+++ frontend/src/employee-frontend/components/out-of-office/queries.ts
@@ -1,33 +1,25 @@
-// SPDX-FileCopyrightText: 2017-2024 City of Espoo
+// SPDX-FileCopyrightText: 2017-2025 City of Espoo
 //
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
-import { mutation, query } from 'lib-common/query'
-import { Arg0 } from 'lib-common/types'
+import { Queries } from 'lib-common/query'
 
 import {
   deleteOutOfOfficePeriod,
   getOutOfOfficePeriods,
   upsertOutOfOfficePeriod
 } from '../../generated/api-clients/outofoffice'
-import { createQueryKeys } from '../../query'
 
-const queryKeys = createQueryKeys('outOfOffice', {
-  periods: () => ['outOfOfficePeriods']
-})
+const q = new Queries()
 
-export const outOfOfficePeriodsQuery = query({
-  api: getOutOfOfficePeriods,
-  queryKey: queryKeys.periods
-})
+export const outOfOfficePeriodsQuery = q.query(getOutOfOfficePeriods)
 
-export const deleteOutOfOfficePeriodMutation = mutation({
-  api: deleteOutOfOfficePeriod,
-  invalidateQueryKeys: () => [queryKeys.periods()]
-})
+export const deleteOutOfOfficePeriodMutation = q.mutation(
+  deleteOutOfOfficePeriod,
+  [outOfOfficePeriodsQuery]
+)
 
-export const upsertOutOfOfficePeriodMutation = mutation({
-  api: (arg: Arg0<typeof upsertOutOfOfficePeriod>) =>
-    upsertOutOfOfficePeriod(arg),
-  invalidateQueryKeys: () => [queryKeys.periods()]
-})
+export const upsertOutOfOfficePeriodMutation = q.mutation(
+  upsertOutOfOfficePeriod,
+  [outOfOfficePeriodsQuery]
+)
