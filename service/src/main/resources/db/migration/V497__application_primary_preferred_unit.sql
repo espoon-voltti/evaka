a6e84fda8c02f03c195dba6ae9ae727dec3a09b4
Petri Lehtinen <petri.lehtinen@reaktor.com>
2025-02-12 14:24:58 +0200

    Fix constraint name

--- service/src/main/resources/db/migration/V497__application_primary_preferred_unit.sql
+++ service/src/main/resources/db/migration/V497__application_primary_preferred_unit.sql
@@ -2,4 +2,4 @@ ALTER TABLE application ADD COLUMN primary_preferred_unit uuid
     GENERATED ALWAYS AS ((document -> 'apply' -> 'preferredUnits' ->> 0)::uuid) STORED
     CONSTRAINT fk$primary_preferred_unit REFERENCES daycare (id);
 
-CREATE INDEX idx$application_primary_unit_status ON application (primary_preferred_unit);
+CREATE INDEX idx$application_primary_preferred_unit ON application (primary_preferred_unit);
