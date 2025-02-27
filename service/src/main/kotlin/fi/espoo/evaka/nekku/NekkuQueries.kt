a1fd648cdec9d864977a198b3ed8ac8700f9dcf1
tahtee <96183234+tahtee@users.noreply.github.com>
2025-02-27 15:01:57 +0200

    First Nekku get customers to db

--- service/src/main/kotlin/fi/espoo/evaka/nekku/NekkuQueries.kt
+++ service/src/main/kotlin/fi/espoo/evaka/nekku/NekkuQueries.kt
@@ -4,31 +4,32 @@
 
 package fi.espoo.evaka.nekku
 
+import fi.espoo.evaka.decision.logger
 import fi.espoo.evaka.shared.db.Database
 import org.jdbi.v3.core.mapper.PropagateNull
 
 data class CustomerNumbers(@PropagateNull val number: String, val name: String)
 
-/** Throws an IllegalStateException if Jamix returns an empty texture list. */
+/** Throws an IllegalStateException if Nekku returns an empty texture list. */
 fun fetchAndUpdateNekkuCustomers(
     client: NekkuClient,
     db: Database.Connection,
     warner: (s: String) -> Unit = loggerWarner,
 ) {
-    val customersFromNekku =
-        client.getCustomers().map { it -> CustomerNumbers(it.number, it.name) }
+    val customersFromNekku = client.getCustomers().map { it -> CustomerNumbers(it.number, it.name) }
 
     if (customersFromNekku.isEmpty())
         error("Refusing to sync empty Nekku customer list into database")
     db.transaction { tx ->
-        val nulledCustomersCount = tx.resetNekkuCustomerNumbersNotContainedWithin(customersFromNekku)
+        val nulledCustomersCount =
+            tx.resetNekkuCustomerNumbersNotContainedWithin(customersFromNekku)
         if (nulledCustomersCount != 0)
             warner(
                 "Nekku custoner list update caused $nulledCustomersCount customer numbers to be set to null"
             )
-        val deletedMealTexturesCount = tx.setCustomerNumbers(customersFromNekku)
+        val deletedCustomerCount = tx.setCustomerNumbers(customersFromNekku)
         logger.info {
-            "Deleted: $deletedMealTexturesCount Nekku customer numbers, inserted ${customersFromNekku.size}"
+            "Deleted: $deletedCustomerCount Nekku customer numbers, inserted ${customersFromNekku.size}"
         }
     }
 }
@@ -36,25 +37,24 @@ fun fetchAndUpdateNekkuCustomers(
 fun Database.Transaction.resetNekkuCustomerNumbersNotContainedWithin(
     nekkuCustomerNumbers: List<CustomerNumbers>
 ): Int {
-    val newMealTextureIds = nekkuCustomerNumbers.map { it.number }
+    val newNekkuCustomerNumbers = nekkuCustomerNumbers.map { it.number }
     val affectedRows = execute {
         sql(
-            "UPDATE daycare_group SET nekku_customer_number = null WHERE nekku_customer_number != ALL (${bind(newMealTextureIds)})"
+            "UPDATE daycare_group SET nekku_customer_number = null WHERE nekku_customer_number != ALL (${bind(newNekkuCustomerNumbers)})"
         )
     }
     return affectedRows
 }
 
-
 fun Database.Transaction.setCustomerNumbers(customerNumbers: List<CustomerNumbers>): Int {
     val newCustomerNumbers = customerNumbers.map { it.number }
     val deletedCustomerCount = execute {
-        sql("DELETE FROM nekku_customer_number WHERE number != ALL (${bind(newCustomerNumbers)})")
+        sql("DELETE FROM nekku_customer WHERE number != ALL (${bind(newCustomerNumbers)})")
     }
-    executeBatch(newCustomerNumbers) {
+    executeBatch(customerNumbers) {
         sql(
             """
-INSERT INTO nekku_customer_number (number, name)
+INSERT INTO nekku_customer (number, name)
 VALUES (
     ${bind{it.number}},
     ${bind{it.name}}
@@ -65,4 +65,9 @@ ON CONFLICT (number) DO UPDATE SET
         )
     }
     return deletedCustomerCount
+}
+
+fun Database.Transaction.getNekkuCustomers(): List<NekkuClient.NekkuCustomer> {
+    return createQuery { sql("SELECT number, name FROM nekku_customer") }
+        .toList<NekkuClient.NekkuCustomer>()
 }
