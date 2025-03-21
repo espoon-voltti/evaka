e4b6061dd8f0a479ff17fe8c5a3acff3745bf2b7
Jonni Madekivi <jonni.madekivi@reaktor.com>
2025-03-21 15:37:58 +0200

    Tekninen: Eslint LocalDate triple equals sääntö (#6537)
    * works for simple cases
    
    * works better
    
    * works for parseIso construction type
    
    * works for more
    
    * fix lint errors
    
    * Another way to check instance type info
    
    * typescript-eslint dependency
    
    * Remove unnecessary dependecies

---
+++ frontend/eslint-plugin/rules/no-localdate-triple-equals.js
@@ -0,0 +1,111 @@
+// SPDX-FileCopyrightText: 2024 City of Espoo
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+module.exports = {
+  meta: {
+    type: 'problem',
+    docs: {
+      description:
+        'Disallow triple equals comparison between LocalDate instances',
+      category: 'Possible Errors',
+      recommended: true
+    },
+    schema: [],
+    messages: {
+      noTripleEquals:
+        'Do not use triple equals (===) to compare LocalDate instances. Use isEqual() method instead.'
+    }
+  },
+  create(context) {
+    let hasLocalDateImport = false
+    let localDateIdentifier = null
+    const localDateVariables = new Set()
+
+    const isLocalDateTypeAnnotation = (typeAnnotation) => {
+      if (!typeAnnotation) return false
+
+      if (typeAnnotation.type === 'TSTypeReference') {
+        return typeAnnotation.typeName.name === 'LocalDate'
+      }
+
+      if (typeAnnotation.type === 'TSUnionType') {
+        return typeAnnotation.types.some(isLocalDateTypeAnnotation)
+      }
+
+      return false
+    }
+
+    const isLocalDateInstance = (node) => {
+      if (!node) return false
+
+      // Check if it's a variable we know is a LocalDate
+      if (node.type === 'Identifier' && localDateVariables.has(node.name)) {
+        return true
+      }
+
+      // Check if it's a direct LocalDate constructor call or static method
+      if (
+        node.type === 'CallExpression' &&
+        node.callee?.type === 'MemberExpression' &&
+        node.callee.object?.type === 'Identifier' &&
+        node.callee.object.name === localDateIdentifier
+      ) {
+        return true
+      }
+
+      return false
+    }
+
+    return {
+      ImportDeclaration(node) {
+        if (node.source.value.endsWith('local-date')) {
+          hasLocalDateImport = true
+          if (node.specifiers?.[0]?.local) {
+            localDateIdentifier = node.specifiers[0].local.name
+          }
+        }
+      },
+
+      VariableDeclarator(node) {
+        // Track variables initialized with LocalDate instances
+        if (node.init && isLocalDateInstance(node.init)) {
+          localDateVariables.add(node.id.name)
+        }
+      },
+
+      TSParameterProperty(node) {
+        // Track parameters with LocalDate type
+        if (
+          node.parameter?.type === 'Identifier' &&
+          isLocalDateTypeAnnotation(
+            node.parameter.typeAnnotation?.typeAnnotation
+          )
+        ) {
+          localDateVariables.add(node.parameter.name)
+        }
+      },
+
+      Identifier(node) {
+        // Track variables with LocalDate type annotation
+        if (isLocalDateTypeAnnotation(node.typeAnnotation?.typeAnnotation)) {
+          localDateVariables.add(node.name)
+        }
+      },
+
+      BinaryExpression(node) {
+        if (!hasLocalDateImport || node.operator !== '===') {
+          return
+        }
+
+        // Check if both operands are LocalDate instances
+        if (isLocalDateInstance(node.left) && isLocalDateInstance(node.right)) {
+          context.report({
+            node,
+            messageId: 'noTripleEquals'
+          })
+        }
+      }
+    }
+  }
+}
---
+++ frontend/eslint-plugin/rules/no-localdate-triple-equals.spec.js
@@ -0,0 +1,124 @@
+// SPDX-FileCopyrightText: 2024 City of Espoo
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+const { RuleTester } = require('eslint')
+const typescriptEslint = require('typescript-eslint')
+
+const rule = require('./no-localdate-triple-equals')
+
+const ruleTester = new RuleTester({
+  languageOptions: {
+    parser: typescriptEslint.parser,
+    parserOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module'
+    }
+  }
+})
+
+ruleTester.run('no-localdate-triple-equals', rule, {
+  valid: [
+    {
+      code: `
+        import LocalDate from './local-date'
+        const date1 = LocalDate.of(2024, 1, 1)
+        const date2 = LocalDate.of(2024, 1, 1)
+        if (date1.isEqual(date2)) {}
+        if ('asd' === 'foober') {}
+      `
+    },
+    {
+      code: `
+        const num1 = 1
+        const num2 = 1
+        if (num1 === num2) {}
+      `
+    },
+    {
+      code: `
+        import LocalDate from './local-date'
+        function foo(param: LocalDate | undefined) {
+          return (param === undefined)
+        }
+      `
+    },
+    {
+      code: `
+        import LocalDate from './local-date'
+        const isInSameTerm = (
+          date1: LocalDate,
+          date2: LocalDate,
+          terms: FiniteDateRange[]
+        ) => {
+          const term1 = terms.find((term) => term.includes(date1))
+          const term2 = terms.find((term) => term.includes(date2))
+          return term1 === term2
+        }
+      `
+    }
+  ],
+  invalid: [
+    {
+      code: `
+        import LocalDate from './local-date'
+        const date1 = LocalDate.of(2024, 1, 1)
+        const date2 = LocalDate.of(2024, 1, 1)
+        if (date1 === date2) {}
+      `,
+      errors: [
+        {
+          messageId: 'noTripleEquals'
+        }
+      ]
+    },
+    {
+      code: `
+        import LocalDate from './local-date'
+        const date1 = LocalDate.of(2024, 1, 1)
+        function foo(param: LocalDate) {
+          const date2 = LocalDate.of(2024, 1, 1)
+          return (param === date2)
+        }
+        foo(date1)
+      `,
+      errors: [
+        {
+          messageId: 'noTripleEquals'
+        }
+      ]
+    },
+    {
+      code: `
+        import LocalDate from './local-date'
+        const validator = (preferredStartDate: LocalDate | null) =>
+          (val: LocalDate | null): string | undefined =>
+            val && preferredStartDate && (val === preferredStartDate)
+              ? undefined
+              : 'error'
+      `,
+      errors: [
+        {
+          messageId: 'noTripleEquals'
+        }
+      ]
+    },
+    {
+      code: `
+        import LocalDate from './local-date'
+        describe('LocalDate', () => {
+          it('supports comparisons', () => {
+            const middle = LocalDate.of(2020, 7, 7)
+            const duplicate = LocalDate.parseIso(middle.formatIso())
+            expect(duplicate === middle).toBeFalsy()
+          })
+        })
+      `,
+      errors: [
+        {
+          messageId: 'noTripleEquals'
+        }
+      ]
+    }
+  ]
+})
