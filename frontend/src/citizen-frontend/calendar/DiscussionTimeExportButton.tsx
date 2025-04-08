855b99bd6d9210d861c1e6ea26ef8f7f158f209e
Tatu Repo <tatu.repo@gofore.com>
2025-04-08 13:29:42 +0300

    Add calendar content encoding and replace vtz library with static copy of EET VTZ

--- frontend/src/citizen-frontend/calendar/DiscussionTimeExportButton.tsx
+++ frontend/src/citizen-frontend/calendar/DiscussionTimeExportButton.tsx
@@ -3,7 +3,6 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
 import ical, { ICalCalendarMethod } from 'ical-generator'
-import { getZoneString } from 'icalzone'
 import React, { useCallback } from 'react'
 
 import { useTranslation } from 'citizen-frontend/localization'
@@ -11,6 +10,26 @@ import { CitizenCalendarEventTime } from 'lib-common/generated/api-types/calenda
 import HelsinkiDateTime from 'lib-common/helsinki-date-time'
 import { Button } from 'lib-components/atoms/buttons/Button'
 import { faCalendar } from 'lib-icons'
+
+const helsinkiVTZLines = [
+  'BEGIN:VTIMEZONE',
+  'TZID:Europe/Helsinki',
+  'BEGIN:STANDARD',
+  'TZNAME:EET',
+  'TZOFFSETFROM:+0300',
+  'TZOFFSETTO:+0200',
+  'DTSTART:19701025T040000',
+  'RRULE:FREQ=YEARLY;BYMONTH=10;BYDAY=-1SU',
+  'END:STANDARD',
+  'BEGIN:DAYLIGHT',
+  'TZNAME:EEST',
+  'TZOFFSETFROM:+0200',
+  'TZOFFSETTO:+0300',
+  'DTSTART:19700329T030000',
+  'RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=-1SU',
+  'END:DAYLIGHT',
+  'END:VTIMEZONE'
+]
 
 interface DiscussionTimeAttendeeInfo {
   groupName: string | null
@@ -36,8 +55,8 @@ export const DiscussionTimeExportButton = React.memo(
       const calendar = ical()
       calendar.method(ICalCalendarMethod.REQUEST)
       calendar.timezone({
-        name: 'VTZ',
-        generator: (zoneName: string) => getZoneString(zoneName) ?? null
+        name: 'EET',
+        generator: () => helsinkiVTZLines.join('\r\n')
       })
       calendar.createEvent({
         summary: eventTitle,
@@ -49,12 +68,11 @@ export const DiscussionTimeExportButton = React.memo(
         end: HelsinkiDateTime.fromLocal(
           discussionTime.date,
           discussionTime.endTime
-        ).formatIso(),
-        timezone: 'Europe/Helsinki'
+        ).formatIso()
       })
       const link = document.createElement('a')
       link.download = fileName
-      link.href = `data:text/calendar;charset=utf-8,${calendar.toString()}`
+      link.href = `data:text/calendar;charset=utf-8,${encodeURIComponent(calendar.toString())}`
       document.body.appendChild(link)
       link.click()
       document.body.removeChild(link)
