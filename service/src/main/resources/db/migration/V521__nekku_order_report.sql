3370f3ff21366d72379e07c9726e9f3b15da0276
tahtee <96183234+tahtee@users.noreply.github.com>
2025-04-24 12:21:18 +0300

    Updated nekku_order_report sql

--- service/src/main/resources/db/migration/V521__nekku_order_report.sql
+++ service/src/main/resources/db/migration/V521__nekku_order_report.sql
@@ -10,18 +10,18 @@ CREATE TABLE nekku_order (
 
 CREATE TABLE nekku_item (
     item_id UUID PRIMARY KEY,
-    order_id INT NOT NULL,
+    nekku_order_id UUID NOT NULL,
     sku TEXT NOT NULL,
     quantity INT NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES nekku_order (order_id)
+    FOREIGN KEY (nekku_order_id) REFERENCES nekku_order (order_id)
 );
 
 
 
 CREATE TABLE nekku_item_product_options (
-    item_id INT NOT NULL,
+    nekku_item_id UUID NOT NULL,
     field_id TEXT NOT NULL,
     value TEXT NOT NULL,
-    FOREIGN KEY (item_id) REFERENCES nekku_item (item_id)
+    FOREIGN KEY (nekku_item_id) REFERENCES nekku_item (item_id)
 );
 
