a63b90467cecdf097586473d7a1204860aa9893e
Vesa Honkasaari <vesa.honkasaari@gofore.com>
2025-05-05 15:48:55 +0300

    Handle additional special diet field types which might come from Nekku

---
+++ service/src/main/resources/db/migration/V525__nekku_special_diet_type_additions.sql
@@ -0,0 +1,4 @@
+ALTER TYPE public.nekku_special_diet_type ADD VALUE 'CHECKBOX';
+ALTER TYPE public.nekku_special_diet_type ADD VALUE 'RADIO';
+ALTER TYPE public.nekku_special_diet_type ADD VALUE 'TEXTAREA';
+ALTER TYPE public.nekku_special_diet_type ADD VALUE 'EMAIL';
