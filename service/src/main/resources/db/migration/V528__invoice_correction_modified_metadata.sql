a958ac4249dbf42c39da85485cd0e57d440ae926
Joosa Kurvinen <joosa.kurvinen@reaktor.com>
2025-05-08 14:19:38 +0300

    add indexes, update tests

--- service/src/main/resources/db/migration/V528__invoice_correction_modified_metadata.sql
+++ service/src/main/resources/db/migration/V528__invoice_correction_modified_metadata.sql
@@ -2,6 +2,7 @@ ALTER TABLE invoice_correction RENAME COLUMN created TO created_at;
 
 ALTER TABLE invoice_correction ADD COLUMN created_by UUID REFERENCES evaka_user(id) NOT NULL DEFAULT '00000000-0000-0000-0000-000000000000'::UUID;
 ALTER TABLE invoice_correction ALTER COLUMN created_by DROP DEFAULT;
+CREATE INDEX fk$invoice_correction_created_by ON invoice_correction(created_by);
 
 DROP TRIGGER set_timestamp ON invoice_correction;
 ALTER TABLE invoice_correction RENAME COLUMN updated TO updated_at;
@@ -13,3 +14,6 @@ ALTER TABLE invoice_correction ALTER COLUMN modified_at SET NOT NULL;
 
 ALTER TABLE invoice_correction ADD COLUMN modified_by UUID REFERENCES evaka_user(id) NOT NULL DEFAULT '00000000-0000-0000-0000-000000000000'::UUID;
 ALTER TABLE invoice_correction ALTER COLUMN modified_by DROP DEFAULT;
+CREATE INDEX fk$invoice_correction_modified_by ON invoice_correction(modified_by);
+
+CREATE INDEX fk$invoice_correction_unit_id ON invoice_correction(unit_id);
