ee34709094b5ef14487025a2f2ed582212668fad
Petri Lehtinen <petri.lehtinen@reaktor.com>
2025-05-21 12:20:41 +0300

    Include polyfills based on usage

--- frontend/src/citizen-frontend/index.tsx
+++ frontend/src/citizen-frontend/index.tsx
@@ -2,7 +2,6 @@
 //
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
-import 'core-js/stable'
 import * as Sentry from '@sentry/browser'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
