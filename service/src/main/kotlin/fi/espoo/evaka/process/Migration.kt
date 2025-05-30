d6d9c2928417b7493e706cc28d4d5dfdd870ea6f
Petri Lehtinen <petri.lehtinen@reaktor.com>
2025-05-30 08:00:04 +0300

    Process metadata migration for fee decisions

--- service/src/main/kotlin/fi/espoo/evaka/process/Migration.kt
+++ service/src/main/kotlin/fi/espoo/evaka/process/Migration.kt
@@ -7,10 +7,13 @@ package fi.espoo.evaka.process
 import fi.espoo.evaka.application.ApplicationStatus
 import fi.espoo.evaka.application.ApplicationType
 import fi.espoo.evaka.application.setApplicationProcessId
+import fi.espoo.evaka.invoicing.data.setFeeDecisionProcessId
 import fi.espoo.evaka.shared.ApplicationId
 import fi.espoo.evaka.shared.ArchiveProcessConfig
 import fi.espoo.evaka.shared.ArchiveProcessType
+import fi.espoo.evaka.shared.EvakaUserId
 import fi.espoo.evaka.shared.FeatureConfig
+import fi.espoo.evaka.shared.FeeDecisionId
 import fi.espoo.evaka.shared.auth.AuthenticatedUser
 import fi.espoo.evaka.shared.db.Database
 import fi.espoo.evaka.shared.domain.EvakaClock
@@ -32,6 +35,17 @@ fun migrateProcessMetadata(
         featureConfig.archiveMetadataOrganization,
         featureConfig.archiveMetadataConfigs,
     )
+
+    val feeDecisionConfig = featureConfig.archiveMetadataConfigs[ArchiveProcessType.FEE_DECISION]
+    if (feeDecisionConfig != null) {
+        migrateFeeDecisionMetadata(
+            dbc,
+            featureConfig.archiveMetadataOrganization,
+            feeDecisionConfig,
+        )
+    } else {
+        logger.warn { "Missing metadata config for FEE_DECISION" }
+    }
 }
 
 private data class ApplicationMigrationData(
@@ -98,6 +112,69 @@ private fun migrateApplicationMetadata(
                     processId = processId,
                     state = ArchivedProcessState.COMPLETED,
                     now = application.statusModifiedAt,
+                    userId = systemInternalUser,
+                )
+            }
+        }
+    }
+}
+
+private data class FeeDecisionMigrationData(
+    val id: FeeDecisionId,
+    val created: HelsinkiDateTime,
+    val approvedAt: HelsinkiDateTime,
+    val approvedBy: EvakaUserId?,
+    val sentAt: HelsinkiDateTime?,
+)
+
+private fun migrateFeeDecisionMetadata(
+    dbc: Database.Connection,
+    archiveMetadataOrganization: String,
+    config: ArchiveProcessConfig,
+) {
+    val systemInternalUser = AuthenticatedUser.SystemInternalUser.evakaUserId
+    dbc.transaction { tx ->
+        val feeDecisions =
+            tx.createQuery {
+                    sql(
+                        """
+                        SELECT id, created, approved_at, approved_by_id AS approved_by, sent_at
+                        FROM fee_decision
+                        WHERE process_id IS NULL AND approved_at IS NOT NULL
+                        ORDER BY created
+                        """
+                    )
+                }
+                .toList<FeeDecisionMigrationData>()
+
+        feeDecisions.forEach { decision ->
+            val processId =
+                tx.insertProcess(
+                        processDefinitionNumber = config.processDefinitionNumber,
+                        year = decision.created.year,
+                        organization = archiveMetadataOrganization,
+                        archiveDurationMonths = config.archiveDurationMonths,
+                        migrated = true,
+                    )
+                    .id
+            tx.setFeeDecisionProcessId(decision.id, processId)
+            tx.insertProcessHistoryRow(
+                processId = processId,
+                state = ArchivedProcessState.INITIAL,
+                now = decision.created,
+                userId = systemInternalUser,
+            )
+            tx.insertProcessHistoryRow(
+                processId = processId,
+                state = ArchivedProcessState.DECIDING,
+                now = decision.approvedAt,
+                userId = decision.approvedBy ?: systemInternalUser,
+            )
+            if (decision.sentAt != null) {
+                tx.insertProcessHistoryRow(
+                    processId = processId,
+                    state = ArchivedProcessState.COMPLETED,
+                    now = decision.sentAt,
                     userId = systemInternalUser,
                 )
             }
