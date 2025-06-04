175192fd20f68671d2c7055976998009f873d9e6
Jarkko Pätäri <jarkko.patari@gofore.com>
2025-06-04 10:39:55 +0300

    Remember selected child list sort on navigation

--- frontend/src/employee-mobile-frontend/remember.tsx
+++ frontend/src/employee-mobile-frontend/remember.tsx
@@ -4,16 +4,35 @@
 
 import React, { createContext, useMemo, useState } from 'react'
 
+import type { AttendanceStatus } from 'lib-common/generated/api-types/attendance'
 import type { GroupId } from 'lib-common/generated/api-types/shared'
+
+import type { SortType } from './child-attendance/ChildList'
+
+type ChildListSort = Record<AttendanceStatus, SortType>
 
 export interface RememberState {
   groupId: GroupId | undefined
   saveGroupId: (groupId: GroupId | undefined) => void
+  childListSort: ChildListSort
+  updateChildListSort: (
+    attendanceStatus: AttendanceStatus,
+    sortType: SortType
+  ) => void
+}
+
+const defaultChildListSort: ChildListSort = {
+  COMING: 'CHILD_FIRST_NAME',
+  PRESENT: 'CHILD_FIRST_NAME',
+  DEPARTED: 'CHILD_FIRST_NAME',
+  ABSENT: 'CHILD_FIRST_NAME'
 }
 
 export const RememberContext = createContext<RememberState>({
   groupId: undefined,
-  saveGroupId: () => undefined
+  saveGroupId: () => undefined,
+  childListSort: defaultChildListSort,
+  updateChildListSort: () => undefined
 })
 
 export const RememberContextProvider = React.memo(
@@ -23,13 +42,20 @@ export const RememberContextProvider = React.memo(
     children: React.ReactNode
   }) {
     const [groupId, setGroupId] = useState<GroupId>()
+    const [childListSort, setChildListSort] = useState(defaultChildListSort)
 
     const value = useMemo<RememberState>(
       () => ({
         groupId,
-        saveGroupId: (groupId: GroupId | undefined) => setGroupId(groupId)
+        saveGroupId: (groupId: GroupId | undefined) => setGroupId(groupId),
+        childListSort,
+        updateChildListSort: (attendanceStatus, sortType) =>
+          setChildListSort({
+            ...childListSort,
+            [attendanceStatus]: sortType
+          })
       }),
-      [groupId, setGroupId]
+      [childListSort, groupId]
     )
     return (
       <RememberContext.Provider value={value}>
