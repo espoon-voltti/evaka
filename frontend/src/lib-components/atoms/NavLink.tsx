3676c6b69ce61b8d2875118f20af7ba99e3d1d7c
Petri Lehtinen <petri.lehtinen@reaktor.com>
2025-06-17 09:49:32 +0300

    Simplify active link matching
    Remove the `hasSubPages` prop and treat all paths as if they contain
    subpages. This is usually the correct assumption, and such the best
    default.
    
    Also support matching multiple routes and extract active route matching
    logic to `useIsRouteActive` hook. Both will be used in subsequent commits.

--- frontend/src/lib-components/atoms/NavLink.tsx
+++ frontend/src/lib-components/atoms/NavLink.tsx
@@ -3,8 +3,8 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
 import classNames from 'classnames'
-import React from 'react'
-import { Link } from 'wouter'
+import React, { useMemo } from 'react'
+import { Link, matchRoute, useLocation, useRouter } from 'wouter'
 
 type HTMLLinkAttributes = Omit<
   React.AnchorHTMLAttributes<HTMLAnchorElement>,
@@ -14,18 +14,34 @@ type HTMLLinkAttributes = Omit<
 export interface Props extends HTMLLinkAttributes {
   className?: string
   to: string
-  children: React.ReactNode
+  matchRoutes?: string | string[]
 }
 
 export default React.forwardRef(function NavLink(
-  { className, ...props }: Props,
+  { className, to, matchRoutes, ...props }: Props,
   ref: React.ForwardedRef<HTMLAnchorElement>
 ) {
+  const isActive = useIsRouteActive(matchRoutes ?? to)
   return (
     <Link
-      className={(active) => classNames(className, { active })}
+      className={classNames(className, { active: isActive })}
       ref={ref}
+      to={to}
       {...props}
     />
   )
 })
+
+export function useIsRouteActive(to: string | string[]): boolean {
+  const parser = useRouter().parser
+  const path = useLocation()[0]
+  return useMemo(() => {
+    if (typeof to === 'string') {
+      return matchRoute(parser, `${to}/*?`, path)[0]
+    } else {
+      return to.some((route) => {
+        return matchRoute(parser, `${route}/*?`, path)[0]
+      })
+    }
+  }, [parser, path, to])
+}
