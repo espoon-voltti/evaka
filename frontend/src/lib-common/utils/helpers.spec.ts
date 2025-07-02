89bd5c77977aa0fe95e485550d0612c3e29ccc2f
Petri Lehtinen <petri.lehtinen@reaktor.com>
2025-07-02 14:12:50 +0300

    Update tests to work with jest 30

--- frontend/src/lib-common/utils/helpers.spec.ts
+++ frontend/src/lib-common/utils/helpers.spec.ts
@@ -1,17 +1,26 @@
 /**
- * @jest-environment jsdom
+ * @jest-environment jsdom-global
  */
 // SPDX-FileCopyrightText: 2017-2020 City of Espoo
 //
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
+import { type JSDOM } from 'jsdom'
+
 import { isProduction, getEnvironment } from './helpers'
 
-function defineWindowLocation(host: string) {
-  const oldLocation = window.location
-  jest
-    .spyOn(window, 'location', 'get')
-    .mockImplementation(() => ({ ...oldLocation, host }))
+declare global {
+  var jsdom: JSDOM
+}
+
+function withHost(host: string, fn: () => void) {
+  const oldHref = window.location.href
+  global.jsdom.reconfigure({ url: `http://${host}` })
+  try {
+    fn()
+  } finally {
+    global.jsdom.reconfigure({ url: oldHref })
+  }
 }
 
 describe('helpers', () => {
@@ -21,46 +30,54 @@ describe('helpers', () => {
 
   describe('isProduction', () => {
     it('returns true when hostname is espoonvarhaiskasvatus.fi', () => {
-      defineWindowLocation('espoonvarhaiskasvatus.fi')
-      expect(isProduction()).toBeTruthy()
+      withHost('espoonvarhaiskasvatus.fi', () => {
+        expect(isProduction()).toBeTruthy()
+      })
     })
 
     it('returns false when hostname is staging.espoonvarhaiskasvatus.fi', () => {
-      defineWindowLocation('staging.espoonvarhaiskasvatus.fi')
-      expect(isProduction()).toBeFalsy()
+      withHost('staging.espoonvarhaiskasvatus.fi', () => {
+        expect(isProduction()).toBeFalsy()
+      })
     })
   })
 
   describe('getEnvironment', () => {
     it('returns "local" when host is localhost:9093', () => {
-      defineWindowLocation('localhost:9093')
-      expect(getEnvironment()).toEqual('local')
+      withHost('localhost:9093', () => {
+        expect(getEnvironment()).toEqual('local')
+      })
     })
 
     it('returns "staging" when hostname is staging.espoonvarhaiskasvatus.fi', () => {
-      defineWindowLocation('staging.espoonvarhaiskasvatus.fi')
-      expect(getEnvironment()).toEqual('staging')
+      withHost('staging.espoonvarhaiskasvatus.fi', () => {
+        expect(getEnvironment()).toEqual('staging')
+      })
     })
 
     it('returns "prod" when hostname is espoonvarhaiskasvatus.fi', () => {
-      defineWindowLocation('espoonvarhaiskasvatus.fi')
-      expect(getEnvironment()).toEqual('prod')
+      withHost('espoonvarhaiskasvatus.fi', () => {
+        expect(getEnvironment()).toEqual('prod')
+      })
     })
 
     it('returns "prod" when hostname is evaka.prod.espoon-voltti.fi', () => {
-      defineWindowLocation('evaka.prod.espoon-voltti.fi')
-      expect(getEnvironment()).toEqual('prod')
+      withHost('evaka.prod.espoon-voltti.fi', () => {
+        expect(getEnvironment()).toEqual('prod')
+      })
     })
 
     it('works with non-predefined environment names', () => {
       const envName = 'somethingtotallysillyandinvalid123'
-      defineWindowLocation(`${envName}.espoonvarhaiskasvatus.fi`)
-      expect(getEnvironment()).toEqual(envName)
+      withHost(`${envName}.espoonvarhaiskasvatus.fi`, () => {
+        expect(getEnvironment()).toEqual(envName)
+      })
     })
 
     it('returns "staging" when hostname is staging.espoonvarhaiskasvatus.fi:443', () => {
-      defineWindowLocation('staging.espoonvarhaiskasvatus.fi:443')
-      expect(getEnvironment()).toEqual('staging')
+      withHost('staging.espoonvarhaiskasvatus.fi:443', () => {
+        expect(getEnvironment()).toEqual('staging')
+      })
     })
   })
 })
