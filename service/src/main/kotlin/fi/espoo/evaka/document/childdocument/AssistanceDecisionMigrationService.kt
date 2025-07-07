a6a0aded2736bc1e442abbbacfb7981e3564c744
Joosa Kurvinen <joosa.kurvinen@reaktor.com>
2025-07-07 16:44:52 +0300

    wip

---
+++ service/src/main/kotlin/fi/espoo/evaka/document/childdocument/AssistanceDecisionMigrationService.kt
@@ -0,0 +1,397 @@
+// SPDX-FileCopyrightText: 2017-2025 City of Espoo
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+package fi.espoo.evaka.document.childdocument
+
+import fi.espoo.evaka.assistanceneed.decision.AssistanceLevel
+import fi.espoo.evaka.assistanceneed.decision.AssistanceNeedDecision
+import fi.espoo.evaka.assistanceneed.decision.AssistanceNeedDecisionStatus
+import fi.espoo.evaka.assistanceneed.decision.getAssistanceNeedDecisionById
+import fi.espoo.evaka.assistanceneed.decision.getAssistanceNeedDecisionDocumentKey
+import fi.espoo.evaka.caseprocess.getCaseProcessByAssistanceNeedDecisionId
+import fi.espoo.evaka.document.ChildDocumentType
+import fi.espoo.evaka.document.getTemplate
+import fi.espoo.evaka.document.getTemplateSummaries
+import fi.espoo.evaka.shared.CaseProcessId
+import fi.espoo.evaka.shared.ChildDocumentDecisionId
+import fi.espoo.evaka.shared.ChildDocumentId
+import fi.espoo.evaka.shared.ChildId
+import fi.espoo.evaka.shared.DaycareId
+import fi.espoo.evaka.shared.EmployeeId
+import fi.espoo.evaka.shared.EvakaUserId
+import fi.espoo.evaka.shared.async.AsyncJob
+import fi.espoo.evaka.shared.async.AsyncJobRunner
+import fi.espoo.evaka.shared.auth.AuthenticatedUser
+import fi.espoo.evaka.shared.db.Database
+import fi.espoo.evaka.shared.domain.DateRange
+import fi.espoo.evaka.shared.domain.EvakaClock
+import fi.espoo.evaka.shared.domain.HelsinkiDateTime
+import fi.espoo.evaka.shared.domain.OfficialLanguage
+import java.time.format.DateTimeFormatter
+import org.springframework.stereotype.Service
+
+@Service
+class AssistanceDecisionMigrationService(private val asyncJobRunner: AsyncJobRunner<AsyncJob>) {
+    init {
+        asyncJobRunner.registerHandler(::migrateDaycareAssistanceDecision)
+    }
+
+    fun migrateDaycareAssistanceDecision(
+        db: Database.Connection,
+        clock: EvakaClock,
+        msg: AsyncJob.MigrateDaycareAssistanceDecision,
+    ) {
+        val (decision, template) =
+            db.read { tx ->
+                val decision = tx.getAssistanceNeedDecisionById(msg.decisionId)
+                val template =
+                    tx.getTemplateSummaries()
+                        .filter {
+                            it.type ==
+                                ChildDocumentType.MIGRATED_DAYCARE_ASSISTANCE_NEED_DECISION &&
+                                it.language.isoLanguage == decision.language.isoLanguage
+                        }
+                        .also {
+                            if (it.isEmpty()) {
+                                throw IllegalStateException(
+                                    "No template found for migrating daycare assistance decision in language ${decision.language}"
+                                )
+                            } else if (it.size > 1) {
+                                throw IllegalStateException(
+                                    "Multiple templates found for migrating daycare assistance decision in language ${decision.language}"
+                                )
+                            }
+                        }
+                        .first()
+                        .let { tx.getTemplate(it.id)!! }
+                decision to template
+            }
+
+        if (!decision.status.isDecided())
+            throw IllegalStateException("Decision ${decision.id} is not in a decided state")
+        val childId =
+            decision.child?.id
+                ?: throw IllegalStateException("Decision ${decision.id} has no child")
+        val decisionNumber =
+            decision.decisionNumber
+                ?: throw IllegalStateException("Decision ${decision.id} has no decision number")
+        val decisionMaker =
+            decision.decisionMaker?.employeeId
+                ?: throw IllegalStateException("Decision ${decision.id} has no decision maker")
+        val decidedAt =
+            decision.decisionMade?.let { HelsinkiDateTime.atStartOfDay(it) }
+                ?: throw IllegalStateException("Decision ${decision.id} has no decision made date")
+        val documentId = ChildDocumentId(decision.id.raw)
+
+        val content = migrateContent(decision, getTranslations(decision.language))
+        validateContentAgainstTemplate(content, template.content)
+
+        db.transaction { tx ->
+            tx.deletePreviouslyMigratedDecision(documentId)
+            val documentKey = tx.getAssistanceNeedDecisionDocumentKey(decision.id)
+            val processId = tx.getCaseProcessByAssistanceNeedDecisionId(decision.id)?.id
+            tx.insertMigratedDocument(
+                id = documentId,
+                type = ChildDocumentType.MIGRATED_DAYCARE_ASSISTANCE_NEED_DECISION,
+                childId = childId,
+                content = content,
+                decidedAt = decidedAt,
+                documentKey = documentKey,
+                decisionMaker = decisionMaker,
+                decisionStatus =
+                    when (decision.status) {
+                        AssistanceNeedDecisionStatus.ACCEPTED ->
+                            ChildDocumentDecisionStatus.ACCEPTED
+                        AssistanceNeedDecisionStatus.REJECTED ->
+                            ChildDocumentDecisionStatus.REJECTED
+                        AssistanceNeedDecisionStatus.ANNULLED ->
+                            ChildDocumentDecisionStatus.ANNULLED
+                        else ->
+                            throw IllegalStateException(
+                                "Decision ${decision.id} is not in a valid state for migration"
+                            )
+                    },
+                decisionValidity = decision.validityPeriod,
+                decisionNumber = decisionNumber,
+                daycareId = decision.selectedUnit?.id,
+                processId = processId,
+            )
+
+            // todo: plan pdf generation if documentKey is null
+            // Note: AsyncJob.CreateChildDocumentPdf needs a new parameter to skip
+            // sending messages after the pdf upload!
+        }
+    }
+}
+
+private fun migrateContent(
+    decision: AssistanceNeedDecision,
+    translations: AssistanceDecisionTranslations,
+): DocumentContent {
+    val guardians = decision.guardianInfo.toList()
+    val guardian1 = guardians.getOrNull(0)
+    val guardian2 = guardians.getOrNull(1)
+
+    return DocumentContent(
+        listOf(
+            AnsweredQuestion.TextAnswer(
+                questionId = "pedagogical_motivation",
+                answer = decision.pedagogicalMotivation ?: "",
+            ),
+            AnsweredQuestion.CheckboxGroupAnswer(
+                questionId = "structural_motivation",
+                answer =
+                    listOfNotNull(
+                        CheckboxGroupAnswerContent(
+                                optionId = "structural_motivation_opt_smaller_group"
+                            )
+                            .takeIf { decision.structuralMotivationOptions.smallerGroup },
+                        CheckboxGroupAnswerContent(
+                                optionId = "structural_motivation_opt_special_group"
+                            )
+                            .takeIf { decision.structuralMotivationOptions.specialGroup },
+                        CheckboxGroupAnswerContent(
+                                optionId = "structural_motivation_opt_small_group"
+                            )
+                            .takeIf { decision.structuralMotivationOptions.smallGroup },
+                        CheckboxGroupAnswerContent(
+                                optionId = "structural_motivation_opt_group_assistant"
+                            )
+                            .takeIf { decision.structuralMotivationOptions.groupAssistant },
+                        CheckboxGroupAnswerContent(
+                                optionId = "structural_motivation_opt_child_assistant"
+                            )
+                            .takeIf { decision.structuralMotivationOptions.childAssistant },
+                        CheckboxGroupAnswerContent(
+                                optionId = "structural_motivation_opt_additional_staff"
+                            )
+                            .takeIf { decision.structuralMotivationOptions.additionalStaff },
+                    ),
+            ),
+            AnsweredQuestion.TextAnswer(
+                questionId = "structural_motivation_description",
+                answer = decision.structuralMotivationDescription ?: "",
+            ),
+            AnsweredQuestion.TextAnswer(
+                questionId = "care_motivation",
+                answer = decision.careMotivation ?: "",
+            ),
+            AnsweredQuestion.CheckboxGroupAnswer(
+                questionId = "services",
+                answer =
+                    listOfNotNull(
+                        CheckboxGroupAnswerContent(optionId = "service_opt_consultation_special_ed")
+                            .takeIf { decision.serviceOptions.consultationSpecialEd },
+                        CheckboxGroupAnswerContent(optionId = "service_opt_part_time_special_ed")
+                            .takeIf { decision.serviceOptions.partTimeSpecialEd },
+                        CheckboxGroupAnswerContent(optionId = "service_opt_full_time_special_ed")
+                            .takeIf { decision.serviceOptions.fullTimeSpecialEd },
+                        CheckboxGroupAnswerContent(
+                                optionId = "service_opt_interpretation_and_assistance_services"
+                            )
+                            .takeIf { decision.serviceOptions.interpretationAndAssistanceServices },
+                        CheckboxGroupAnswerContent(optionId = "service_opt_special_aides").takeIf {
+                            decision.serviceOptions.specialAides
+                        },
+                    ),
+            ),
+            AnsweredQuestion.TextAnswer(
+                questionId = "services_motivation",
+                answer = decision.servicesMotivation ?: "",
+            ),
+            AnsweredQuestion.DateAnswer(
+                questionId = "guardians_heard_on",
+                answer = decision.guardiansHeardOn,
+            ),
+            AnsweredQuestion.GroupedTextFieldsAnswer(
+                questionId = "guardian_1",
+                answer =
+                    listOf(
+                        listOf(
+                            guardian1?.name ?: "",
+                            guardian1?.isHeard?.let { heard ->
+                                if (heard) translations.yes else translations.no
+                            } ?: "",
+                        )
+                    ),
+            ),
+            AnsweredQuestion.TextAnswer(
+                questionId = "guardian_1_details",
+                answer = guardian1?.details ?: "",
+            ),
+            AnsweredQuestion.GroupedTextFieldsAnswer(
+                questionId = "guardian_2",
+                answer =
+                    listOf(
+                        listOf(
+                            guardian2?.name ?: "",
+                            guardian2?.isHeard?.let { heard ->
+                                if (heard) translations.yes else translations.no
+                            } ?: "",
+                        )
+                    ),
+            ),
+            AnsweredQuestion.TextAnswer(
+                questionId = "guardian_2_details",
+                answer = guardian2?.details ?: "",
+            ),
+            AnsweredQuestion.CheckboxAnswer(
+                questionId = "other_representative_heard",
+                answer = decision.otherRepresentativeHeard,
+            ),
+            AnsweredQuestion.TextAnswer(
+                questionId = "other_representative_details",
+                answer = decision.otherRepresentativeDetails ?: "",
+            ),
+            AnsweredQuestion.TextAnswer(
+                questionId = "view_of_guardians",
+                answer = decision.viewOfGuardians ?: "",
+            ),
+            AnsweredQuestion.CheckboxGroupAnswer(
+                questionId = "assistance_levels",
+                answer =
+                    decision.assistanceLevels.map { CheckboxGroupAnswerContent(optionId = it.name) },
+            ),
+            AnsweredQuestion.DateAnswer(
+                questionId = "validity_period_start",
+                answer = decision.validityPeriod.start,
+            ),
+            AnsweredQuestion.TextAnswer(
+                questionId = "validity_period_end",
+                answer =
+                    when {
+                        !decision.assistanceLevels.contains(
+                            AssistanceLevel.ASSISTANCE_SERVICES_FOR_TIME
+                        ) -> "-"
+                        decision.endDateNotKnown || decision.validityPeriod.end == null ->
+                            translations.endDateNotKnown
+                        else ->
+                            decision.validityPeriod.end.format(
+                                DateTimeFormatter.ofPattern("dd.MM.yyyy")
+                            )
+                    },
+            ),
+            AnsweredQuestion.TextAnswer(
+                questionId = "selected_unit_name",
+                answer = decision.selectedUnit?.name ?: "",
+            ),
+            AnsweredQuestion.TextAnswer(
+                questionId = "motivation_for_decision",
+                answer = decision.motivationForDecision ?: "",
+            ),
+            AnsweredQuestion.GroupedTextFieldsAnswer(
+                questionId = "preparers",
+                listOf(
+                    listOf(
+                        decision.preparedBy1?.name ?: "",
+                        decision.preparedBy1?.title ?: "",
+                        decision.preparedBy1?.phoneNumber ?: "",
+                    ),
+                    listOf(
+                        decision.preparedBy2?.name ?: "",
+                        decision.preparedBy2?.title ?: "",
+                        decision.preparedBy2?.phoneNumber ?: "",
+                    ),
+                ),
+            ),
+            AnsweredQuestion.GroupedTextFieldsAnswer(
+                questionId = "decision_maker",
+                listOf(
+                    listOf(decision.decisionMaker?.name ?: "", decision.decisionMaker?.title ?: "")
+                ),
+            ),
+        )
+    )
+}
+
+private fun Database.Transaction.deletePreviouslyMigratedDecision(id: ChildDocumentId) {
+    val type =
+        createQuery { sql("SELECT type FROM child_document WHERE id = ${bind(id)}") }
+            .exactlyOneOrNull<ChildDocumentType>() ?: return
+
+    if (
+        type != ChildDocumentType.MIGRATED_DAYCARE_ASSISTANCE_NEED_DECISION &&
+            type != ChildDocumentType.MIGRATED_PRESCHOOL_ASSISTANCE_NEED_DECISION
+    )
+        throw IllegalStateException(
+            "Found an existing document with id $id that is not a migrated assistance need decision"
+        )
+
+    execute { sql("DELETE FROM child_document_read WHERE document_id = ${bind(id)}") }
+    execute {
+        sql(
+            """
+        DELETE FROM child_document_decision 
+        WHERE id IN (SELECT decision_id FROM child_document WHERE id = ${bind(id)})
+    """
+        )
+    }
+    execute { sql("DELETE FROM child_document WHERE id = ${bind(id)}") }
+}
+
+private fun Database.Transaction.insertMigratedDocument(
+    id: ChildDocumentId,
+    type: ChildDocumentType,
+    childId: ChildId,
+    content: DocumentContent,
+    decidedAt: HelsinkiDateTime,
+    documentKey: String?,
+    decisionMaker: EmployeeId,
+    decisionStatus: ChildDocumentDecisionStatus,
+    decisionValidity: DateRange,
+    decisionNumber: Long,
+    daycareId: DaycareId?,
+    processId: CaseProcessId?,
+) {
+    val userId = EvakaUserId(decisionMaker.raw)
+    val systemUser = AuthenticatedUser.SystemInternalUser.evakaUserId
+
+    val childDocumentDecisionId =
+        createQuery {
+                sql(
+                    """
+        INSERT INTO child_document_decision (created_at, created_by, modified_at, modified_by, status, valid_from, valid_to, decision_number, daycare_id) 
+        VALUES (${bind(decidedAt)}, ${bind(userId)}, ${bind(decidedAt)}, ${bind(userId)}, $decisionStatus, ${bind(decisionValidity.start)}, ${bind(decisionValidity.end)}, ${bind(decisionNumber)}, ${bind(daycareId)})
+        RETURNING id
+    """
+                )
+            }
+            .exactlyOne<ChildDocumentDecisionId>()
+
+    createUpdate {
+            sql(
+                """
+INSERT INTO child_document (id, child_id, template_id, content, published_at, status, published_content, modified_at, document_key, content_modified_at, content_modified_by, process_id, created_by, type, decision_maker, decision_id) 
+VALUES (${bind(id)}, ${bind(childId)}, ${bind(content)}, ${bind(decidedAt)}, 'COMPLETED', ${bind(content)}, ${bind(decidedAt)}, ${bind(documentKey)}, ${bind(decidedAt)}, ${bind(userId)}, ${bind(processId)}, ${bind(systemUser)}, ${bind(type)}, ${bind(decisionMaker)}, ${bind(childDocumentDecisionId)})
+"""
+            )
+        }
+        .updateExactlyOne()
+}
+
+private data class AssistanceDecisionTranslations(
+    val yes: String,
+    val no: String,
+    val endDateNotKnown: String,
+)
+
+private val translationsFi =
+    AssistanceDecisionTranslations(
+        yes = "Kyllä",
+        no = "Ei",
+        endDateNotKnown = "Tukipalvelun päättymisajankohta ei tiedossa",
+    )
+
+private val translationsSv =
+    AssistanceDecisionTranslations(
+        yes = "Ja",
+        no = "Nej",
+        endDateNotKnown = "Slutdatumet för stödtjänster är okänt",
+    )
+
+private fun getTranslations(lang: OfficialLanguage) =
+    when (lang) {
+        OfficialLanguage.FI -> translationsFi
+        OfficialLanguage.SV -> translationsSv
+    }
--- service/src/main/kotlin/fi/espoo/evaka/shared/async/AsyncJob.kt
+++ service/src/main/kotlin/fi/espoo/evaka/shared/async/AsyncJob.kt
@@ -467,6 +467,17 @@ sealed interface AsyncJob : AsyncJobPayload {
         override val user: AuthenticatedUser? = null
     }
 
+    data class MigrateDaycareAssistanceDecision(val decisionId: AssistanceNeedDecisionId) :
+        AsyncJob {
+        override val user: AuthenticatedUser? = null
+    }
+
+    data class MigratePreschoolAssistanceDecision(
+        val decisionId: AssistanceNeedPreschoolDecisionId
+    ) : AsyncJob {
+        override val user: AuthenticatedUser? = null
+    }
+
     companion object {
         val main =
             AsyncJobRunner.Pool(
@@ -486,6 +497,8 @@ sealed interface AsyncJob : AsyncJobPayload {
                     GenerateFinanceDecisions::class,
                     InitializeFamilyFromApplication::class,
                     InvoiceCorrectionMigration::class,
+                    MigrateDaycareAssistanceDecision::class,
+                    MigratePreschoolAssistanceDecision::class,
                     NotifyDecisionCreated::class,
                     NotifyFeeDecisionApproved::class,
                     NotifyFeeDecisionPdfGenerated::class,
