e5b5bcb495d14c6e138f7ba37385bd9deef03e20
Mikko Savolainen <mikko.savolainen@gofore.com>
2025-08-25 13:24:25 +0300

    fix test comments

--- frontend/src/e2e-test/specs/5_employee/month-calendar.spec.ts
+++ frontend/src/e2e-test/specs/5_employee/month-calendar.spec.ts
@@ -359,7 +359,7 @@ describe('Employee - Unit month calendar', () => {
         .absenceCell(testChild2.id, today)
         .missingQuestionnaireAnswer.waitUntilHidden()
 
-      // Missing holiday reservation is shown for holiday period dates
+      // Missing questionnaire answers are shown for holiday period dates
       await monthCalendarPage.nextWeekButton.click()
       let date = holidayStart
       while (date <= holidayEnd) {
@@ -370,7 +370,7 @@ describe('Employee - Unit month calendar', () => {
       }
     })
 
-    test('Missing holiday questionnaire indicators are not shown if placement started after questionnaire active period', async () => {
+    test('Missing holiday questionnaire answer indicators are not shown if placement started after questionnaire active period', async () => {
       await Fixture.holidayQuestionnaire({
         active: new FiniteDateRange(today.addWeeks(-2), today.addDays(-1)),
         periodOptions: [holidayRange]
@@ -391,7 +391,7 @@ describe('Employee - Unit month calendar', () => {
         .absenceCell(testChild2.id, today)
         .missingQuestionnaireAnswer.waitUntilHidden()
 
-      // Missing holiday reservation is shown for holiday period dates
+      // Missing questionnaire answers are not shown for holiday period dates
       await monthCalendarPage.nextWeekButton.click()
       let date = holidayStart
       while (date <= holidayEnd) {
