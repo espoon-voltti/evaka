ed85e6c933178a2394fdf1f7580146f8d463ab60
Joosa Kurvinen <joosa.kurvinen@reaktor.com>
2025-09-23 13:31:12 +0300

    render application cards

--- frontend/src/employee-frontend/components/applications/desktop/PlacementDesktop.tsx
+++ frontend/src/employee-frontend/components/applications/desktop/PlacementDesktop.tsx
@@ -3,10 +3,21 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
 import React from 'react'
+import styled from 'styled-components'
 
-import type { PagedApplicationSummaries } from 'lib-common/generated/api-types/application'
-import type { ApplicationId } from 'lib-common/generated/api-types/shared'
+import type {
+  ApplicationSummary,
+  PagedApplicationSummaries
+} from 'lib-common/generated/api-types/application'
+import type { DaycareId } from 'lib-common/generated/api-types/shared'
+import {
+  FixedSpaceColumn,
+  FixedSpaceRow
+} from 'lib-components/layout/flex-helpers'
 import { AlertBox } from 'lib-components/molecules/MessageBoxes'
+import { Gap } from 'lib-components/white-space'
+
+import ApplicationCard from './ApplicationCard'
 
 export default React.memo(function PlacementDesktop({
   applicationSummaries
@@ -37,17 +48,45 @@ export default React.memo(function PlacementDesktop({
 
   return (
     <PlacementDesktopValidated
-      applicationIds={applicationSummaries.data.map((a) => a.id)}
+      applications={applicationSummaries.data}
+      primaryUnits={primaryUnits}
     />
   )
 })
 
 const PlacementDesktopValidated = React.memo(
   function PlacementDesktopValidated({
-    applicationIds
+    applications,
+    primaryUnits
   }: {
-    applicationIds: ApplicationId[]
+    applications: ApplicationSummary[]
+    primaryUnits: DaycareId[]
   }) {
-    return <div>applications: {applicationIds.length}</div>
+    return (
+      <FixedSpaceRow>
+        <DaycaresColumn>Yksiköitä: {primaryUnits.length}</DaycaresColumn>
+        <ApplicationsColumn>
+          <div style={{ textAlign: 'right' }}>
+            Hakemuksia: {applications.length}
+          </div>
+          <Gap size="s" />
+          <FixedSpaceColumn alignItems="flex-end">
+            {applications.map((application) => (
+              <ApplicationCard key={application.id} application={application} />
+            ))}
+          </FixedSpaceColumn>
+        </ApplicationsColumn>
+      </FixedSpaceRow>
+    )
   }
 )
+
+const DaycaresColumn = styled.div`
+  flex-grow: 1;
+  width: 50%;
+`
+
+const ApplicationsColumn = styled.div`
+  flex-grow: 1;
+  width: 50%;
+`
