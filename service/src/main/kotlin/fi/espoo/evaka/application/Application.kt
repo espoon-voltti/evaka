a03d87fd8184e19803fd6bc0ea3e35e2db3490f6
Joosa Kurvinen <joosa.kurvinen@reaktor.com>
2025-09-24 15:54:09 +0300

    include trial placement unit in application summary

--- service/src/main/kotlin/fi/espoo/evaka/application/Application.kt
+++ service/src/main/kotlin/fi/espoo/evaka/application/Application.kt
@@ -74,6 +74,7 @@ data class ApplicationSummary(
     val streetAddress: String?,
     val postalCode: String?,
     val postOffice: String?,
+    val trialPlacementUnit: DaycareId? = null,
 )
 
 data class PlacementProposalStatus(
