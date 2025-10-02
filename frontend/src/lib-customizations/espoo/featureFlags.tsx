b03bd861284119c702b8c17c61bada193b7cb588
Jonni Madekivi <jonni.madekivi@reaktor.com>
2025-10-02 09:34:06 +0300

    Enable Aromi front-end flag in Espoo staging

--- frontend/src/lib-customizations/espoo/featureFlags.tsx
+++ frontend/src/lib-customizations/espoo/featureFlags.tsx
@@ -96,7 +96,7 @@ const features: Features = {
     archiveIntegration: {
       childDocuments: true
     },
-    aromiIntegration: false,
+    aromiIntegration: true,
     citizenChildDocumentTypes: true,
     decisionChildDocumentTypes: true,
     showCitizenApplicationPreschoolTerms: true,
