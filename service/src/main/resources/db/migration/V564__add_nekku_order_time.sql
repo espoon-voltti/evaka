91af6536029df34a39b5d9e04862c8dfe064848f
tahtee <96183234+tahtee@users.noreply.github.com>
2025-10-15 15:51:05 +0300

    Update every rows when sql is used for first time

--- service/src/main/resources/db/migration/V564__add_nekku_order_time.sql
+++ service/src/main/resources/db/migration/V564__add_nekku_order_time.sql
@@ -2,5 +2,4 @@ ALTER TABLE nekku_orders_report
     ADD COLUMN created_at timestamptz NOT NULL DEFAULT now();
 
 UPDATE nekku_orders_report
-    SET created_at = (delivery_date + time '08:00')::timestamptz
-    WHERE created_at IS NULL;
+    SET created_at = (delivery_date + time '08:00')::timestamptz;
