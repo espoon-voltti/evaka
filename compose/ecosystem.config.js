3cb2cb694108298a67e2831f949ce146ae50ea2b
Petri Lehtinen <petri.lehtinen@reaktor.com>
2025-12-31 11:07:35 +0200

    Add multi-instance support

--- compose/ecosystem.config.js
+++ compose/ecosystem.config.js
@@ -4,6 +4,19 @@
 
 const path = require('path')
 
+const instance = parseInt(process.env.EVAKA_INSTANCE || '0', 10)
+
+const ports = {
+  db: 5432 + instance,
+  redis: 6379 + instance,
+  s3: 9876 + instance,
+  sftp: 2222 + instance,
+  idp: 9090 + instance,
+  apigw: 3000 + instance,
+  service: 8888 + instance,
+  frontend: 9099 + instance
+}
+
 const defaults = {
   autorestart: false
 }
@@ -13,13 +26,25 @@ module.exports = {
     name: 'apigw',
     script: 'yarn && yarn clean && yarn dev',
     cwd: path.resolve(__dirname, '../apigw'),
+    env: {
+      HTTP_PORT: ports.apigw,
+      EVAKA_SERVICE_URL: `http://localhost:${ports.service}`,
+      REDIS_PORT: ports.redis,
+      EVAKA_BASE_URL: `http://localhost:${ports.frontend}`,
+      SFI_SAML_CALLBACK_URL: `http://localhost:${ports.frontend}/api/application/auth/saml/login/callback`,
+      SFI_SAML_ENTRYPOINT: `http://localhost:${ports.idp}/idp/sso`,
+      SFI_SAML_LOGOUT_URL: `http://localhost:${ports.idp}/idp/slo`,
+      SFI_SAML_ISSUER: `http://localhost:${ports.frontend}/api/application/auth/saml/`
+    },
     ...defaults
   }, {
     name: 'frontend',
     script: 'yarn && yarn clean && yarn dev',
     cwd: path.resolve(__dirname, '../frontend'),
     env: {
-      'ICONS': process.env.ICONS
+      ICONS: process.env.ICONS,
+      EVAKA_FRONTEND_PORT: ports.frontend,
+      EVAKA_APIGW_PORT: ports.apigw
     },
     ...defaults
   }, {
@@ -27,6 +52,11 @@ module.exports = {
     script: `${__dirname}/run-after-db.sh`,
     args: './gradlew --no-daemon bootRun',
     cwd: path.resolve(__dirname, '../service'),
+    env: {
+      SERVER_PORT: ports.service,
+      EVAKA_DATABASE_URL: `jdbc:postgresql://localhost:${ports.db}/evaka_local`,
+      EVAKA_LOCAL_S3_URL: `https://localhost:${ports.s3}`
+    },
     ...defaults
   }, /*{
     name: 'ai',
