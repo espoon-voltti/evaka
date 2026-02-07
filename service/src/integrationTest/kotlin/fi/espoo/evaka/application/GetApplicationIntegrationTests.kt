ac533e8c8e407ad083fd628420fdfd51cc3afde1
Petri Lehtinen <petri.lehtinen@reaktor.com>
2026-02-07 12:09:13 +0200

    Replace global test fixtures in GetApplicationIntegrationTests

--- service/src/integrationTest/kotlin/fi/espoo/evaka/application/GetApplicationIntegrationTests.kt
+++ service/src/integrationTest/kotlin/fi/espoo/evaka/application/GetApplicationIntegrationTests.kt
@@ -9,11 +9,12 @@ import fi.espoo.evaka.application.persistence.daycare.DaycareFormV0
 import fi.espoo.evaka.application.persistence.daycare.OtherPerson
 import fi.espoo.evaka.shared.ApplicationId
 import fi.espoo.evaka.shared.DaycareId
-import fi.espoo.evaka.shared.EmployeeId
 import fi.espoo.evaka.shared.auth.AuthenticatedUser
 import fi.espoo.evaka.shared.auth.CitizenAuthLevel
 import fi.espoo.evaka.shared.auth.UserRole
 import fi.espoo.evaka.shared.auth.insertDaycareAclRow
+import fi.espoo.evaka.shared.dev.DevCareArea
+import fi.espoo.evaka.shared.dev.DevDaycare
 import fi.espoo.evaka.shared.dev.DevEmployee
 import fi.espoo.evaka.shared.dev.DevGuardian
 import fi.espoo.evaka.shared.dev.DevPerson
@@ -26,19 +27,10 @@ import fi.espoo.evaka.shared.domain.MockEvakaClock
 import fi.espoo.evaka.shared.domain.NotFound
 import fi.espoo.evaka.shared.domain.RealEvakaClock
 import fi.espoo.evaka.shared.job.ScheduledJobs
-import fi.espoo.evaka.test.validDaycareApplication
-import fi.espoo.evaka.testAdult_1
-import fi.espoo.evaka.testAdult_2
-import fi.espoo.evaka.testArea
-import fi.espoo.evaka.testChild_1
-import fi.espoo.evaka.testChild_2
-import fi.espoo.evaka.testChild_3
-import fi.espoo.evaka.testDaycare
-import fi.espoo.evaka.testDecisionMaker_1
+import fi.espoo.evaka.test.getValidDaycareApplication
 import fi.espoo.evaka.toDaycareFormAdult
 import fi.espoo.evaka.toDaycareFormChild
 import fi.espoo.evaka.vtjclient.service.persondetails.MockPersonDetailsService
-import fi.espoo.evaka.vtjclient.service.persondetails.legacyMockVtjDataset
 import java.time.LocalDate
 import java.time.LocalTime
 import java.util.UUID
@@ -55,27 +47,52 @@ class GetApplicationIntegrationTests : FullApplicationTest(resetDbBeforeEach = t
     @Autowired lateinit var stateService: ApplicationStateService
     @Autowired lateinit var scheduledJobs: ScheduledJobs
 
-    private val serviceWorker =
-        AuthenticatedUser.Employee(testDecisionMaker_1.id, setOf(UserRole.SERVICE_WORKER))
-    private val citizen = AuthenticatedUser.Citizen(testAdult_1.id, CitizenAuthLevel.STRONG)
-    private val testSpecialEducationTeacherId = EmployeeId(UUID.randomUUID())
-    private val testSpecialEducationTeacher =
-        AuthenticatedUser.Employee(testSpecialEducationTeacherId, setOf())
+    private val area = DevCareArea()
+    private val daycare = DevDaycare(areaId = area.id)
+    private val employee = DevEmployee()
+    private val specialEducationTeacher = DevEmployee()
+    private val adult1 =
+        DevPerson(
+            ssn = "010180-1232",
+            firstName = "John",
+            lastName = "Doe",
+            streetAddress = "Kamreerintie 2",
+            postalCode = "02770",
+            postOffice = "Espoo",
+        )
+    private val adult2 =
+        DevPerson(
+            ssn = "010279-123L",
+            firstName = "Joan",
+            lastName = "Doe",
+            streetAddress = "Kamreerintie 2",
+            postalCode = "02770",
+            postOffice = "Espoo",
+        )
+    private val child1 = DevPerson(ssn = "010617A123U")
+    private val child2 = DevPerson()
+    private val child3 = DevPerson()
 
+    private val serviceWorker =
+        AuthenticatedUser.Employee(employee.id, setOf(UserRole.SERVICE_WORKER))
+    private val citizen = AuthenticatedUser.Citizen(adult1.id, CitizenAuthLevel.STRONG)
+
+    private val validDaycareApplication = getValidDaycareApplication(preferredUnit = daycare)
     private val validDaycareForm = DaycareFormV0.fromApplication2(validDaycareApplication)
 
     @BeforeEach
     fun beforeEach() {
         db.transaction { tx ->
-            tx.insert(testDecisionMaker_1)
-            tx.insert(testArea)
-            tx.insert(testDaycare)
-            listOf(testAdult_1, testAdult_2).forEach { tx.insert(it, DevPersonType.ADULT) }
-            listOf(testChild_1, testChild_2, testChild_3).forEach {
-                tx.insert(it, DevPersonType.CHILD)
-            }
+            tx.insert(area)
+            tx.insert(daycare)
+            tx.insert(employee)
+            tx.insert(specialEducationTeacher)
+            listOf(adult1, adult2).forEach { tx.insert(it, DevPersonType.ADULT) }
+            listOf(child1, child2, child3).forEach { tx.insert(it, DevPersonType.CHILD) }
         }
-        MockPersonDetailsService.add(legacyMockVtjDataset())
+        MockPersonDetailsService.addPersons(adult1, adult2, child1)
+        MockPersonDetailsService.addDependants(adult1, child1)
+        MockPersonDetailsService.addDependants(adult2, child1)
     }
 
     @Test
@@ -88,13 +105,12 @@ class GetApplicationIntegrationTests : FullApplicationTest(resetDbBeforeEach = t
         val applicationId =
             db.transaction { tx ->
                 tx.insertTestApplication(
-                    childId = testChild_1.id,
-                    guardianId = testAdult_1.id,
+                    childId = child1.id,
+                    guardianId = adult1.id,
                     type = ApplicationType.DAYCARE,
                     document =
                         validDaycareForm.copy(
-                            apply =
-                                validDaycareForm.apply.copy(preferredUnits = listOf(testDaycare.id))
+                            apply = validDaycareForm.apply.copy(preferredUnits = listOf(daycare.id))
                         ),
                 )
             }
@@ -102,8 +118,8 @@ class GetApplicationIntegrationTests : FullApplicationTest(resetDbBeforeEach = t
         val data = getApplication(applicationId)
 
         assertEquals(applicationId, data.application.id)
-        assertEquals(testChild_1.id, data.application.childId)
-        assertEquals(testAdult_1.id, data.application.guardianId)
+        assertEquals(child1.id, data.application.childId)
+        assertEquals(adult1.id, data.application.guardianId)
         assertEquals(emptySet(), db.read { tx -> tx.getApplicationOtherGuardians(applicationId) })
 
         assertEquals(ApplicationType.DAYCARE, data.application.type)
@@ -125,7 +141,7 @@ class GetApplicationIntegrationTests : FullApplicationTest(resetDbBeforeEach = t
             db.transaction { tx ->
                 tx.insertTestApplication(
                     childId = childId,
-                    guardianId = testAdult_1.id,
+                    guardianId = adult1.id,
                     type = ApplicationType.DAYCARE,
                     document =
                         validDaycareForm.copy(
@@ -157,7 +173,7 @@ class GetApplicationIntegrationTests : FullApplicationTest(resetDbBeforeEach = t
         val applicationId =
             db.transaction { tx ->
                 tx.insertTestApplication(
-                    childId = testChild_1.id,
+                    childId = child1.id,
                     guardianId = guardianId,
                     type = ApplicationType.DAYCARE,
                     document =
@@ -188,38 +204,38 @@ class GetApplicationIntegrationTests : FullApplicationTest(resetDbBeforeEach = t
             db.transaction { tx ->
                 listOf(
                     tx.insertTestApplication(
-                        childId = testChild_1.id,
-                        guardianId = testAdult_1.id,
+                        childId = child1.id,
+                        guardianId = adult1.id,
                         status = ApplicationStatus.CREATED,
                         type = ApplicationType.DAYCARE,
                         document =
                             DaycareFormV0(
                                 ApplicationType.DAYCARE,
-                                child = testChild_1.toDaycareFormChild(),
-                                guardian = testAdult_1.toDaycareFormAdult(),
+                                child = child1.toDaycareFormChild(),
+                                guardian = adult1.toDaycareFormAdult(),
                             ),
                     ),
                     tx.insertTestApplication(
-                        childId = testChild_2.id,
-                        guardianId = testAdult_1.id,
+                        childId = child2.id,
+                        guardianId = adult1.id,
                         status = ApplicationStatus.CREATED,
                         type = ApplicationType.DAYCARE,
                         document =
                             DaycareFormV0(
                                 ApplicationType.DAYCARE,
-                                child = testChild_2.toDaycareFormChild(),
-                                guardian = testAdult_1.toDaycareFormAdult(),
+                                child = child2.toDaycareFormChild(),
+                                guardian = adult1.toDaycareFormAdult(),
                             ),
                     ),
                     tx.insertTestApplication(
-                        childId = testChild_3.id,
-                        guardianId = testAdult_1.id,
+                        childId = child3.id,
+                        guardianId = adult1.id,
                         type = ApplicationType.DAYCARE,
                         document =
                             DaycareFormV0(
                                 ApplicationType.DAYCARE,
-                                child = testChild_3.toDaycareFormChild(),
-                                guardian = testAdult_1.toDaycareFormAdult(),
+                                child = child3.toDaycareFormChild(),
+                                guardian = adult1.toDaycareFormAdult(),
                             ),
                     ),
                 )
@@ -251,15 +267,14 @@ class GetApplicationIntegrationTests : FullApplicationTest(resetDbBeforeEach = t
     @Test
     fun `application attachments`() {
         db.transaction { tx ->
-            tx.insert(DevEmployee(testSpecialEducationTeacherId))
             tx.insertDaycareAclRow(
-                testDaycare.id,
-                testSpecialEducationTeacherId,
+                daycare.id,
+                specialEducationTeacher.id,
                 UserRole.SPECIAL_EDUCATION_TEACHER,
             )
         }
 
-        val applicationId = createPlacementProposalWithAttachments(testDaycare.id)
+        val applicationId = createPlacementProposalWithAttachments(daycare.id)
 
         // Service workers sees attachments
         val serviceWorkerResult = getApplication(applicationId, serviceWorker)
@@ -268,13 +283,16 @@ class GetApplicationIntegrationTests : FullApplicationTest(resetDbBeforeEach = t
         // Special education teacher sees the application (because it has an assistance need) but
         // doesn't see the attachments
         val specialEducationTeacherResult =
-            getApplication(applicationId, testSpecialEducationTeacher)
+            getApplication(
+                applicationId,
+                AuthenticatedUser.Employee(specialEducationTeacher.id, setOf()),
+            )
         assertEquals(0, specialEducationTeacherResult.attachments.size)
     }
 
     @Test
     fun `application attachments when service workers adds attachments, end user does not see attachments uploaded by service worker`() {
-        val applicationId = createPlacementProposalWithAttachments(testDaycare.id)
+        val applicationId = createPlacementProposalWithAttachments(daycare.id)
 
         assertTrue(uploadAttachment(applicationId, serviceWorker))
 
@@ -290,16 +308,16 @@ class GetApplicationIntegrationTests : FullApplicationTest(resetDbBeforeEach = t
         val applicationId =
             db.transaction { tx ->
                 tx.insertTestApplication(
-                        childId = testChild_1.id,
-                        guardianId = testAdult_1.id,
+                        childId = child1.id,
+                        guardianId = adult1.id,
                         type = ApplicationType.DAYCARE,
-                        otherGuardians = setOf(testAdult_2.id),
+                        otherGuardians = setOf(adult2.id),
                         allowOtherGuardianAccess = true,
                         document =
                             validDaycareForm.copy(
                                 apply =
                                     validDaycareForm.apply.copy(
-                                        preferredUnits = listOf(testDaycare.id),
+                                        preferredUnits = listOf(daycare.id),
                                         siblingBasis = true,
                                         siblingSsn = "secret",
                                         siblingName = "secret",
@@ -316,20 +334,16 @@ class GetApplicationIntegrationTests : FullApplicationTest(resetDbBeforeEach = t
                                     ),
                             ),
                     )
-                    .also {
-                        tx.insert(
-                            DevGuardian(guardianId = testAdult_2.id, childId = testChild_1.id)
-                        )
-                    }
+                    .also { tx.insert(DevGuardian(guardianId = adult2.id, childId = child1.id)) }
             }
 
         val guardianResult =
             getApplication(
                 applicationId,
-                AuthenticatedUser.Citizen(testAdult_1.id, CitizenAuthLevel.STRONG),
+                AuthenticatedUser.Citizen(adult1.id, CitizenAuthLevel.STRONG),
             )
         assertEquals(
-            listOf(PreferredUnit(id = testDaycare.id, name = testDaycare.name)),
+            listOf(PreferredUnit(id = daycare.id, name = daycare.name)),
             guardianResult.form.preferences.preferredUnits,
         )
         assertEquals(
@@ -350,10 +364,10 @@ class GetApplicationIntegrationTests : FullApplicationTest(resetDbBeforeEach = t
         val otherGuardianResult =
             getApplication(
                 applicationId,
-                AuthenticatedUser.Citizen(testAdult_2.id, CitizenAuthLevel.STRONG),
+                AuthenticatedUser.Citizen(adult2.id, CitizenAuthLevel.STRONG),
             )
         assertEquals(
-            listOf(PreferredUnit(id = testDaycare.id, name = testDaycare.name)),
+            listOf(PreferredUnit(id = daycare.id, name = daycare.name)),
             otherGuardianResult.form.preferences.preferredUnits,
         )
         assertEquals(
@@ -391,7 +405,7 @@ class GetApplicationIntegrationTests : FullApplicationTest(resetDbBeforeEach = t
         val applicationId =
             db.transaction { tx ->
                 tx.insertTestApplication(
-                    childId = testChild_1.id,
+                    childId = child1.id,
                     guardianId = citizen.id,
                     status = ApplicationStatus.CREATED,
                     type = ApplicationType.DAYCARE,
