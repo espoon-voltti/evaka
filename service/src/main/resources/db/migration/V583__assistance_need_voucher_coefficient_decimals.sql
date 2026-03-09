8a99fc026ece0e3e8dfb11f1cb38e8d68682c8e3
Joonas Särkiniemi <joonas.sarkiniemi@haltu.fi>
2026-03-09 13:41:49 +0200

    Change assistance need coefficient validation to be 4 decimals instead of 2 for more accurate values

--- service/src/main/resources/db/migration/V583__assistance_need_voucher_coefficient_decimals.sql
+++ service/src/main/resources/db/migration/V583__assistance_need_voucher_coefficient_decimals.sql
@@ -1,2 +1,5 @@
 ALTER TABLE assistance_need_voucher_coefficient
     ALTER COLUMN coefficient TYPE numeric(6, 4);
+
+ALTER TABLE voucher_value_decision
+    ALTER COLUMN assistance_need_coefficient TYPE numeric(6, 4);
