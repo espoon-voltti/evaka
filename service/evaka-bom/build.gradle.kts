71e15ff7d5a7ad5b1d6ec7f37b43ce9549c9c50e
Petri Lehtinen <petri.lehtinen@reaktor.com>
2026-03-27 12:36:49 +0200

    CVE fix

--- service/evaka-bom/build.gradle.kts
+++ service/evaka-bom/build.gradle.kts
@@ -62,7 +62,7 @@ dependencies {
     api(platform("tools.jackson:jackson-bom:3.1.0"))
     api(platform("com.squareup.okhttp3:okhttp-bom:5.3.2"))
     api(platform("io.opentelemetry:opentelemetry-bom:1.60.1"))
-    api(platform("io.netty:netty-bom:4.2.10.Final"))
+    api(platform("io.netty:netty-bom:4.2.12.Final"))
     api(platform("org.apache.cxf:cxf-bom:4.2.0"))
     api(platform("org.jdbi:jdbi3-bom:3.51.0"))
     api(platform(libs.kotlin.bom))
