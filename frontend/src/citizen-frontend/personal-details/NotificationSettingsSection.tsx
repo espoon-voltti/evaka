8f1d0b1b90b51c89cdb5fefda50c2c8b71ee9ae4
Jonni Madekivi <jonni@egarden.fi>
2026-04-14 21:00:00 +0300

    feat(citizen/personal-details): nest push settings inside notification card

--- frontend/src/citizen-frontend/personal-details/NotificationSettingsSection.tsx
+++ frontend/src/citizen-frontend/personal-details/NotificationSettingsSection.tsx
@@ -22,6 +22,7 @@ import { featureFlags } from 'lib-customizations/citizen'
 import { faPen } from 'lib-icons'
 
 import { useTranslation } from '../localization'
+import { WebPushSettingsSection } from '../webpush/WebPushSettingsSection'
 
 import { EditButtonRow } from './components'
 import { updateNotificationSettingsMutation } from './queries'
@@ -258,6 +259,8 @@ export default React.memo(
             />
           </FixedSpaceRow>
         ) : null}
+        <Gap $size="m" />
+        <WebPushSettingsSection />
       </div>
     )
   })
