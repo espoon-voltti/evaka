8617fc8c3ed96f4ef11bc42bdaead81391605741
Petri Lehtinen <petri.lehtinen@reaktor.com>
2026-04-29 14:13:19 +0300

    Enable decision reasoning options in test in all seutu municipalities

--- frontend/src/lib-customizations/hameenkyro/featureFlags.tsx
+++ frontend/src/lib-customizations/hameenkyro/featureFlags.tsx
@@ -52,7 +52,8 @@ const features: Features = {
   default: {
     ...prod,
     environmentLabel: 'Test',
-    citizenChildDocumentTypes: true
+    citizenChildDocumentTypes: true,
+    decisionReasoningOptions: true
   },
   prod
 }
