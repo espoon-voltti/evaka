f7e50c52e6ad044a2ee87865ac5350ddcf1e61f7
Olli-Heikki Inkinen <olli-heikki.inkinen@reaktor.com>
2026-09-08 11:22:09 +0300

    Migrate ChildDocumentDownload to AuditContext

--- service/src/main/kotlin/evaka/core/document/childdocument/ChildDocumentControllerCitizen.kt
+++ service/src/main/kotlin/evaka/core/document/childdocument/ChildDocumentControllerCitizen.kt
@@ -90,6 +90,7 @@ class ChildDocumentControllerCitizen(
         clock: EvakaClock,
         @PathVariable documentId: ChildDocumentId,
     ): ResponseEntity<Any> {
+        val audit = AuditContext().add(documentId)
         return db.connect { dbc ->
                 dbc.read { tx ->
                     accessControl.requirePermissionFor(
@@ -99,10 +100,11 @@ class ChildDocumentControllerCitizen(
                         Action.Citizen.ChildDocument.DOWNLOAD,
                         documentId,
                     )
+                    tx.getChildDocumentChildId(documentId)?.let { audit.add(it) }
                     childDocumentService.getPdfResponse(tx, documentId)
                 }
             }
-            .also { Audit.ChildDocumentDownload.log(targetId = AuditId(documentId)) }
+            .also { audit.log(Audit.ChildDocumentDownload, clock) }
     }
 
     @PutMapping("/{documentId}/read")
