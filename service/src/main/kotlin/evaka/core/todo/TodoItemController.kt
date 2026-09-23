adcd4b02163ec2c29db25999e596fe353d31e311
Joosa Kurvinen <joosa.kurvinen@reaktor.com>
2026-09-23 16:03:19 +0300

    Add endpoints for listing, creating and deleting to-do items

---
+++ service/src/main/kotlin/evaka/core/todo/TodoItemController.kt
@@ -0,0 +1,50 @@
+// SPDX-FileCopyrightText: 2017-2026 City of Espoo
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+package evaka.core.todo
+
+import evaka.core.shared.TodoItemId
+import evaka.core.shared.auth.AuthenticatedUser
+import evaka.core.shared.db.Database
+import evaka.core.shared.domain.EvakaClock
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TodoItemController {
+    @GetMapping("/employee/todo-items")
+    fun getTodoItems(
+        db: Database,
+        user: AuthenticatedUser.Employee,
+        clock: EvakaClock,
+    ): List<TodoItem> {
+        return db.connect { dbc -> dbc.read { tx -> tx.getTodoItems(user.id) } }
+    }
+
+    @PostMapping("/employee/todo-items")
+    fun createTodoItem(
+        db: Database,
+        user: AuthenticatedUser.Employee,
+        clock: EvakaClock,
+        @RequestBody body: TodoItemRequest,
+    ): TodoItemId {
+        return db.connect { dbc ->
+            dbc.transaction { tx -> tx.insertTodoItem(user.id, clock.now(), body) }
+        }
+    }
+
+    @DeleteMapping("/employee/todo-items/{id}")
+    fun deleteTodoItem(
+        db: Database,
+        user: AuthenticatedUser.Employee,
+        clock: EvakaClock,
+        @PathVariable id: TodoItemId,
+    ) {
+        db.connect { dbc -> dbc.transaction { tx -> tx.deleteTodoItem(id) } }
+    }
+}
